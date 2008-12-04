83d8dd167dd731e1dbfb5092c92176b984fc9729
gauvain <gauvain@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2008-12-04 09:55:03 +0000

    autotoolize the chroot scripts; still needs some love (xfce menu not included in build, distcheck broken)
    git-svn-id: file:///home/code/svn/ovd/trunk@44 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

---
+++ chroot-utils/autogen.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+# build configure.in using svn revno
+if [ -d .svn ]; then
+    revision=$(LC_ALL=C svn info $0 | awk '/^Revision: / {printf "%05d\n", $2}')
+    sed -e "s/@REVISION@/${revision}/g" \
+        < "configure.in.in" > "configure.in"
+fi
+
+aclocal
+autoconf
+automake --add-missing --gnu --copy
+
