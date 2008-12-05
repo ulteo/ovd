e00570abf32816001a24773c02a2888890a36e77
gauvain <gauvain@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2008-12-05 15:57:49 +0000

    handle /dev/pts mount in the daemon init script
    git-svn-id: file:///home/code/svn/ovd/trunk@55 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/autogen.sh
+++ SessionManager/autogen.sh
@@ -4,7 +4,7 @@
 if [ -d .svn ]; then
     revision=$(LC_ALL=C svn info $0 | awk '/^Revision: / {printf "%05d\n", $2}')
     sed -e "s/@REVISION@/${revision}/g" \
-        < "configure.in.in" >> "configure.in"
+        < "configure.in.in" > "configure.in"
 fi
 
 glib-gettextize --force --copy
