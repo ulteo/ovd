6ebc208b732fb4d3dc2dca41db8c64171ed1a660
laurent <laurent@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2009-01-23 15:43:31 +0000

    bugfix typo
    git-svn-id: file:///home/code/svn/ovd/trunk@314 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/admin/classes/Preferences_admin.class.php
+++ SessionManager/web/admin/classes/Preferences_admin.class.php
@@ -274,7 +274,7 @@ class Preferences_admin extends Preferences {
 		foreach ($available_module as $module_name => $sub_module)
 			$modules_prettyname[$module_name] = $module_name;
 
-		$c = new config_element('module_enable',_('Modules options'), _('Choose the modules you want to enable.'), _('Choose the modules you want to enable.'), array('UserDB', 'ApplicationDB' 'UserGroupDB'), $modules_prettyname, 3);
+		$c = new config_element('module_enable',_('Modules options'), _('Choose the modules you want to enable.'), _('Choose the modules you want to enable.'), array('UserDB', 'ApplicationDB', 'UserGroupDB'), $modules_prettyname, 3);
 		$this->add($c,'general');
 
 		foreach ($available_module as $mod => $sub_mod){
