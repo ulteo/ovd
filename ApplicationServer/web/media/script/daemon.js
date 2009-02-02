4c3f08335bf85b228d791c260ae5de23d30b2824
jeremy <jeremy@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2009-02-02 10:15:43 +0000

    Printing applet version is invalid
    git-svn-id: file:///home/code/svn/ovd/trunk@403 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- ApplicationServer/web/media/script/daemon.js
+++ ApplicationServer/web/media/script/daemon.js
@@ -8,6 +8,7 @@ var my_height;
 
 var session_state = -1;
 var old_session_state = -1;
+
 var nb_share = 0;
 
 var window_alive = true;
@@ -120,8 +121,7 @@ function client_exit() {
 	new Ajax.Request(
 		'webservices/client_exit.php',
 		{
-			method: 'get',
-			asynchronous: true
+			method: 'get'
 		}
 	);
 }
@@ -190,12 +190,13 @@ function onUpdateInfos(transport) {
 
   var buffer = xml.getElementsByTagName('session');
 
-  if (buffer.length !=1) {
+  if (buffer.length != 1) {
     push_log('[session] bad xml format', 'error');
     return;
   }
 
   var sessionNode = buffer[0];
+
   if (! sessionNode.hasAttribute('status')) {
     push_log('[session] bad xml format', 'error');
     return;
@@ -211,24 +212,25 @@ function onUpdateInfos(transport) {
   else
     push_log('[session] Status: '+session_state, 'debug');
 
-  var print_Node = sessionNode.getElementsByTagName('print');
-  if (print_Node.length > 0) {
-    print_Node = print_Node[0];
+  var printNode = sessionNode.getElementsByTagName('print');
+  if (printNode.length > 0) {
+    printNode = printNode[0];
 
-    var path = print_Node.getAttribute('path');
-    var timestamp = print_Node.getAttribute('time');
+    var path = printNode.getAttribute('path');
+    var timestamp = printNode.getAttribute('time');
     do_print(path, timestamp);
   }
 
-  var sharing_Node = sessionNode.getElementsByTagName('sharing');
-  if (sharing_Node.length > 0) {
-    sharing_Node = sharing_Node[0];
-    if (! sharing_Node.hasAttribute('count')) {
+  var sharingNode = sessionNode.getElementsByTagName('sharing');
+  if (sharingNode.length > 0) {
+    sharingNode = sharingNode[0];
+
+    if (! sharingNode.hasAttribute('count')) {
       push_log('[session] bad xml format', 'error');
       return;
     }
 
-    var nb = sharing_Node.getAttribute('count');
+    var nb = sharingNode.getAttribute('count');
     if (nb_share != nb) {
       nb_share = nb;
       push_log('[session] nb share: '+nb_share, 'info');
@@ -241,8 +243,8 @@ function do_print(path, timestamp) {
 
   var print_url = 'http://'+server+'/webservices/print.php?timestamp='+timestamp;
 
-  $('printerContainer').innerHTML = '<applet code="com.ulteo.OnlineDesktopPrinting" archive="ulteo-printing-0.5.jar" codebase="applet/" width="1" height="1" name="ulteoprinting"><param name="url" value="'+print_url+'"><param name="filename" value="'+path+'"></applet>';
-
+  $('printerContainer').innerHTML = '<applet code="com.ulteo.OnlineDesktopPrinting" archive="ulteo-printing-0.5.1.jar" codebase="applet/" width="1" height="1" name="ulteoprinting"><param name="url" value="'+print_url+'"><param name="filename" value="'+path+'"></applet>';
   $('printerContainer').show();
+
   push_log('[print] Applet: starting', 'warning');
 }
