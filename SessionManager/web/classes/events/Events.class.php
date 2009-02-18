bed83711a99683a9de14f92e60509e7743ba0904
gauvain <gauvain@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2009-02-18 09:59:53 +0000

    add missing file
    git-svn-id: file:///home/code/svn/ovd/trunk@594 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

---
+++ SessionManager/web/classes/events/Events.class.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright (C) 2009 Ulteo SAS
+ * http://www.ulteo.com
+ * Author Gauvain Pocentek <gauvain@ulteo.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ **/
+
+require_once(dirname(__FILE__).'/../../includes/core.inc.php');
+
+class Events {
+	private static $events;
+
+	public static function loadAll() {
+		if (! isset(self::$events)) {
+			self::$events = array();
+			foreach (glob(EVENTS_DIR.'/*.class.php') as $file) {
+				$class_name = preg_replace ('/\.class\.php$/', '', basename($file));
+				self::$events[] = new $class_name();
+			}
+		}
+
+		return self::$events;
+	}
+}
