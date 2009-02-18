ab065b37e96df39bf11d879a1d2859cd8b26a532
laurent <laurent@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2009-02-18 13:00:25 +0000

    add UserGroupDB::ldap_posix
    git-svn-id: file:///home/code/svn/ovd/trunk@600 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

---
+++ SessionManager/web/modules/UserGroupDB/ldap_posix.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Copyright (C) 2009 Ulteo SAS
+ * http://www.ulteo.com
+ * Author Laurent CLOUET <laurent@ulteo.com>
+ *
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ **/
+class UserGroupDB_ldap_posix extends UserGroupDB_ldap_memberof{
+
+	public function import($id_) {
+		Logger::debug('main',"UserGroupDB::ldap_posix::import (id = $id_)");
+		
+		$prefs = Preferences::getInstance();
+		if (! $prefs)
+			die_error('get Preferences failed',__FILE__,__LINE__);
+		
+		$mods_enable = $prefs->get('general','module_enable');
+		if (! in_array('UserGroupDB',$mods_enable))
+			die_error(_('Module UserGroupDB must be enabled'),__FILE__,__LINE__);
+		
+		$configLDAP = $prefs->get('UserDB','ldap');
+		$configLDAP['userbranch'] = 'ou=Group'; // TODO : put it in a pref
+		
+		$ldap = new LDAP($configLDAP);
+		$sr = $ldap->search('cn='.$id_, NULL);
+		$infos = $ldap->get_entries($sr);
+		if (!is_array($infos))
+			return NULL;
+		if (isset($infos[0])) {
+			$info = $infos[0];
+			if ( isset($info['dn']) && $info['cn']) {
+				if (is_string($info['dn']) && isset($info['cn']) && is_array($info['cn']) && isset($info['cn'][0]) ) {
+					// return  = new UsersGroup($info['dn'], $info['cn'][0], '', true);
+					if (isset($info['description'][0]))
+						$description = $info['description'][0];
+					else
+						$description = '';
+					return  new UsersGroup($info['cn'][0], $info['cn'][0], $description, true);
+				}
+			}
+		}
+		return NULL;
+	}
+	
+	public function getList() {
+		Logger::debug('main','UserGroupDB::ldap_posix::getList');
+		
+		$prefs = Preferences::getInstance();
+		if (! $prefs)
+			die_error('get Preferences failed',__FILE__,__LINE__);
+		
+		$mods_enable = $prefs->get('general','module_enable');
+		if (! in_array('UserGroupDB',$mods_enable))
+			die_error(_('Module UserGroupDB must be enabled'),__FILE__,__LINE__);
+		
+		$configLDAP = $prefs->get('UserDB','ldap');
+		$configLDAP['userbranch'] = 'ou=Group'; // TODO : put it in a pref
+		
+		$ldap = new LDAP($configLDAP);
+		$sr = $ldap->search('cn=*', NULL);
+		$infos = $ldap->get_entries($sr);
+		$groups = array();
+		foreach ($infos as $info){
+			if ( isset($info['dn']) && $info['cn']) {
+				if (is_string($info['dn']) && isset($info['cn']) && is_array($info['cn']) && isset($info['cn'][0]) ) {
+					if (isset($info['description'][0]))
+						$description = $info['description'][0];
+					else
+						$description = '';
+					
+					$groups[$info['dn']] = new UsersGroup($info['cn'][0], $info['cn'][0], $description, true);
+				}
+			}
+		}
+		return $groups;
+	}
+	
+	
+	
+	public function configuration(){
+		// TODO
+		return array();
+	}
+	
+	public static function prettyName() {
+		return _('LDAP using posix group');
+	}
+	
+	public static function isDefault() {
+		return false;
+	}
+	
+	public static function liaisonType() {
+		return 'ldap_posix';
+	}
+}
+ 
