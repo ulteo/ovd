8c7a0a69a06f70dac4e581b979e06f81a22fd2df
gauvain <gauvain@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2009-02-24 12:18:41 +0000

    typos
    git-svn-id: file:///home/code/svn/ovd/trunk@672 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- ApplicationServer/daemon/functions.d/webservices.sh
+++ ApplicationServer/daemon/functions.d/webservices.sh
@@ -17,7 +17,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-webservices_wget='wget --no-check-certificate retry-connrefused --timeout=5'
+webservices_wget='wget --no-check-certificate --retry-connrefused --timeout=5'
 
 webservices_session_request() {
     local args="session=$1&status=$2&fqdn=${SERVERNAME}"
