be275ecb7289469a97a055b53b9a6c51e54d6789
jeremy <jeremy@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2009-02-26 16:22:47 +0000

    Change Index to Home in admin menu
    git-svn-id: file:///home/code/svn/ovd/trunk@756 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/admin/menu.php
+++ SessionManager/web/admin/menu.php
@@ -19,7 +19,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  **/
 $items = array(
-	'index.php'		=>	_('Index'),
+	'index.php'		=>	_('Home'),
 	'servers.php'		=>	_('Servers'),
 // 	'sessions.php'		=>	_('Sessions'),
 	'users.php'		=>	_('Users'),
