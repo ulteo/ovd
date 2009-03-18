302e2f61906de40b11559dcd07cfd5f3225b9eda
laurent <laurent@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2009-03-18 10:55:24 +0000

    bugfix init function
    git-svn-id: file:///home/code/svn/ovd/trunk@903 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/admin/includes/functions.inc.php
+++ SessionManager/web/admin/includes/functions.inc.php
@@ -20,7 +20,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  **/
- require_once(dirname(__FILE__).'/core.inc.php');
 
 function display_loadbar($percents_) {
 	$bar_width = 1.25;
@@ -162,6 +161,7 @@ function init($host_, $database_, $prefix_, $user_, $password_) {
 	$ret = $p->isValid();
 	if ($ret !== true) {
 		echo 'error isValid : '.$ret.'<br>';
+		return false;
 	}
 	$p->backup();
 	return true;
