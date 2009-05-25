f5ee537ac7aabd621fa274f4e81d23924453246e
jeremy <jeremy@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2009-05-25 12:04:38 +0000

    Modularization of AuthMethod_Token so it can also be used with eXo
    git-svn-id: file:///home/code/svn/ovd/trunk@1241 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/modules/AuthMethod/Token.php
+++ SessionManager/web/modules/AuthMethod/Token.php
@@ -22,7 +22,10 @@
 class AuthMethod_Token extends AuthMethod {
 	public function get_login() {
 		$buf = $this->prefs->get('AuthMethod','Token');
-		$token_url = $buf['user_authenticate_token_url'];
+		$token_url = $buf['url'];
+		$user_node_name = $buf['user_node_name'];
+		$login_attribute_name = $buf['login_attribute_name'];
+
 		if (! isset($token_url) or $token_url=='')
 			return NULL;
 
@@ -39,17 +42,17 @@ class AuthMethod_Token extends AuthMethod {
 			Logger::error('main', 'Token webservice does not send a valid XML');
 			return NULL;
 		}
-		$user_node = $dom->getElementsByTagname('user')->item(0);
+		$user_node = $dom->getElementsByTagname($user_node_name)->item(0);
 		if (! is_object($user_node)) {
 			Logger::error('main', 'Token webservice does not send a valid XML');
 			return NULL;
 		}
-		if (! $user_node->hasAttribute('login')) {
+		if (! $user_node->hasAttribute($login_attribute_name)) {
 			Logger::error('main', 'Token webservice does not send a valid XML');
 			return NULL;
 		}
 
-		$this->login = $user_node->getAttribute('login');
+		$this->login = $user_node->getAttribute($login_attribute_name);
 		return $this->login;
 	}
 
@@ -63,8 +66,17 @@ class AuthMethod_Token extends AuthMethod {
 
 	public static function prefsIsValid($prefs_, &$log=array()) {
 		$buf = $prefs_->get('AuthMethod','Token');
-		$token_url = $buf['user_authenticate_token_url'];
-		if (! isset($token_url) || $token_url=='')
+		$token_url = $buf['url'];
+		$user_node_name = $buf['user_node_name'];
+		$login_attribute_name = $buf['login_attribute_name'];
+
+		if (! isset($token_url) || $token_url == '')
+			return false;
+
+		if (! isset($user_node_name) || $user_node_name == '')
+			return false;
+
+		if (! isset($login_attribute_name) || $login_attribute_name == '')
 			return false;
 
 		if (! strstr($token_url, '%TOKEN%'))
@@ -75,7 +87,9 @@ class AuthMethod_Token extends AuthMethod {
 
 	public static function configuration() {
 		return array(
-			new ConfigElement_input('user_authenticate_token_url', _('Token validation URL'), _('If a token argument is sent to startsession, the system tries to get an user login by requesting the token validation url.<br /><br />Put here the url to request if a token argument is sent to <i>startsession</i> instead of login/password.<br /><br />The special string <b>%TOKEN%</b> needs to be set because it\'s replaced by the token argument when the URL is requested.'), _('If a token argument is sent to startsession, the system tries to get an user login by requesting the token validation url.<br /><br />Put here the url to request if a token argument is sent to <i>startsession</i> instead of login/password.<br /><br />The special string <b>%TOKEN%</b> needs to be set because it\'s replaced by the token argument when the URL is requested.'), 'http://trust.server.com?token=%TOKEN%')
+			new ConfigElement_input('url', _('Token validation URL'), _('If a token argument is sent to startsession, the system tries to get an user login by requesting the token validation url.<br /><br />Put here the url to request if a token argument is sent to <i>startsession</i> instead of login/password.<br /><br />The special string <b>%TOKEN%</b> needs to be set because it\'s replaced by the token argument when the URL is requested.'), _('If a token argument is sent to startsession, the system tries to get an user login by requesting the token validation url.<br /><br />Put here the url to request if a token argument is sent to <i>startsession</i> instead of login/password.<br /><br />The special string <b>%TOKEN%</b> needs to be set because it\'s replaced by the token argument when the URL is requested.'), 'http://trust.server.com?token=%TOKEN%'),
+			new ConfigElement_input('user_node_name', _('Token XML user node name'), _('The id of the XML node which contains the user login'), _('The id of the XMl node which contains the user login'), 'user'),
+			new ConfigElement_input('login_attribute_name', _('Token XML login attribute name'), _('The name of the XML attribute which contains the user login (in the node "user_node_name")'), _('The name of the XML attribute which contains the user login (in the node "user_node_name")'), 'login')
 		);
 	}
 }
