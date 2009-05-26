22a0bcf9dd5c57c45013457db1e2a56b1260c332
laurent <laurent@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2009-05-26 16:18:54 +0000

    optimization of Logger class (use an instance to reduce the number of call to Preferences::getInstance)
    git-svn-id: file:///home/code/svn/ovd/trunk@1251 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/classes/Logger.class.php
+++ SessionManager/web/classes/Logger.class.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * Copyright (C) 2008 Ulteo SAS
+ * Copyright (C) 2008,2009 Ulteo SAS
  * http://www.ulteo.com
- * Author Jeremy DESVAGES <jeremy@ulteo.com>
+ * Author Jeremy DESVAGES <jeremy@ulteo.com> 2008
+ * Author Laurent CLOUET <laurent@ulteo.com> 2009
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -21,22 +22,31 @@
 require_once(dirname(__FILE__).'/../includes/core-minimal.inc.php');
 
 class Logger {
+	protected static $level_flags=NULL;
+	
 	public static function append($module_='main', $data_='', $level_='info') {
-		if ($data_ === '')
-			return;
-
-		if (Preferences::hasInstance()) {
-			$prefs = Preferences::getInstance();
-			if (is_object($prefs))
-				$level_flags = $prefs->get('general', 'log_flags');
-
-			if (!isset($level_flags) || !is_array($level_flags))
-				$level_flags = array($level_);
-
-			if (in_array($level_, $level_flags))
-				@file_put_contents(SESSIONMANAGER_LOGS.'/'.strtolower($module_).'.log', @date('M j H:i:s').' - '.$_SERVER['REMOTE_ADDR'].' - '.strtoupper($level_).' - '.$data_."\r\n", FILE_APPEND);
-		} else
-			@file_put_contents(SESSIONMANAGER_LOGS.'/'.strtolower($module_).'.log', @date('M j H:i:s').' - '.$_SERVER['REMOTE_ADDR'].' - '.strtoupper($level_).' - NOPREFS - '.$data_."\r\n", FILE_APPEND);
+		if (is_null(self::$level_flags)) {
+			if ($data_ === '')
+				return;
+			
+			if (Preferences::hasInstance()) {
+				$prefs = Preferences::getInstance();
+				$level_flags2 = array();
+				
+				if (is_object($prefs))
+					$level_flags2 = $prefs->get('general', 'log_flags');
+				if (!isset($level_flags) || !is_array($level_flags))
+					$level_flags2 = array($level_);
+				
+				self::$level_flags = $level_flags2;
+			}
+			else {
+				@file_put_contents(SESSIONMANAGER_LOGS.'/'.strtolower($module_).'.log', @date('M j H:i:s').' - '.$_SERVER['REMOTE_ADDR'].' - '.strtoupper($level_).' - NOPREFS - '.$data_."\r\n", FILE_APPEND);
+				return;
+			}
+		}
+		if (in_array($level_, self::$level_flags))
+			@file_put_contents(SESSIONMANAGER_LOGS.'/'.strtolower($module_).'.log', @date('M j H:i:s').' - '.$_SERVER['REMOTE_ADDR'].' - '.strtoupper($level_).' - '.$data_."\r\n", FILE_APPEND);
 	}
 
 	public static function debug($module_='main', $data_='') {
