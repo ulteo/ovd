441a74e0ddde54813b0d78e2708da1c459c4f4a4
jeremy <jeremy@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2009-09-04 15:38:57 +0000

    Prepare Ulteo XML actions for AjaXplorer when starting portal
    git-svn-id: file:///home/code/svn/ovd/trunk@1633 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- ApplicationServer/web/start.php
+++ ApplicationServer/web/start.php
@@ -20,6 +20,145 @@
  **/
 require_once(dirname(__FILE__).'/includes/core.inc.php');
 
+function generateAjaxplorerActionsXML() {
+	$dom = new DomDocument('1.0', 'utf-8');
+
+	$driver_node = $dom->createElement('driver');
+	$driver_node->setAttribute('name', 'fs');
+	$driver_node->setAttribute('className', 'class.fsDriver.php');
+	$dom->appendChild($driver_node);
+
+	$actions_node = $dom->createElement('actions');
+	$driver_node->appendChild($actions_node);
+
+	$actions = array();
+	foreach ($_SESSION['parameters']['applications'] as $app_line) {
+		$buf = explode('|', $app_line);
+
+		$app_id = $buf[0];
+		$app_desktopfile = $buf[2];
+
+		$buf = new DesktopFile(CHROOT.'/'.$app_desktopfile, $_SESSION['parameters']['locale']);
+		$buf->parse();
+
+		if (count($buf->getMimeType()) == 0)
+			continue;
+
+		$application = query_url(SESSIONMANAGER_URL.'/webservices/application.php?id='.$app_id.'&fqdn='.SERVERNAME);
+
+		$sm_dom = new DomDocument();
+		@$sm_dom->loadXML($application);
+
+		if (! $sm_dom->hasChildNodes())
+			continue;
+
+		$application_node = $sm_dom->getElementsByTagname('application')->item(0);
+		if (is_null($application_node))
+			continue;
+
+		$executable_node = $application_node->getElementsByTagname('executable')->item(0);
+		if (is_null($executable_node))
+			continue;
+
+		if (! $executable_node->hasAttribute('command'))
+			continue;
+
+		$app_command = $executable_node->getAttribute('command');
+
+		$app_icon = query_url(SESSIONMANAGER_URL.'/webservices/icon.php?id='.$app_id.'&fqdn='.SERVERNAME);
+		@file_put_contents(dirname(__FILE__).'/portal/ajaxplorer/client/images/crystal/actions/16/ulteo'.$app_id.'.png', $app_icon);
+		@file_put_contents(dirname(__FILE__).'/portal/ajaxplorer/client/images/crystal/actions/22/ulteo'.$app_id.'.png', $app_icon);
+		@file_put_contents(dirname(__FILE__).'/portal/ajaxplorer/client/images/crystal/actions/32/ulteo'.$app_id.'.png', $app_icon);
+
+	$clientcallback_cdata = <<<EOF
+var path;
+if (window.actionArguments && window.actionArguments.length > 0) {
+	path = window.actionArguments[0];
+} else {
+	userSelection = ajaxplorer.getFilesList().getUserSelection();
+	if (userSelection && userSelection.isUnique())
+		path = userSelection.getUniqueFileName();
+}
+
+var window_ = window.open('about:blank', 'Ulteo'+Math.round(Math.random()*100), 'toolbar=no,status=no,top=0,left=0,width='+screen.width+',height='+screen.height+',scrollbars=no,resizable=no,resizeable=no,fullscreen=no');
+
+setTimeout(function() {
+	window_.location.href = '../external_app.php?app_id=$app_id&command=$app_command&doc='+path;
+}, 1000);
+EOF;
+
+		$actions['ulteo'.$app_id] = array(
+			'id'				=>	$app_id,
+			'command'			=>	$buf->getCommand(),
+			'text'				=>	$buf->getName(),
+			'mimes'				=>	$buf->getMimeType(),
+			'clientcallback'	=>	$clientcallback_cdata
+		);
+	}
+
+	foreach ($actions as $k => $v) {
+		$action_node = $dom->createElement('action');
+		$action_node->setAttribute('name', $k);
+		$action_node->setAttribute('fileDefault', 'true');
+		$actions_node->appendChild($action_node);
+
+		$gui_node = $dom->createElement('gui');
+		$gui_node->setAttribute('text', $v['text']);
+		$gui_node->setAttribute('title', $v['text']);
+		$gui_node->setAttribute('src', $k.'.png');
+		$gui_node->setAttribute('hasAccessKey', 'false');
+		$action_node->appendChild($gui_node);
+
+			$context_node = $dom->createElement('context');
+			$context_node->setAttribute('selection', 'true');
+			$context_node->setAttribute('dir', 'false');
+			$context_node->setAttribute('recycle', 'false');
+			$context_node->setAttribute('actionBar', 'false');
+			$context_node->setAttribute('actionBarGroup', 'get');
+			$context_node->setAttribute('contextMenu', 'true');
+			$context_node->setAttribute('infoPanel', 'true');
+			$context_node->setAttribute('inZip', 'false');
+			$context_node->setAttribute('ulteoMimes', implode(',', $v['mimes']));
+			$gui_node->appendChild($context_node);
+
+			$selectioncontext_node = $dom->createElement('selectionContext');
+			$selectioncontext_node->setAttribute('dir', 'false');
+			$selectioncontext_node->setAttribute('file', 'true');
+			$selectioncontext_node->setAttribute('recycle', 'false');
+			$selectioncontext_node->setAttribute('unique', 'true');
+			$gui_node->appendChild($selectioncontext_node);
+
+		$rightscontext_node = $dom->createElement('rightsContext');
+		$rightscontext_node->setAttribute('noUser', 'true');
+		$rightscontext_node->setAttribute('userLogged', 'only');
+		$rightscontext_node->setAttribute('read', 'true');
+		$rightscontext_node->setAttribute('write', 'false');
+		$rightscontext_node->setAttribute('adminOnly', 'false');
+		$action_node->appendChild($rightscontext_node);
+
+		$processing_node = $dom->createElement('processing');
+		$action_node->appendChild($processing_node);
+
+			$clientcallback_node = $dom->createElement('clientCallback');
+			$clientcallback_node->setAttribute('prepareModal', 'true');
+
+			$clientcallback_cdata_node = $dom->createCDATASection($v['clientcallback']);
+			$clientcallback_node->appendChild($clientcallback_cdata_node);
+
+			$processing_node->appendChild($clientcallback_node);
+
+			$servercallback_node = $dom->createElement('serverCallback');
+			$servercallback_node->setAttribute('methodName', 'switchAction');
+			$processing_node->appendChild($servercallback_node);
+	}
+
+	$xml = $dom->saveXML();
+
+	put_to_file(SESSION_PATH.'/'.$_SESSION['session'].'/parameters/ajaxplorerActions.xml', $xml);
+
+	return true;
+}
+
 $session = $_SESSION['session'];
 
 if (!isset($session) || $session == '')
@@ -46,9 +185,13 @@ if ($_SESSION['type'] == 'start' && get_from_file(SESSION_PATH.'/'.$session.'/in
 
 	@touch(SESSION_PATH.'/'.$session.'/infos/keepmealive');
 
+	generateAjaxplorerActionsXML();
+
 	put_to_file(SESSION_PATH.'/'.$session.'/infos/status', 1);
 } elseif ($_SESSION['type'] == 'resume' && get_from_file(SESSION_PATH.'/'.$session.'/infos/status') == 10) {
 	@touch(SESSION_PATH.'/'.$session.'/infos/keepmealive');
+
+	generateAjaxplorerActionsXML();
 
 	put_to_file(SESSION_PATH.'/'.$session.'/infos/status', 11);
 }
