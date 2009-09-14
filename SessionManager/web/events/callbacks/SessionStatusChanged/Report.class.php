e140350db10ed38cd11428e74c34276fdf94ac5d
gauvain <gauvain@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2009-09-14 16:08:01 +0000

    typo bis
    git-svn-id: file:///home/code/svn/ovd/trunk@1691 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/events/callbacks/SessionStatusChanged/Report.class.php
+++ SessionManager/web/events/callbacks/SessionStatusChanged/Report.class.php
@@ -31,7 +31,7 @@ class SessionStatusChangedReport extends EventCallback {
 				$token = $this->ev->id;
 				$sql_sessions = get_from_cache ('reports', 'sessids');
 				if (! is_array ($sql_sessions))
-					$sql_session = array ();
+					$sql_sessions = array ();
 				if (! array_key_exists($token, $sql_sessions)) {
 					$sessitem = new SessionReportItem($token);
 					if ($sessitem->getId() >= 0) {
