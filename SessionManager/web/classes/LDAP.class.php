db2a3d83f248d129d88b35632114e611c963aaeb
laurent <laurent@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2009-09-30 12:47:29 +0000

    Bugfix: add quote on ldap_search command
    git-svn-id: file:///home/code/svn/ovd/trunk@1826 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/classes/LDAP.class.php
+++ SessionManager/web/classes/LDAP.class.php
@@ -145,7 +145,7 @@ class LDAP {
 		if (!$buf) {
 			Logger::error('main', "LDAP::bind bind($dn_,$pwd_) failed : (error:".$this->errno().')');
 			$searchbase =$this->userbranch.','.$this->suffix;
-			$ldapsearch = 'ldapsearch -x -h "'.$this->host.'" -p '.$this->port.'  -P '.$this->protocol_version.' -W -D '.$dn_.' -LLL -b '.$searchbase;
+			$ldapsearch = 'ldapsearch -x -h "'.$this->host.'" -p '.$this->port.'  -P '.$this->protocol_version.' -W -D "'.$dn_.'" -LLL -b "'.$searchbase.'"';
 			Logger::error('main', 'LDAP - failed to validate the configuration please try this bash command : '.$ldapsearch);
 			return false;
 		}
