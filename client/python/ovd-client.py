ee18decb0a770845218db42a88d4ca817403c121
julien <julien@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2009-11-02 17:13:12 +0000

    Bugfix commit revision 2083
    git-svn-id: file:///home/code/svn/ovd/trunk@2084 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- client/python/ovd-client.py
+++ client/python/ovd-client.py
@@ -59,7 +59,7 @@ def parse_access(data):
         return False
 
     node = node[0]
-    for (attr, m) in [('passwd', 'pass'), ('port', 'port')]:
+    for (attr, m) in [('host','host'), ('user', 'login'), ('passwd', 'pass') ]:
         if not node.hasAttribute(attr):
             print "Bad xml result"
             return False
@@ -75,7 +75,7 @@ def parse_access(data):
         return False
 
     node = node[0]
-    for (attr, m) in [('host','host'), ('passwd', 'pass'), ('port', 'port')]:
+    for (attr, m) in [('passwd', 'pass'), ('port', 'port')]:
         if not node.hasAttribute(attr):
             print "Bad xml result"
             return False
