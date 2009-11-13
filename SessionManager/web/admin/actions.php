05d342e0e84369a99aa1f4897f16f7f00670dc5a
laurent <laurent@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2009-11-13 16:14:06 +0000

    Better popup_info on UserGroup management
    git-svn-id: file:///home/code/svn/ovd/trunk@2167 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/admin/actions.php
+++ SessionManager/web/admin/actions.php
@@ -146,14 +146,32 @@ if ($_REQUEST['name'] == 'User_UserGroup') {
 
 	if ($_REQUEST['action'] == 'add') {
 		$ret = Abstract_Liaison::save('UsersGroup', $_REQUEST['element'], $_REQUEST['group']);
-		if ($ret === true)
-			popup_info(sprintf(_('UsersGroup \'%s\' successfully modified'), $_REQUEST['group']));
+		if ($ret === true) {
+			$userGroupDB = UserGroupDB::getInstance();
+			$group = $userGroupDB->import($_REQUEST['group']);
+			if (is_object($group)) {
+				popup_info(sprintf(_('UsersGroup \'%s\' successfully modified'), $group->name));
+			}
+			else {
+				// problem, what to do ?
+				popup_info(sprintf(_('UsersGroup \'%s\' successfully modified'), $_REQUEST['group']));
+			}
+		}
 	}
 
 	if ($_REQUEST['action'] == 'del') {
 		$ret = Abstract_Liaison::delete('UsersGroup', $_REQUEST['element'], $_REQUEST['group']);
-		if ($ret === true)
-			popup_info(sprintf(_('UsersGroup \'%s\' successfully modified'), $_REQUEST['group']));
+		if ($ret === true) {
+			$userGroupDB = UserGroupDB::getInstance();
+			$group = $userGroupDB->import($_REQUEST['group']);
+			if (is_object($group)) {
+				popup_info(sprintf(_('UsersGroup \'%s\' successfully modified'), $group->name));
+			}
+			else {
+				// problem, what to do ?
+				popup_info(sprintf(_('UsersGroup \'%s\' successfully modified'), $_REQUEST['group']));
+			}
+		}
 	}
 }
 
