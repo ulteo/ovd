65001e67fe971fb0c88cfa09d2b47cf664809ce9
jeremy <jeremy@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2009-11-18 16:19:59 +0000

    Improvement: adding windows server in session infos page
    git-svn-id: file:///home/code/svn/ovd/trunk@2197 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/admin/sessions.php
+++ SessionManager/web/admin/sessions.php
@@ -103,6 +103,8 @@ if (isset($_POST['join'])) {
 
 	echo '<ul>';
 	echo '<li><strong>Server:</strong> <a href="servers.php?action=manage&fqdn='.$session->getAttribute('server').'">'.$session->getAttribute('server').'</a></li>';
+	if (isset($session->settings['windows_server']))
+		echo '<li><strong>Windows Server:</strong> <a href="servers.php?action=manage&fqdn='.$session->settings['windows_server'].'">'.$session->settings['windows_server'].'</a></li>';
 	echo '<li><strong>User:</strong> <a href="users.php?action=manage&id='.$session->getAttribute('user_login').'">'.$session->getAttribute('user_displayname').'</a></li>';
 	echo '<li><strong>Type:</strong> ';
 	if ($session->getAttribute('mode') == 'desktop')
