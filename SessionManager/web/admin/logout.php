01681e398c8b848bdc7083e5d8a32e19f9f44224
jeremy <jeremy@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2009-12-07 14:07:14 +0000

    Typo
    git-svn-id: file:///home/code/svn/ovd/trunk@2318 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/admin/logout.php
+++ SessionManager/web/admin/logout.php
@@ -18,7 +18,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  **/
- require_once(dirname(__FILE__).'/includes/core.inc.php');
+require_once(dirname(__FILE__).'/includes/core.inc.php');
 
 unset($_SESSION['admin_login']);
 if (isset($_SESSION['admin_ovd_user']))
