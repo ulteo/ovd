a75ee0f7d02afd4d25e48a04a465bff816ae94f3
laurent <laurent@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-01-07 14:38:26 +0000

    Backport from trunk r2148: Bugfix: on Session::orderDeletion if windows does not exist anymore
    git-svn-id: file:///home/code/svn/ovd/branches/2.0@2419 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/classes/Session.class.php
+++ SessionManager/web/classes/Session.class.php
@@ -202,6 +202,10 @@ class Session {
 
 		if (isset($this->settings['windows_server'])) {
 			$windows_server = Abstract_Server::load($this->settings['windows_server']);
+			if (! $windows_server) {
+				Logger::error('main', 'Session::orderDeletion Unable to delete windows session \''.$this->id.'\' server \''.$this->settings['windows_server'].'\' not found');
+				return false;
+			}
 
 			$buf = $windows_server->orderWindowsSessionDeletion($this->id);
 			if (! $buf) {
