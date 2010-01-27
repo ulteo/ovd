9b19b17c66c435a60e15dd50f265b6419bee020f
jeremy <jeremy@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-01-27 15:49:09 +0000

    Re-ask for user password after a successful login
    git-svn-id: file:///home/code/svn/ovd/trunk@2544 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- WebInterface/web/media/script/common.js
+++ WebInterface/web/media/script/common.js
@@ -68,6 +68,7 @@ function onStartSessionSuccess(transport) {
 	$('session_login').value = session_login;
 	$('session_password').value = session_password;
 
+	$('session_password').value = '';
 	enableLogin();
 
 	return true;
