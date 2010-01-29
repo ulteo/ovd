52112145fc633b0337c57468d8044210ca52a6c4
jeremy <jeremy@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-01-29 14:51:22 +0000

    Adding webservice to fetch applications list from session id
    git-svn-id: file:///home/code/svn/ovd/trunk@2582 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

---
+++ SessionManager/web/webservices/apps.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright (C) 2010 Ulteo SAS
+ * http://www.ulteo.com
+ * Author Jeremy DESVAGES <jeremy@ulteo.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ **/
+
+require_once(dirname(__FILE__).'/../includes/core.inc.php');
+
+header('Content-Type: text/xml; charset=utf-8');
+
+$dom = new DomDocument('1.0', 'utf-8');
+
+$session = Abstract_Session::load($_GET['session']);
+if (! $session)
+	die();
+
+$userDB = UserDB::getInstance();
+
+$user = $userDB->import($session->settings['user_login']);
+if (! is_object($user))
+	die();
+
+$applications_node = $dom->createElement('applications');
+
+foreach ($user->applications() as $application) {
+	$application_node = $dom->createElement('application');
+	$application_node->setAttribute('id', $application->getAttribute('id'));
+	$application_node->setAttribute('name', $application->getAttribute('name'));
+	$applications_node->appendChild($application_node);
+}
+
+$dom->appendChild($applications_node);
+
+$xml = $dom->saveXML();
+
+echo $xml;
