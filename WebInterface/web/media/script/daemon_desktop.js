2f62f1fa02c3f3c6e6dc3051eea8fa054469d0b7
jeremy <jeremy@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-02-09 15:57:32 +0000

    Typo
    git-svn-id: file:///home/code/svn/ovd/trunk@2727 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- WebInterface/web/media/script/daemon_desktop.js
+++ WebInterface/web/media/script/daemon_desktop.js
@@ -28,7 +28,7 @@ var Desktop = Class.create(Daemon, {
 
 		$('splashContainer').hide();
 
-		var applet_html_string = '<applet id="ulteoapplet" name="ulteoapplet" code="'+this.applet_main_class+'" codebase="applet/" archive="gnu-getopt.jar,log4j-1.2.jar,'+this.applet_version+'" cache_archive="gnu-getopt.jar,log4j-1.2.jar,'+this.applet_version+'" cache_archive_ex="gnu-getopt.jar,log4j-1.2.jar,'+this.applet_version+';preload" mayscript="true" width="'+this.my_width+'" height="'+parseInt(this.my_height-100)+'"> \
+		var applet_html_string = '<applet id="ulteoapplet" name="ulteoapplet" code="'+this.applet_main_class+'" codebase="applet/" archive="gnu-getopt.jar,log4j-1.2.jar,'+this.applet_version+'" cache_archive="gnu-getopt.jar,log4j-1.2.jar,'+this.applet_version+'" cache_archive_ex="gnu-getopt.jar,log4j-1.2.jar,'+this.applet_version+';preload" mayscript="true" width="'+this.my_width+'" height="'+this.my_height+'"> \
 			<param name="name" value="ulteoapplet" /> \
 			<param name="code" value="'+this.applet_main_class+'" /> \
 			<param name="codebase" value="applet/" /> \
