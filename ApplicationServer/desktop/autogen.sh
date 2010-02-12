bdca61df9cc062731dc985441681294aaadab1ee
samuel <samuel@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-02-12 15:07:07 +0000

    Improvement: merged two packages (ulteo-ovd-xfce-default-settings and start-ovd) for create a new one -> ovd-desktop
    git-svn-id: file:///home/code/svn/ovd/trunk@2752 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

---
+++ ApplicationServer/desktop/autogen.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# check needed tools
+tools=( aclocal libtoolize autoheader autoconf automake libtool )
+for tool in "${tools[@]}"; do
+  which $tool > /dev/null
+  if ! test $? -eq 0
+  then
+    echo "error, install $tool"
+    exit 1
+  fi
+done
+
+# build configure.in using svn revno
+if [ -d .svn ]; then
+    revision=$(LC_ALL=C svn info . | awk '/^Revision: / {printf "%05d\n", $2}')
+else
+    revision=0
+fi
+sed -e "s/@REVISION@/${revision}/g" < configure.ac.in > configure.in
+
+autoreconf -fvi
