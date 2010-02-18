23df64aa438f3870d1121ce5bc4a3208e8f6f93c
samuel <samuel@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-02-18 16:01:52 +0000

    Bugfix: improve OvdShell/autogen.sh
    git-svn-id: file:///home/code/svn/ovd/trunk@2793 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- ApplicationServer/OvdShells/autogen.sh
+++ ApplicationServer/OvdShells/autogen.sh
@@ -3,5 +3,7 @@
 # build configure.in using svn revno
 if [ -d .svn ]; then
     revision=$(LC_ALL=C svn info . | awk '/^Revision: / {printf "%05d\n", $2}')
-    sed -e "s/@REVISION@/${revision}/g" < setup.py.in > setup.py
+else
+    revision=0
 fi
+sed -e "s/@REVISION@/${revision}/g" < setup.py.in > setup.py
