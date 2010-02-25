69f8363332ef527b0c5ce9db824db9276ec8add7
jeremy <jeremy@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-02-25 09:58:11 +0000

    Push OVD Session ID in PHP $_SESSION
    git-svn-id: file:///home/code/svn/ovd/trunk@2827 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/startsession.php
+++ SessionManager/web/startsession.php
@@ -444,6 +444,8 @@ foreach ($session->servers as $server) {
 	}
 }
 
+$_SESSION['session_id'] = $session->id;
+
 header('Content-Type: text/xml; charset=utf-8');
 $dom = new DomDocument('1.0', 'utf-8');
 
