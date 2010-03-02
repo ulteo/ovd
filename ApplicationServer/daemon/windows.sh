54a8b7cdc4f5549900faa74b6172e4e671eaa2b1
laurent <laurent@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-03-02 14:01:05 +0000

    Bugfix: pid file missing (caused by rev2824)
    git-svn-id: file:///home/code/svn/ovd/branches/2.5@2855 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- ApplicationServer/daemon/windows.sh
+++ ApplicationServer/daemon/windows.sh
@@ -41,6 +41,7 @@ dir=$SESSID_DIR/sessions/$job
 log_INFO "Session $SESSID detect job $job"
 
 install -d -o www-data -g www-data -m 770 $dir
+get_pid >$dir/pid
 
 geometry=$(cat $SESSID_DIR/parameters/geometry)
 
