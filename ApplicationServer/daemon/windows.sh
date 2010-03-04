ff8ee1deb9b71353ab22ebae43cda7dbc9634bf6
laurent <laurent@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-03-04 13:51:39 +0000

    Bugfix: add application_switch_status on portal windows application
    git-svn-id: file:///home/code/svn/ovd/branches/2.0@2869 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- ApplicationServer/daemon/windows.sh
+++ ApplicationServer/daemon/windows.sh
@@ -42,6 +42,7 @@ log_INFO "Session $SESSID detect job $job"
 
 install -d -o www-data -g www-data -m 770 $dir
 get_pid >$dir/pid
+application_switch_status $SESSID $job 1
 
 geometry=$(cat $SESSID_DIR/parameters/geometry)
 
