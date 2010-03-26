1a77fb517c958c5acb3730cf103db993e19d4f10
samuel <samuel@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-03-26 13:50:37 +0000

    Code cleaning: remove useless code in chroot autogen
    git-svn-id: file:///home/code/svn/ovd/branches/2.5@2936 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- chroot-apps/autogen.sh
+++ chroot-apps/autogen.sh
@@ -1,10 +1,6 @@
 #!/bin/sh -e
 
-# build configure.in using svn revno
-if [ -d .svn ]; then
-    revision=$(LC_ALL=C svn info $0 | awk '/^Revision: / {printf "%05d\n", $2}')
-    sed -e "s/@REVISION@/${revision}/g" \
-        < "configure.in.in" > "configure.in"
-fi
-
+#
+# Please install the Ulteo xfce4-dev-tools package to run this script
+#
 xdt-autogen $@
