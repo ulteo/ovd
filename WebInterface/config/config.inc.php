19f41dceefa3e991e713f31d821619c7149fc3ae
jeremy <jeremy@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-04-15 11:42:58 +0000

    Update of default config file
    git-svn-id: file:///home/code/svn/ovd/trunk@3051 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- WebInterface/config/config.inc.php
+++ WebInterface/config/config.inc.php
@@ -1,2 +1,6 @@
 <?php
+//URL of the SessionManager to link the WebInterface to
 define('SESSIONMANAGER_URL', 'http://@SESSION_MANAGER@/sessionmanager/');
+
+//Enable/disable debug mode
+define('DEBUG_MODE', 0);
