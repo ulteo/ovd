7f7218b4c6ada75469e1a12ce41479da384945d8
samuel <samuel@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-04-20 15:25:48 +0000

    Packaging: fix a intltoolize issue
    git-svn-id: file:///home/code/svn/ovd/branches/2.5@3079 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- chroot-apps/autogen.sh
+++ chroot-apps/autogen.sh
@@ -4,3 +4,6 @@
 # Please install the Ulteo xfce4-dev-tools package to run this script
 #
 xdt-autogen $@
+
+# hack for certain intltoolize version that doesn't create these files
+touch intltool-extract.in intltool-merge.in intltool-update.in
