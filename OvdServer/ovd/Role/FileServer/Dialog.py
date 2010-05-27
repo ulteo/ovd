43e430d6a72eba7dd98203621fcae539268f3a72
julien <julien@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-05-27 14:54:38 +0000

    use net usershare to manage samba use share
    git-svn-id: file:///home/code/svn/ovd/trunk@3248 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- OvdServer/ovd/Role/FileServer/Dialog.py
+++ OvdServer/ovd/Role/FileServer/Dialog.py
@@ -44,9 +44,215 @@ class Dialog(AbstractDialog):
 		path = request["path"]
 		
 		if request["method"] == "GET":
-			return None
+			Logger.debug("do_GET "+path)
+			
+			if path == "/list/all":
+				return self.req_list_all(request)
+			
+			elif path.startswith("/profile/create"):
+				buf = path[len("/profile/create/"):]
+				
+				doc = Document()
+				rootNode = doc.createElement('profile')
+				rootNode.setAttribute("id", buf)
+				doc.appendChild(rootNode)
+				request["data"] = doc.toxml()
+				
+				return self.req_profile_create(request)
+			
+			elif path.startswith("/profile/enable"):
+				buf = path[len("/profile/enable/"):]
+				
+				doc = Document()
+				rootNode = doc.createElement('profile')
+				rootNode.setAttribute("id", buf)
+				rootNode.setAttribute("user", "toto")
+				rootNode.setAttribute("password", "tata")
+				doc.appendChild(rootNode)
+				request["data"] = doc.toxml()
+				
+				return self.req_profile_enable(request)
+			
+			elif path.startswith("/profile/disable"):
+				buf = path[len("/profile/disable/"):]
+				
+				doc = Document()
+				rootNode = doc.createElement('profile')
+				rootNode.setAttribute("id", buf)
+				doc.appendChild(rootNode)
+				request["data"] = doc.toxml()
+				
+				return self.req_profile_disable(request)
+	
+			
+			
+			
 		
 		elif request["method"] == "POST":
-			return None
+			if path == "/profile/create":
+				return self.req_profile_create(request)
 		
 		return None
+	
+	def req_list_all(self, request):
+		profiles = self.role_instance.get_profiles()
+		shares = self.role_instance.get_shares()
+		
+		doc = Document()
+		rootNode = doc.createElement('fs')
+		doc.appendChild(rootNode)
+		
+		for profile in profiles:
+			node = doc.createElement('profile')
+			node.setAttribute("id", profile)
+			rootNode.appendChild(node)
+		for share in shares:
+			node = doc.createElement('share')
+			node.setAttribute("id", share)
+			rootNode.appendChild(node)
+		
+		return self.req_answer(doc)
+	
+	def req_profile_create(self, request):
+		try:
+			document = minidom.parseString(request["data"])
+			roodNode = document.documentElement
+			
+			if roodNode.nodeName != "profile":
+				raise Exception("invalid root node")
+			
+			if not roodNode.hasAttribute("id"):
+				raise Exception("invalid root node")
+			
+			profile = roodNode.getAttribute("id")
+			if len(profile)==0 or "/" in profile:
+				raise Exception("invalid root node")
+			
+		except Exception, err:
+			Logger.warn("Invalid xml input: "+str(err))
+			doc = Document()
+			rootNode = doc.createElement('error')
+			rootNode.setAttribute("id", "usage")
+			doc.appendChild(rootNode)
+			return self.req_answer(doc)
+		
+		if self.role_instance.exists_profile(profile):
+			doc = Document()
+			rootNode = doc.createElement('error')
+			rootNode.setAttribute("id", "already_exists")
+			doc.appendChild(rootNode)
+			return self.req_answer(doc)
+		
+		if not self.role_instance.create_profile(profile):
+			doc = Document()
+			rootNode = doc.createElement('error')
+			rootNode.setAttribute("id", "system_error")
+			doc.appendChild(rootNode)
+			return self.req_answer(doc)
+		
+		
+		doc = Document()
+		rootNode = doc.createElement('profile')
+		rootNode.setAttribute("id", profile)
+		doc.appendChild(rootNode)
+		return self.req_answer(doc)
+
+	def req_profile_enable(self, request):
+		try:
+			document = minidom.parseString(request["data"])
+			roodNode = document.documentElement
+			
+			if roodNode.nodeName != "profile":
+				raise Exception("invalid root node")
+			
+			if not roodNode.hasAttribute("id"):
+				raise Exception("invalid root node")
+			
+			elements = {}
+			for item in ["id", "user", "password"]:
+				if not roodNode.hasAttribute(item):
+					raise Exception("invalid root node")
+				elements[item] = roodNode.getAttribute(item)
+				if len(elements[item])==0:
+					raise Exception("invalid root node")
+			
+			profile = elements["id"]
+			
+		except Exception, err:
+			Logger.warn("Invalid xml input: "+str(err))
+			doc = Document()
+			rootNode = doc.createElement('error')
+			rootNode.setAttribute("id", "usage")
+			doc.appendChild(rootNode)
+			return self.req_answer(doc)
+		
+		if not self.role_instance.exists_profile(profile):
+			doc = Document()
+			rootNode = doc.createElement('error')
+			rootNode.setAttribute("id", "not_exists")
+			doc.appendChild(rootNode)
+			return self.req_answer(doc)
+		
+		if not self.role_instance.enable_profile(profile, elements["user"], elements["password"]):
+			doc = Document()
+			rootNode = doc.createElement('error')
+			rootNode.setAttribute("id", "system_error")
+			doc.appendChild(rootNode)
+			return self.req_answer(doc)
+		
+		
+		doc = Document()
+		rootNode = doc.createElement('profile')
+		rootNode.setAttribute("id", profile)
+		rootNode.setAttribute("status", "enable")
+		doc.appendChild(rootNode)
+		return self.req_answer(doc)
+
+
+	def req_profile_disable(self, request):
+		try:
+			document = minidom.parseString(request["data"])
+			roodNode = document.documentElement
+			
+			if roodNode.nodeName != "profile":
+				raise Exception("invalid root node")
+			
+			if not roodNode.hasAttribute("id"):
+				raise Exception("invalid root node")
+			
+			if not roodNode.hasAttribute("id"):
+				raise Exception("invalid root node")
+			
+			profile = roodNode.getAttribute("id")
+			if len(profile)==0 or "/" in profile:
+				raise Exception("invalid root node")
+			
+		except Exception, err:
+			Logger.warn("Invalid xml input: "+str(err))
+			doc = Document()
+			rootNode = doc.createElement('error')
+			rootNode.setAttribute("id", "usage")
+			doc.appendChild(rootNode)
+			return self.req_answer(doc)
+		
+		if not self.role_instance.exists_profile(profile):
+			doc = Document()
+			rootNode = doc.createElement('error')
+			rootNode.setAttribute("id", "not_exists")
+			doc.appendChild(rootNode)
+			return self.req_answer(doc)
+		
+		if not self.role_instance.disable_profile(profile):
+			doc = Document()
+			rootNode = doc.createElement('error')
+			rootNode.setAttribute("id", "system_error")
+			doc.appendChild(rootNode)
+			return self.req_answer(doc)
+		
+		
+		doc = Document()
+		rootNode = doc.createElement('profile')
+		rootNode.setAttribute("id", profile)
+		rootNode.setAttribute("status", "disable")
+		doc.appendChild(rootNode)
+		return self.req_answer(doc)
--- OvdServer/ovd/Role/FileServer/__init__.py
+++ OvdServer/ovd/Role/FileServer/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2009 Ulteo SAS
+# Copyright (C) 2009-2010 Ulteo SAS
 # http://www.ulteo.com
-# Author Julien LANGLOIS <julien@ulteo.com> 2009
+# Author Julien LANGLOIS <julien@ulteo.com> 2009, 2010
 #
 # This program is free software; you can redistribute it and/or 
 # modify it under the terms of the GNU General Public License
@@ -18,6 +18,9 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+import commands
+import glob
+import os
 import time
 from xml.dom.minidom import Document
 
@@ -30,6 +33,9 @@ from Dialog import Dialog
 
 
 class FileServer(AbstractRole):
+	spool = "/var/lib/ulteo/ovd/fs"
+	shares_dir = "/var/lib/samba/usershares"
+	
 	def __init__(self, main_instance):
 		AbstractRole.__init__(self, main_instance)
 		self.dialog = Dialog(self)
@@ -38,7 +44,13 @@ class FileServer(AbstractRole):
 	def init(self):
 		Logger.info("FileServer init")
 		
+		if not os.path.isdir(self.spool):
+			os.makedirs(self.spool)
+		
+		self.cleanup_samba()
+		
 		return True
+
 	
 	@staticmethod
 	def getName():
@@ -54,3 +66,76 @@ class FileServer(AbstractRole):
 		while 1:
 			time.sleep(30)
 			Logger.debug("FileServer run loop")
+	
+	
+	def get_enabled_usershares(self):
+		s, o = commands.getstatusoutput("net usershare list")
+		if s is not 0:
+			Logger.error("FS: unable to 'net usershare list': %d => %s"%(s, o))
+			return []
+		
+		return [s.strip() for s in o.splitlines()]
+	
+	def cleanup_samba(self):
+		# check samba conf
+		ret = True
+		
+		for share in self.get_enabled_usershares():
+			s, o = commands.getstatusoutput("net usershare delete %s"%(share))
+			if s is not 0:
+				Logger.error("FS: unable to 'net usershare delete': %d => %s"%(s, o))
+				ret = False
+		
+		return ret
+	
+	def get_profiles(self):
+		profiles = []
+		
+		for f in glob.glob(self.spool+"/*"):
+			name = os.path.basename(f)
+			if name.startswith("p_"):
+				profiles.append(name[2:])
+			
+		return profiles
+	
+	def exists_profile(self, name):
+		return os.path.isdir(self.spool+"/p_"+name)
+	
+	def enable_profile(self, name, user, passwd):
+		s, o = commands.getstatusoutput('net usershare add %s "%s"'%("p_"+name, self.spool+"/p_"+name))
+		if s is not 0:
+			Logger.error("FS: unable to 'net usershare add': %d => %s"%(s, o))
+		
+		return s==0
+		
+	def disable_profile(self, name):
+		s, o = commands.getstatusoutput("net usershare delete p_%s"%(name))
+		if s is not 0:
+			Logger.error("FS: unable to 'net usershare delete': %d => %s"%(s, o))
+		
+		return s==0
+		
+	
+	def create_profile(self, name):
+		try:
+			os.mkdir(self.spool+"/p_"+name)
+		except:
+			Logger.warn("FS: unable to create profile '%s'"%(name))
+			return False
+		
+		return True
+	
+	def get_shares(self):
+		shares = []
+		
+		for f in glob.glob(self.spool+"/*"):
+			name = os.path.basename(f)
+			if name.startswith("s_"):
+				shares.append(name[2:])
+			
+		return shares
+	
+	def exists_share(self, name):
+		return os.path.isdir(self.spool+"/s_"+name)
+	
+
