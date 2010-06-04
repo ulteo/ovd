d6cf9d9ccc3e6cb1c4365269e95cc8604c4f25d4
samuel <samuel@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-06-04 11:51:12 +0000

    remove chroot-apps
    git-svn-id: file:///home/code/svn/ovd/trunk@3292 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- chroot-apps/autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-#
-# Please install the Ulteo xfce4-dev-tools package to run this script
-#
-xdt-autogen $@
-
-# hack for certain intltoolize version that doesn't create these files
-touch intltool-extract.in intltool-merge.in intltool-update.in
