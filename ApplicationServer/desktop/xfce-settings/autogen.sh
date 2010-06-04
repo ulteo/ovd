1ca2f3d3eef68acf59fa734366fdb65afdb536e9
samuel <samuel@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-06-04 15:00:12 +0000

    xfce sources cleaning
    git-svn-id: file:///home/code/svn/ovd/trunk@3296 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- ApplicationServer/desktop/xfce-settings/autogen.sh
+++ ApplicationServer/desktop/xfce-settings/autogen.sh
@@ -1,23 +1,10 @@
-#!/bin/bash
+#!/bin/sh -e
 
-# check needed tools
-tools=( aclocal libtoolize autoheader autoconf automake libtool )
-for tool in "${tools[@]}"; do
-  which $tool > /dev/null
-  if ! test $? -eq 0
-  then
-    echo "error, install $tool"
-    exit 1
-  fi
-done
+#####
+# Please install the Ulteo xfce4-dev-tools package to run this script
+#####
 
-# build configure.in using svn revno
-if [ -d .svn ]; then
-    revision=$(LC_ALL=C svn info . | awk '/^Revision: / {printf "%05d\n", $2}')
-else
-    revision=0
-fi
-sed -e "s/@REVISION@/${revision}/g" < configure.ac.in > configure.in
+xdt-autogen $@
 
-autoreconf -fvi
-./configure $@
+# hack for certain intltoolize version that doesn't create these files
+touch intltool-extract.in intltool-merge.in intltool-update.in
