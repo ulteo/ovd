4fac0b5146be870aba75a14ac3ab0f0460d0cd8f
jeremy <jeremy@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-06-14 13:43:59 +0000

    Merge:   r3175: "Per server" Sessions loading improvement
    git-svn-id: file:///home/code/svn/ovd/trunk@3367 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/classes/Server.class.php
+++ SessionManager/web/classes/Server.class.php
@@ -455,7 +455,7 @@ class Server {
 	public function getNbUsedSessions() {
 		Logger::debug('main', 'Starting Server::getNbUsedSessions for \''.$this->fqdn.'\'');
 
-  		$buf = Abstract_Session::getByServer($this->fqdn);
+  		$buf = Abstract_Session::countByServer($this->fqdn);
 
 		return count($buf);
 	}
