a8267fc01d794db77566d667b3c0fe02014d6765
laurent <laurent@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-07-06 16:00:54 +0000

    Bugfix: variable not initialized (bugfix of rev 3269)
    git-svn-id: file:///home/code/svn/ovd/trunk@3599 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- OvdServer/ovd/Role/FileServer/Share.py
+++ OvdServer/ovd/Role/FileServer/Share.py
@@ -2,6 +2,7 @@
 
 # Copyright (C) 2010 Ulteo SAS
 # http://www.ulteo.com
+# Author Laurent CLOUET <laurent@ulteo.com> 2010
 # Author Julien LANGLOIS <julien@ulteo.com> 2010
 #
 # This program is free software; you can redistribute it and/or 
@@ -94,6 +95,7 @@ class Share:
 	def disable(self):
 		cmd = "net usershare delete %s"%(self.name)
 		s, o = commands.getstatusoutput(cmd)
+		ret = True
 		if s is not 0:
 			ret = False
 			Logger.error("FS: unable to del share")
