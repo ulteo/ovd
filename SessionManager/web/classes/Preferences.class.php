557c4e8a548d80de76fdc7dfecb7bdd34cdd5402
jeremy <jeremy@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-07-06 09:40:45 +0000

    Bugfix from r3571: unnecessary inclusion
    git-svn-id: file:///home/code/svn/ovd/trunk@3576 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/classes/Preferences.class.php
+++ SessionManager/web/classes/Preferences.class.php
@@ -19,7 +19,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  **/
 require_once(dirname(__FILE__).'/../includes/core.inc.php');
-require_once(dirname(__FILE__).'/../admin/includes/core.inc.php');
 
 class Preferences {
 	public $elements;
