89eb643e3e34642abbc9b06d8f0687b96330eb49
julien <julien@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-07-08 12:02:35 +0000

    Improve the externalapps client a little bit
    git-svn-id: file:///home/code/svn/ovd/trunk@3614 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- client/java/src/org/ulteo/ovd/client/StartTokenAuth.java
+++ client/java/src/org/ulteo/ovd/client/StartTokenAuth.java
@@ -21,40 +21,95 @@
 package org.ulteo.ovd.client;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 
 import org.ini4j.Wini;
 import org.ulteo.ovd.client.integrated.OvdClientIntegrated;
 
 import gnu.getopt.Getopt;
+import gnu.getopt.LongOpt;
 
 public class StartTokenAuth {
-
+	public static String name = "OvdExternalAppsClient";
+	
+	public static void usage() {
+		System.err.println("Usage: java -jar "+name+" [-c configfile] [-s host] [-t token]");
+		System.err.println("	-c|--config=FILE			Configuration file");
+		System.err.println("	-o|--output=FILE			redirect output to file");
+		System.err.println("	-s|--session-manager=HOST	Session Manager adress");
+		System.err.println("	-t|--token=TOKEN			authentication token");
+	}
+	
+	
 	public static void main(String[] args) {
-		Getopt opt = new Getopt("ExternalApps",args,"c:");
-		OvdClient cli = null;
+		LongOpt[] options = new LongOpt[4];
+		StringBuffer optionsBuffer = new StringBuffer();
+		options[0] = new LongOpt("config", LongOpt.REQUIRED_ARGUMENT, optionsBuffer, 0);
+		options[1] = new LongOpt("output", LongOpt.REQUIRED_ARGUMENT, optionsBuffer, 0);
+		options[2] = new LongOpt("session-manager", LongOpt.REQUIRED_ARGUMENT, optionsBuffer, 0);
+		options[3] = new LongOpt("token", LongOpt.REQUIRED_ARGUMENT, optionsBuffer, 0);
 		
-		String profile = null;
+		Getopt opt = new Getopt(name, args, "c:o:s:t:", options);
+		
+		String ovdServer = null;
+		String token = null;
 		int c;
 		while ((c = opt.getopt()) != -1) {
-			if(c == 'c') {
-				profile = new String(opt.getOptarg());
+			if (c == 'c' || c == 0 && opt.getLongind() == 0) {
+				try {
+					Wini ini = new Wini(new File(opt.getOptarg()));
+					ovdServer = ini.get("server", "host");
+					ovdServer.isEmpty();
+					token = ini.get("token", "token");
+					token.isEmpty();
+				}
+				catch(Exception err) {
+					System.err.println("Error: invalid configurtion file '"+opt.getOptarg()+"'");
+					usage();
+					System.exit(1);
+				}
+			}
+			else if (c == 'o' || c == 0 && opt.getLongind() == 1) {
+				
+				try {
+					FileOutputStream fos = new FileOutputStream(opt.getOptarg());
+					PrintStream stream = new PrintStream(fos);
+					System.setOut(stream);
+					System.setErr(stream);
+				}
+				catch(Exception err) {
+					System.err.println("Error: unable to open file '"+opt.getOptarg()+"'");
+					usage();
+					System.exit(1);
+				}
+			}
+			else if (c == 's' || c == 0 && opt.getLongind() == 2) {
+				ovdServer = opt.getOptarg();
+			}
+			else if (c == 't' || c == 0 && opt.getLongind() == 3) {
+				token = opt.getOptarg();
+			}
+			else {
+				usage();
+				System.exit(1);
 			}
 		}
 		
-		if (profile != null) {
-			try {
-				String ovdServer = null;
-				String token = null;
-				Wini ini = new Wini(new File(profile));
-				ovdServer = ini.get("server", "host");
-				token = ini.get("token", "token");
-				cli = new OvdClientIntegrated(ovdServer, true, token);
-			} catch (IOException ioe) {
-				ioe.printStackTrace();
-			}
-			cli.start();
+		if (ovdServer == null) {
+			System.err.println("Error: No SM host specified");
+			usage();
+			System.exit(1);
 		}
+		
+		if (token == null) {
+			System.err.println("Error: No token specified");
+			usage();
+			System.exit(1);
+		}
+		
+		OvdClient cli = new OvdClientIntegrated(ovdServer, true, token);
+		cli.start();
 	}
-
 }
