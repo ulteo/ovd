85201b28b3fbb4141da9c820a16be8082fedb4c3
thomas <thomas@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-07-19 12:24:53 +0000

    Improvement: HexDump can now manage RdpPacket_Localised
    git-svn-id: file:///home/code/svn/ovd/trunk@3694 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- client/java/src/net/propero/rdp/HexDump.java
+++ client/java/src/net/propero/rdp/HexDump.java
@@ -30,7 +30,7 @@ public class HexDump {
      * @param data Array of byte data to be encoded
      * @param msg Message to include with outputted hex debug messages
      */
-    public static void encode(byte[] data, String msg/*PrintStream out*/) {
+    public static void encode(byte[] data, String msg) {
     	int i, thisline, offset = 0;
 
     	while (offset < data.length)
@@ -53,67 +53,11 @@ public class HexDump {
     		offset += thisline;
     	}
     }
-		/*int count = 0;
-		String index;
-		String number;
-		
-		logger.debug(msg);
-		
-		while(count < data.length) {
-		    index = Integer.toHexString(count);
-		    switch(index.length()) {
-		    	case(1):
-		    		index = "0000000".concat(index);
-		    		break;
-		    	case(2):
-		    		index = "000000".concat(index);
-		    		break;
-		    	case(3):
-		    		index = "00000".concat(index);
-		    		break;
-		    	case(4):
-		    		index = "0000".concat(index);
-		    		break;
-		    	case(5):
-		    		index = "000".concat(index);
-		    		break;
-		    	case(6):
-		    		index = "00".concat(index);
-		    		break;
-		    	case(7):
-		    		index = "0".concat(index);
-		    		break;
-		    	case(8):
-		    		break;
-		    	default:
-		    		return;
-		    }
-		    
-		    index += ": ";
-		    //out.print(index + ": ");
-		    for(int i = 0; i < 16; i++) {
-		    	if(count >= data.length) {
-		    		break;
-		    	}
-		    	number = Integer.toHexString((data[count]&0x000000ff));
-		    	switch(number.length()) {
-		    		case(1):
-		    			number = "0".concat(number);
-		    			break;
-		    		case(2):
-		    			break;
-		    		default:
-		    			logger.debug(index);
-		    			//out.println("");
-		    			return;
-		    	}
-		    	index+= (number + " "); 
-		    	//out.print(number + " ");
-		    	count++;
-		    }
-		    logger.debug(index);
-		    //out.println("");
-		}
-	
-    }*/
+
+    public static void encode(RdpPacket_Localised data, String msg) {
+	    byte[] buffer = new byte[data.size()];
+	    data.copyToByteArray(buffer, 0, 0, buffer.length);
+
+	    HexDump.encode(buffer, msg);
+    }
 }
