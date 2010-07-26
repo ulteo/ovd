04bafb0962637ce9ecf314c5957ff05f9bf2ea3e
jeremy <jeremy@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-07-26 15:10:06 +0000

    Userlist is now XML-formatted
    git-svn-id: file:///home/code/svn/ovd/trunk@3786 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/webservices/userlist.php
+++ SessionManager/web/webservices/userlist.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Copyright (C) 2008 Ulteo SAS
+ * Copyright (C) 2008-2010 Ulteo SAS
  * http://www.ulteo.com
- * Author Jeremy DESVAGES <jeremy@ulteo.com>
- * Author Laurent CLOUET <laurent@ulteo.com>
+ * Author Jeremy DESVAGES <jeremy@ulteo.com> 2008
+ * Author Laurent CLOUET <laurent@ulteo.com> 2010
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -21,14 +21,23 @@
  **/
 require_once(dirname(__FILE__).'/../includes/core.inc.php');
 
-header('Content-Type: text/plain');
-
 $userDB = UserDB::getInstance();
 
+$users = array();
 if ($userDB->canShowList())
-	$list_users = $userDB->getList();
-else
-	$list_users = array();
+	$users = $userDB->getList();
 
-foreach ($list_users as $list_user)
-	echo $list_user->getAttribute('login')."\n";
+$dom = new DomDocument('1.0', 'utf-8');
+
+$users_node = $dom->createElement('users');
+foreach ($users as $user) {
+	$user_node = $dom->createElement('user');
+	$user_node->setAttribute('login', $user->getAttribute('login'));
+	$user_node->setAttribute('displayname', $user->getAttribute('displayname'));
+	$users_node->appendChild($user_node);
+}
+$dom->appendChild($users_node);
+
+header('Content-Type: text/xml; charset=utf-8');
+echo $dom->saveXML();
+die();
