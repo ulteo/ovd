b687e81c7b3d7a115b947067f429c17ec4a96e9d
david <david@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-08-02 07:32:53 +0000

    Improvement: add autocompilation script
    git-svn-id: file:///home/code/svn/ovd/trunk@3948 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

---
+++ ApplicationServer/windows/PDFPrinterDriver/make.bat
@@ -0,0 +1,2 @@
+cmd /C make86.bat
+cmd /C make64.bat
---
+++ ApplicationServer/windows/PDFPrinterDriver/make86.bat
@@ -0,0 +1,13 @@
+set WINDDK_PATH=C:\WinDDK\7600.16385.1
+set PATH=%PATH%;%WINDDK_PATH%\bin
+set INITIAL_DIR=%cd%
+md \tmp
+copy * \tmp
+
+call setenv.bat %WINDDK_PATH%  x86 fre WXP
+cd \tmp
+build
+md "%INITIAL_DIR%\%_BUILDARCH%"
+copy "%_BUILDARCH%" "%INITIAL_DIR%\%_BUILDARCH%"
+cd "%INITIAL_DIR%"
+rmdir /Q /S \tmp
