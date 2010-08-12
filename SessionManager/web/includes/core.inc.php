f4c998eae2d88d3bb1156e3636de23f48d71fd56
jeremy <jeremy@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-08-12 14:51:45 +0000

    Bugfix: dont try to create sharedfolders SPOOL folder, it is not used anymore
    git-svn-id: file:///home/code/svn/ovd/trunk@4126 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/includes/core.inc.php
+++ SessionManager/web/includes/core.inc.php
@@ -26,7 +26,7 @@
 
 require_once(dirname(__FILE__).'/core-minimal.inc.php');
 
-$folders = array('reporting', 'tmp', 'cache', 'sharedfolders');
+$folders = array('reporting', 'tmp', 'cache');
 foreach ($folders as $folder) {
 	$buf = strtoupper($folder).'_DIR';
 	define($buf, SESSIONMANAGER_SPOOL.'/'.$folder);
