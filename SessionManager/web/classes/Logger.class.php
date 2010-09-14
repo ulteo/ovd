18f73b49232e5ee14c808623f2dfe43f9a28e686
jeremy <jeremy@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-09-14 15:01:38 +0000

    Improvement: Logger
    git-svn-id: file:///home/code/svn/ovd/trunk@4564 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/classes/Logger.class.php
+++ SessionManager/web/classes/Logger.class.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * Copyright (C) 2008,2009 Ulteo SAS
+ * Copyright (C) 2008-2010 Ulteo SAS
  * http://www.ulteo.com
- * Author Jeremy DESVAGES <jeremy@ulteo.com> 2008
  * Author Laurent CLOUET <laurent@ulteo.com> 2009
+ * Author Jeremy DESVAGES <jeremy@ulteo.com> 2008
+ * Author Julien LANGLOIS <julien@ulteo.com> 2010
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -22,31 +23,59 @@
 require_once(dirname(__FILE__).'/../includes/core-minimal.inc.php');
 
 class Logger {
-	protected static $level_flags = NULL;
-	
+	protected static $instances = array();
+	protected $filename = NULL;
+	protected $fd = NULL;
+
+	protected $level_flags = NULL;
+
+	public function __construct($module_='main') {
+		$this->filename = SESSIONMANAGER_LOGS.'/'.strtolower($module_).'.log';
+		$this->fd = @fopen($this->filename, 'a');
+		if ($this->fd === false)
+			$this->fd = NULL;
+
+		try {
+			$prefs = Preferences::getInstance();
+			$level_flags2 = array();
+
+			if (is_object($prefs))
+				$level_flags2 = $prefs->get('general', 'log_flags');
+			if (! isset($level_flags2) || ! is_array($level_flags2))
+				$level_flags2 = array($level_);
+
+			$this->level_flags = $level_flags2;
+		} catch (Exception $e) {}
+	}
+
+	public function __destruct() {
+		if (! is_null($this->fd))
+			@fclose($this->fd);
+	}
+
+	public function write($data_, $level_='info') {
+		if (is_array($this->level_flags) && ! in_array($level_, $this->level_flags))
+			return;
+
+		if (is_null($this->fd))
+			return;
+
+		$msg = @date('M j H:i:s').' - '.$_SERVER['REMOTE_ADDR'].' - '.strtoupper($level_).' - ';
+		if (is_null($this->level_flags))
+			$msg.=  'NOPREFS - ';
+		$msg.= $data_."\r\n";
+
+		@fwrite($this->fd, $msg);
+	}
+
 	public static function append($module_='main', $data_='', $level_='info') {
-		if (is_null(self::$level_flags)) {
-			if ($data_ === '')
-				return;
-			
-			if (Preferences::hasInstance()) {
-				$prefs = Preferences::getInstance();
-				$level_flags2 = array();
-				
-				if (is_object($prefs))
-					$level_flags2 = $prefs->get('general', 'log_flags');
-				if (! isset($level_flags2) || ! is_array($level_flags2))
-					$level_flags2 = array($level_);
-				
-				self::$level_flags = $level_flags2;
-			} else {
-				@file_put_contents(SESSIONMANAGER_LOGS.'/'.strtolower($module_).'.log', @date('M j H:i:s').' - '.$_SERVER['REMOTE_ADDR'].' - '.strtoupper($level_).' - NOPREFS - '.$data_."\r\n", FILE_APPEND);
-				return;
-			}
-		}
-		
-		if (in_array($level_, self::$level_flags))
-			@file_put_contents(SESSIONMANAGER_LOGS.'/'.strtolower($module_).'.log', @date('M j H:i:s').' - '.$_SERVER['REMOTE_ADDR'].' - '.strtoupper($level_).' - '.$data_."\r\n", FILE_APPEND);
+		if ($data_ == '')
+			return;
+
+		if (! array_key_exists($module_, self::$instances))
+			self::$instances[$module_] = new Logger($module_);
+
+		self::$instances[$module_]->write($data_, $level_);
 	}
 
 	public static function debug($module_='main', $data_='') {
