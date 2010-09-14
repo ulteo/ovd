4afaad691eaa57dd42ff6370794c4138b53a8d15
jeremy <jeremy@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-09-14 13:52:45 +0000

    Bugfix: core inclusion in crons
    git-svn-id: file:///home/code/svn/ovd/trunk@4560 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/crons/hourly.php
+++ SessionManager/web/crons/hourly.php
@@ -19,7 +19,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  **/
-require_once(dirname(__FILE__).'/../admin/includes/core.inc.php');
+require_once(dirname(__FILE__).'/../admin/includes/core-minimal.inc.php');
 
 //BEGIN Updating logs cache
 $servers = Servers::getAll();
--- SessionManager/web/crons/minutely.php
+++ SessionManager/web/crons/minutely.php
@@ -18,6 +18,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  **/
-require_once(dirname(__FILE__).'/../admin/includes/core.inc.php');
+require_once(dirname(__FILE__).'/../admin/includes/core-minimal.inc.php');
 
 exit(0);
