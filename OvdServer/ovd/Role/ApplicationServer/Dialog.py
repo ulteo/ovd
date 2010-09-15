b5245e9b10975f3c0bcd49ffd42a81e86ff2e76b
laurent <laurent@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-09-15 09:06:18 +0000

    Improvement: add log
    git-svn-id: file:///home/code/svn/ovd/trunk@4572 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- OvdServer/ovd/Role/ApplicationServer/Dialog.py
+++ OvdServer/ovd/Role/ApplicationServer/Dialog.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2008-2009 Ulteo SAS
+# Copyright (C) 2008-2010 Ulteo SAS
 # http://www.ulteo.com
 # Author Julien LANGLOIS <julien@ulteo.com> 2008
-# Author Laurent CLOUET <laurent@ulteo.com> 2009
+# Author Laurent CLOUET <laurent@ulteo.com> 2009-2010
 #
 # This program is free software; you can redistribute it and/or 
 # modify it under the terms of the GNU General Public License
@@ -69,6 +69,7 @@ class Dialog(AbstractDialog):
 			return None
 		
 		elif request["method"] == "POST":
+			Logger.debug("do_POST "+path)
 			if path == "/session/create":
 				return self.req_session_create(request)
 			
