58a116bdfda690a9a5fb88526a6e8067a5977fc7
julien <julien@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-09-20 09:26:53 +0000

    Feature: add a standalone native client. Java is not require
    git-svn-id: file:///home/code/svn/ovd/trunk@4615 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

---
+++ client/wrapper/main.c
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2009 Ulteo SAS
+ * http://www.ulteo.com
+ * Author Julien LANGLOIS <julien@ulteo.com> 2010
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+
+#ifndef _WIN32_IE
+#define _WIN32_IE 0x0500
+#endif //WIN32_IE
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <shlobj.h>
+
+#define JAR_FILE "OVDNativeClient.jar"
+
+int launch(const char *command_line, ...);
+
+void usage(const char *name) {
+    fprintf(stderr, "Usage: %s [-a jar-file]\n", name);
+    fprintf(stderr, "\t-a jar-file: the jar name to execute instead of the default one\n");
+}
+
+
+int main(int argc, LPSTR argv[]) {
+    //    TCHAR path[MAX_PATH];
+    int ret;
+    
+    if (argc > 1) {
+        fprintf(stderr, "ToDo.\n");
+        usage(argv[0]);
+        return 1;
+    }
+
+    {
+        TCHAR pwd[MAX_PATH];
+        GetCurrentDirectory(MAX_PATH, pwd);
+        printf("Pwd: '%s'\n", pwd);
+    }
+
+    //  PathCombine();
+    printf("Before jre\n");
+    ret = launch("jre\\bin\\java.exe -jar \"%s\"", JAR_FILE);
+    printf("After jre %d\n",ret);
+    return ret;
+}
+
+
+int launch(const char *command_line, ...) {
+  STARTUPINFO si;
+  PROCESS_INFORMATION pi;
+  DWORD lpExitCode;
+  DWORD dwCreationFlags = 0;
+  DWORD pid;
+  char buf[2048];
+  va_list ap;
+  
+  va_start(ap, command_line);
+  vsnprintf(buf, sizeof(buf), command_line, ap);
+  va_end(ap);
+  
+  ZeroMemory( &si, sizeof(si) );
+  si.cb = sizeof(si);
+  ZeroMemory( &pi, sizeof(pi) );
+  
+  si.dwFlags = STARTF_USESHOWWINDOW|STARTF_USESTDHANDLES;
+  si.wShowWindow = SW_HIDE;
+  
+  printf("Launch '%s'\n", buf);
+
+  if( !CreateProcess( NULL, (LPSTR)buf, NULL, NULL, FALSE, dwCreationFlags, NULL, NULL, &si, &pi )) {
+    return -1;
+  }
+  pid = pi.dwProcessId;
+  
+  // Wait until child process exits.
+  WaitForSingleObject( pi.hProcess, INFINITE );
+  GetExitCodeProcess(pi.hProcess, &lpExitCode);
+  
+  // Close process and thread handles. 
+  CloseHandle(pi.hProcess);
+  CloseHandle(pi.hThread);
+  
+  return (int)lpExitCode;  
+}
