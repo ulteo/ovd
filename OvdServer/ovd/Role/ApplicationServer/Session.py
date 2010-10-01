dc4b098107a159353fedbb670024b55f19c0c34f
julien <julien@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-10-01 14:41:07 +0000

    Bugfix: listdir trhows an exception when removing the directory in the same time
    git-svn-id: file:///home/code/svn/ovd/trunk@4758 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- OvdServer/ovd/Role/ApplicationServer/Session.py
+++ OvdServer/ovd/Role/ApplicationServer/Session.py
@@ -19,6 +19,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+import glob
 import locale
 import os
 import time
@@ -135,13 +136,14 @@ class Session:
 				encoding = "UTF8"
 			
 			applications = {}
-			for basename in os.listdir(self.instanceDirectory):
+			for path in glob.glob(os.path.join(self.instanceDirectory, "*")):
+				basename = os.path.basename(path)
+				
 				if type(basename) is unicode:
 					name = basename
 				else:
 					name = unicode(basename, encoding)
 				
-				path = os.path.join(self.instanceDirectory, basename)
 				if not os.path.isfile(path):
 					continue
 				
