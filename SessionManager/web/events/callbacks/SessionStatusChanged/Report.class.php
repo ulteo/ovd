e6e8de54f307c49e23e5392525f7856026c91496
laurent <laurent@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-10-06 11:35:38 +0000

    Improvement: remove useless log
    git-svn-id: file:///home/code/svn/ovd/trunk@4806 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/events/callbacks/SessionStatusChanged/Report.class.php
+++ SessionManager/web/events/callbacks/SessionStatusChanged/Report.class.php
@@ -28,48 +28,31 @@ class SessionStatusChangedReport extends EventCallback {
 		switch ($this->ev->status) {
 			/* session starts */
 			case Session::SESSION_STATUS_READY:
-				Logger::critical('main', 'SessionStatusChangedReport::run inited 00');
 				$token = $this->ev->id;
-				Logger::critical('main', 'SessionStatusChangedReport::run inited 04');
 				$sql_sessions = get_from_cache ('reports', 'sessids');
-				Logger::critical('main', 'SessionStatusChangedReport::run inited 07');
 				if (! is_array ($sql_sessions))
 					$sql_sessions = array ();
-				Logger::critical('main', 'SessionStatusChangedReport::run inited 10');
 				if (! array_key_exists($token, $sql_sessions)) {
-					Logger::critical('main', 'SessionStatusChangedReport::run inited 20 array_key_exists($token, $sql_sessions) -> faux');
 					$buf532 = Abstract_ReportSession::exists($token);
-					Logger::critical('main', 'SessionStatusChangedReport::run inited 30');
-					Logger::critical('main', 'SessionStatusChangedReport::run $buf532 '.serialize($buf532));
 					if ($buf532 == true) {
-						Logger::critical('main', 'SessionStatusChangedReport::run inited 40 $buf532 is true');
 						$sessitem = Abstract_ReportSession::load($token);
 					}
 					else {
-						Logger::critical('main', 'SessionStatusChangedReport::run inited 50');
 						if (! Abstract_Session::exists($token)) {
-							Logger::critical('main', 'SessionStatusChangedReport::run inited 60 Abstract_Session::exists($token) -> faux');
 							Logger::error('main', "SessionStatusChangedReport::run failed to load session '$token'");
 							return false;
 						}
-						Logger::critical('main', 'SessionStatusChangedReport::run inited 80');
 						$sessitem = new SessionReportItem($token);
 						$ret = Abstract_ReportSession::create($sessitem);
-						Logger::critical('main', 'SessionStatusChangedReport::run inited 87');
 						if (! $ret) {
-							Logger::critical('main', 'SessionStatusChangedReport::run inited 90 Abstract_ReportSession::create -> problem');
 							Logger::error('main', "SessionStatusChangedReport::run failed to save SessionReportItem($token)");
 							return false;
 						}
-						Logger::critical('main', 'SessionStatusChangedReport::run inited 95');
 					}
-					Logger::critical('main', 'SessionStatusChangedReport::run inited 100');
 					if ($sessitem->getId() >= 0) {
 						$sql_sessions[$token] = $sessitem;
 					}
-					Logger::critical('main', 'SessionStatusChangedReport::run inited 110');
 					set_cache($sql_sessions, 'reports', 'sessids');
-					Logger::critical('main', 'SessionStatusChangedReport::run inited 120');
 				}
 				break;
 
