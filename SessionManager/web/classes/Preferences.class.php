a7149e8ae4943e8fc7d8edcb5aedf9d62629a2da
julien <julien@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-11-04 10:48:10 +0000

    Add missing languages
    git-svn-id: file:///home/code/svn/ovd/trunk@5147 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/classes/Preferences.class.php
+++ SessionManager/web/classes/Preferences.class.php
@@ -179,7 +179,21 @@ class Preferences {
 		$this->add($c,'general');
 
 		$c = new ConfigElement_select('admin_language', _('Administration console language'), _('Administration console language'), _('Administration console language'), 'auto');
-		$c->setContentAvailable(array('auto'=>_('Autodetect'),'en_GB'=>'English','fr_FR'=>'Français','ja_JP'=>'日本語','ru_RU'=>'Русский'));
+		$c->setContentAvailable(array(
+			'auto'=>_('Autodetect'),
+			'ar_AE'=>'العربي',
+			'de_DE'=>'Deutsch',
+			'en_GB'=>'English',
+			'es_ES'=>'Español',
+			'fr_FR'=>'Français',
+			'id_ID'=>'Bahasa Indonesia',
+			'it_IT'=>'Italiano',
+			'ja_JP'=>'日本語',
+			'nl_NL'=>'Nederlands',
+			'pt_BR'=>'Português (Brasil)',
+			'ru_RU'=>'Русский',
+			'zh_CN'=>'中文简体',
+		));
 		$this->add($c,'general');
 
 		$c = new ConfigElement_multiselect('log_flags', _('Debug options list'), _('Select debug options you want to enable.'), _('Select debug options you want to enable.'), array('info','warning','error','critical'));
@@ -335,7 +349,20 @@ class Preferences {
 		$this->add($c,'general','session_settings_defaults');
 
 		$c = new ConfigElement_select('language', _('Default language for session'), _('Default language for session'), _('Default language for session'), 'en_GB');
-		$c->setContentAvailable(array('en_GB'=>'English','fr_FR'=>'Français','ja_JP'=>'日本語','ru_RU'=>'Русский'));
+		$c->setContentAvailable(array(
+			'ar_AE'=>'العربي',
+			'de_DE'=>'Deutsch',
+			'en_GB'=>'English',
+			'es_ES'=>'Español',
+			'fr_FR'=>'Français',
+			'id_ID'=>'Bahasa Indonesia',
+			'it_IT'=>'Italiano',
+			'ja_JP'=>'日本語',
+			'nl_NL'=>'Nederlands',
+			'pt_BR'=>'Português (Brasil)',
+			'ru_RU'=>'Русский',
+			'zh_CN'=>'中文简体',
+		));
 		$this->add($c,'general','session_settings_defaults');
 		$c = new ConfigElement_select('timeout', _('Default timeout for session'), _('Default timeout for session'), _('Default timeout for session'), 86400);
 		$c->setContentAvailable(array(60 => _('1 minute'),120 => _('2 minutes'),300 => _('5 minutes'),600 => _('10 minutes'),900 => _('15 minutes'),1800 => _('30 minutes'),3600 => _('1 hour'),7200 => _('2 hours'),18000 => _('5 hours'),43200 => _('12 hours'),86400 => _('1 day'),172800 => _('2 days'),604800 => _('1 week'),2764800 => _('1 month'),-1 => _('Never')));
