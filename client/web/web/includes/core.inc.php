ulteo/ovd

commit 947752955e9a06540238a591921025b324951d01

Name change: Webinterface -> Web client (now in client/web)
git-svn-id: file:///home/code/svn/ovd/trunk@4644 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

client/web/web/includes/core.inc.php
@@ -1,0 +1,54 @@
+<?php
+/**
+ * Copyright (C) 2010 Ulteo SAS
+ * http://www.ulteo.com
+ * Author Jeremy DESVAGES <jeremy@ulteo.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ **/
+
+define('WEB_CLIENT_ROOT', realpath(dirname(__FILE__).'/..'));
+
+$buf = @ini_get('include_path');
+@ini_set('include_path', $buf.':'.WEB_CLIENT_ROOT.'/PEAR');
+
+define('WEB_CLIENT_CONF_FILE', '/etc/ulteo/webclient/config.inc.php');
+
+if (file_exists(WEB_CLIENT_CONF_FILE))
+	include_once(WEB_CLIENT_CONF_FILE);
+
+require_once(dirname(__FILE__).'/functions.inc.php');
+
+@session_start();
+
+$sessionmanager_url = NULL;
+if (defined('SESSIONMANAGER_HOST'))
+	$sessionmanager_url = 'https://'.SESSIONMANAGER_HOST.'/ovd/client/';
+elseif (array_key_exists('web-client', $_SESSION) && array_key_exists('sessionmanager_url', $_SESSION['web-client']))
+	$sessionmanager_url = $_SESSION['web-client']['sessionmanager_url'];
+
+$debug_mode = false;
+if (defined('DEBUG_MODE') && DEBUG_MODE == 1)
+	$debug_mode = true;
+
+if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+	$buf = explode(',', strtolower($_SERVER['HTTP_ACCEPT_LANGUAGE']));
+	$buf = explode(';', $buf[0]);
+	$user_language = strtolower(str_replace('_', '-', $buf[0]));
+	$user_keymap = $user_language;
+} else {
+	$user_language = 'en-us';
+	$user_keymap = $user_language;
+}

WebInterface/web/includes/core.inc.php
@@ -1,54 +1,0 @@
-<?php
-/**
- * Copyright (C) 2010 Ulteo SAS
- * http://www.ulteo.com
- * Author Jeremy DESVAGES <jeremy@ulteo.com>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; version 2
- * of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- **/
-
-define('WEBINTERFACE_ROOT', realpath(dirname(__FILE__).'/..'));
-
-$buf = @ini_get('include_path');
-@ini_set('include_path', $buf.':'.WEBINTERFACE_ROOT.'/PEAR');
-
-define('WEBINTERFACE_CONF_FILE', '/etc/ulteo/webinterface/config.inc.php');
-
-if (file_exists(WEBINTERFACE_CONF_FILE))
-	include_once(WEBINTERFACE_CONF_FILE);
-
-require_once(dirname(__FILE__).'/functions.inc.php');
-
-@session_start();
-
-$sessionmanager_url = NULL;
-if (defined('SESSIONMANAGER_HOST'))
-	$sessionmanager_url = 'https://'.SESSIONMANAGER_HOST.'/ovd/client/';
-elseif (array_key_exists('webinterface', $_SESSION) && array_key_exists('sessionmanager_url', $_SESSION['webinterface']))
-	$sessionmanager_url = $_SESSION['webinterface']['sessionmanager_url'];
-
-$debug_mode = false;
-if (defined('DEBUG_MODE') && DEBUG_MODE == 1)
-	$debug_mode = true;
-
-if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
-	$buf = explode(',', strtolower($_SERVER['HTTP_ACCEPT_LANGUAGE']));
-	$buf = explode(';', $buf[0]);
-	$user_language = strtolower(str_replace('_', '-', $buf[0]));
-	$user_keymap = $user_language;
-} else {
-	$user_language = 'en-us';
-	$user_keymap = $user_language;
-}

commit e8d13d12e58754441c6cda6e1246c9bffbf4fe1d

Bugfix: wrong $_SESSION key name (web-client => ovd-client), introduced by julien in r4644
git-svn-id: file:///home/code/svn/ovd/trunk@5400 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

client/web/web/includes/core.inc.php
@@ -38,8 +38,8 @@ require_once(dirname(__FILE__).'/functions.inc.php');
 $sessionmanager_url = NULL;
 if (defined('SESSIONMANAGER_HOST'))
 	$sessionmanager_url = 'https://'.SESSIONMANAGER_HOST.'/ovd/client/';
-elseif (array_key_exists('web-client', $_SESSION) && array_key_exists('sessionmanager_url', $_SESSION['web-client']))
-	$sessionmanager_url = $_SESSION['web-client']['sessionmanager_url'];
+elseif (array_key_exists('ovd-client', $_SESSION) && array_key_exists('sessionmanager_url', $_SESSION['ovd-client']))
+	$sessionmanager_url = $_SESSION['ovd-client']['sessionmanager_url'];
 
 $debug_mode = false;
 if (defined('DEBUG_MODE') && DEBUG_MODE == 1)