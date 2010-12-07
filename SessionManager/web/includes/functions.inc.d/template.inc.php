60de82a9476d48b78660153a8c09f55f3e224961
jeremy <jeremy@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-12-07 13:51:25 +0000

    Improvement: die_error() logs should be CRITICAL-leveled
    git-svn-id: file:///home/code/svn/ovd/trunk@5482 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/includes/functions.inc.d/template.inc.php
+++ SessionManager/web/includes/functions.inc.d/template.inc.php
@@ -38,7 +38,7 @@ function die_error($error_=false, $file_=NULL, $line_=NULL, $display_=false) {
 
 	$file_ = substr(str_replace(SESSIONMANAGER_ROOT, '', $file_), 1);
 
-	Logger::error('main', 'die_error() called with message \''.$error_.'\' in '.$file_.':'.$line_);
+	Logger::critical('main', 'die_error() called with message \''.$error_.'\' in '.$file_.':'.$line_);
 
 	header('Content-Type: text/xml; charset=utf-8');
 
