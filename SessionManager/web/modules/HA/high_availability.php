d41a46406af58574cc3df23699d21d72ee2b1f6d
laurent <laurent@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2010-12-08 09:46:38 +0000

    Typo: add license, copyright and author
    git-svn-id: file:///home/code/svn/ovd/trunk@5489 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/modules/HA/high_availability.php
+++ SessionManager/web/modules/HA/high_availability.php
@@ -1,4 +1,24 @@
 <?php
+/**
+ * Copyright (C) 2010 Ulteo SAS
+ * http://www.ulteo.com
+ * Author Arnaud LEGRAND <arnaud@ulteo.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ **/
+
 require_once(dirname(__FILE__).'/../../includes/core.inc.php');
 class HA_high_availability  extends HA {
 	public function __construct () {}
