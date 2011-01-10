c40b41543702b8cd81e0e6ca1d211fc090fa81b2
jeremy <jeremy@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2011-01-10 13:34:18 +0000

    Improvement: adding "daemon_external" for external Session mode
    git-svn-id: file:///home/code/svn/ovd/trunk@5776 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

---
+++ client/web/web/media/script/daemon_external.js
@@ -0,0 +1,268 @@
+/**
+ * Copyright (C) 2009-2010 Ulteo SAS
+ * http://www.ulteo.com
+ * Author Jeremy DESVAGES <jeremy@ulteo.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ **/
+
+var External = Class.create(Daemon, {
+	mode: 'applications',
+
+	applications: new Hash(),
+	applicationsPanel: null,
+	running_applications: new Hash(),
+	runningApplicationsPanel: null,
+
+	liaison_runningapplicationtoken_application: new Hash(),
+
+	initialize: function(applet_version_, applet_main_class_, debug_) {
+		Daemon.prototype.initialize.apply(this, [applet_version_, applet_main_class_, debug_]);
+
+		$('applicationsAppletContainer').innerHTML = '';
+
+		this.applicationsPanel = new ApplicationsPanel($('appsContainer'));
+		this.runningApplicationsPanel = new ApplicationsPanel($('runningAppsContainer'));
+	},
+
+	connect_servers: function() {
+		this.push_log('debug', '[external] connect_servers()');
+
+		if (! $('ulteoapplet') || ! $('ulteoapplet').isActive()) {
+			this.push_log('warning', '[external] connect_servers() - Applet is not ready');
+			setTimeout(this.connect_servers.bind(this), 1000);
+			return;
+		}
+
+		var servers = this.servers.values();
+		for (var i=0; i < servers.length; i++) {
+			if (servers[i].token != null)
+				this.push_log('info', '[external] connect_servers() - Connecting to server "'+servers[i].id+'"');
+			else
+				this.push_log('info', '[external] connect_servers() - Connecting to server "'+servers[i].fqdn+'"');
+			servers[i].connect();
+		}
+
+		setTimeout(this.explorer_loop.bind(this), 5000);
+
+		return true;
+	},
+
+	do_started: function() {
+		this.push_log('debug', '[external] do_started()');
+
+		Daemon.prototype.do_started.apply(this);
+
+		setTimeout(this.connect_servers.bind(this), 1000);
+	},
+
+	parse_do_started: function(transport) {
+		this.push_log('debug', '[external] parse_do_started(transport@do_started())');
+
+		var applet_params = new Hash();
+		applet_params.set('keymap', this.keymap);
+		applet_params.set('multimedia', this.multimedia);
+		applet_params.set('redirect_client_printers', this.redirect_client_printers);
+		applet_params.set('redirect_client_drives', this.redirect_client_drives);
+
+		var applet = buildAppletNode('ulteoapplet', this.applet_main_class, 'log4j-1.2.jar,'+this.applet_version, applet_params);
+		$('applicationsAppletContainer').show();
+		$('applicationsAppletContainer').appendChild(applet);
+
+		return true;
+	},
+
+	parse_list_servers: function(transport) {
+		this.push_log('debug', '[external] parse_list_servers(transport@list_servers())');
+
+		var xml = transport.responseXML;
+
+		var sessionNode = xml.getElementsByTagName('session');
+
+		if (sessionNode.length != 1) {
+			this.push_log('error', '[external] parse_list_servers(transport@list_servers()) - Invalid XML (No "session" node)');
+			return;
+		}
+
+		var serverNodes = xml.getElementsByTagName('server');
+
+		for (var i=0; i<serverNodes.length; i++) {
+			try { // IE does not have hasAttribute in DOM API...
+				var mode_gateway = false;
+				var port = 3389;
+				try {
+					var token = serverNodes[i].getAttribute('token');
+					if (token == null)
+						go_to_the_catch_please(); //call a function which does not exist to throw an exception and go to the catch()
+
+					mode_gateway = true;
+					port = 443;
+				} catch(e) {}
+
+				var server = new Server(i, i, serverNodes[i].getAttribute('fqdn'), port, serverNodes[i].getAttribute('login'), serverNodes[i].getAttribute('password'));
+				if (mode_gateway)
+					server.setToken(serverNodes[i].getAttribute('token'));
+
+				if (mode_gateway)
+					this.push_log('info', '[external] parse_list_servers(transport@list_servers()) - Adding server "'+server.id+'" to servers list');
+				else
+					this.push_log('info', '[external] parse_list_servers(transport@list_servers()) - Adding server "'+server.fqdn+'" to servers list');
+				this.servers.set(server.id, server);
+				this.liaison_server_applications.set(server.id, new Array());
+
+				var applicationNodes = serverNodes[i].getElementsByTagName('application');
+
+				for (var j=0; j<applicationNodes.length; j++) {
+					try { // IE does not have hasAttribute in DOM API...
+						this.push_log('info', '[external] parse_list_servers(transport@list_servers()) - Adding application "'+applicationNodes[j].getAttribute('id')+'" to applications list');
+
+						if (typeof this.liaison_server_applications.get(server.id) == 'undefined')
+							continue;
+
+						var application = new Application(applicationNodes[j].getAttribute('id'), applicationNodes[j].getAttribute('name'), server.id);
+						this.applications.set(application.id, application);
+						this.applicationsPanel.add(application);
+						this.liaison_server_applications.get(server.id).push(application.id);
+					} catch(e) {
+						this.push_log('error', '[external] parse_list_servers(transport@list_servers()) - Invalid XML (Missing argument for "application" node '+j+')');
+						return;
+					}
+				}
+			} catch(e) {
+				this.push_log('error', '[external] parse_list_servers(transport@list_servers()) - Invalid XML (Missing argument for "server" node '+i+')');
+				return;
+			}
+		}
+
+		this.ready = true;
+	},
+
+	list_running_apps: function(applicationsNode_) {
+		this.push_log('debug', '[external] list_running_apps(xml@applicationsNode)');
+
+		var runningApplicationsNodes = applicationsNode_.getElementsByTagName('running');
+
+		var apps_in_xml = new Array();
+
+		for (var i=0; i < runningApplicationsNodes.length; i++) {
+			var pid = runningApplicationsNodes[i].getAttribute('job');
+			var app_status = parseInt(runningApplicationsNodes[i].getAttribute('status'));
+
+			if (typeof this.running_applications.get(pid) == 'undefined') {
+				var app_id = runningApplicationsNodes[i].getAttribute('app_id');
+
+				var app_object = this.applications.get(app_id);
+				if (typeof app_object == 'undefined')
+					continue;
+
+				var instance = new Running_Application(app_object.id, app_object.name, app_object.server, pid, app_status, this.getContext());
+				this.running_applications.set(instance.pid, instance);
+			} else {
+				var instance = this.running_applications.get(pid);
+				instance.update(app_status);
+			}
+
+			apps_in_xml.push(pid);
+		}
+	},
+
+	applicationStatus: function(token_, status_) {
+		this.push_log('debug', '[external] applicationStatus(token: '+token_+', status: '+status_+')');
+
+		var app_status = 2;
+
+		if (typeof this.running_applications.get(token_) == 'undefined') {
+			this.push_log('info', '[external] applicationStatus(token: '+token_+', status: '+status_+') - Creating "running" application "'+token_+'"');
+
+			var app_id = this.liaison_runningapplicationtoken_application.get(token_);
+			if (typeof app_id == 'undefined')
+				return false;
+
+			var app_object = this.applications.get(app_id);
+			if (typeof app_object == 'undefined') {
+				this.push_log('error', '[external] applicationStatus(token: '+token_+', status: '+status_+') - Application "'+app_id+'" does not exist');
+				return false;
+			}
+
+			var instance = new Running_Application(app_object.id, app_object.name, app_object.server, token_, app_status, this.getContext());
+			this.running_applications.set(instance.pid, instance);
+
+			if (status_ == 'started')
+				this.push_log('info', '[external] applicationStatus(token: '+token_+', status: '+status_+') - Adding "running" application "'+token_+'" to running applications list');
+		} else {
+			this.push_log('info', '[external] applicationStatus(token: '+token_+', status: '+status_+') - Updating "running" application "'+token_+'" status: "'+app_status+'"');
+
+			var instance = this.running_applications.get(token_);
+			instance.update(app_status);
+
+			if (status_ == 'stopped') {
+				this.push_log('info', '[external] applicationStatus(token: '+token_+', status: '+status_+') - Deleting "running" application "'+token_+'" from running applications list');
+
+				var app_id = this.liaison_runningapplicationtoken_application.get(token_);
+				if (typeof app_id == 'undefined')
+					return false;
+			}
+		}
+
+		return true;
+	},
+
+	explorer_loop: function() {
+		this.push_log('debug', '[external] explorer_loop()');
+
+		this.check_start_app();
+
+		if (! this.is_stopped())
+			setTimeout(this.explorer_loop.bind(this), 2000);
+	},
+
+	check_start_app: function() {
+		this.push_log('debug', '[external] check_start_app()');
+
+		new Ajax.Request(
+			'start_app.php',
+			{
+				method: 'get',
+				parameters: {
+					check: true,
+					differentiator: Math.floor(Math.random()*50000)
+				},
+				onSuccess: this.parse_check_start_app.bind(this)
+			}
+		);
+	},
+
+	parse_check_start_app: function(transport) {
+		this.push_log('error', '[external] parse_check_start_app(transport@check_start_app())');//debug
+
+		var xml = transport.responseXML;
+
+		var buffer = xml.getElementsByTagName('start_app');
+
+		if (buffer.length == 0)
+			return;
+
+		for (var i=0; i<buffer.length; i++) {
+			var application = this.applications.get(parseInt(buffer[i].getAttribute('id')));
+			application.launch();
+		}
+	}
+});
+
+function applicationStatus(token_, status_) {
+	daemon.push_log('debug', '[proxy] applicationStatus(token: '+token_+', status: '+status_+')');
+
+	return daemon.applicationStatus(token_, status_);
+}
