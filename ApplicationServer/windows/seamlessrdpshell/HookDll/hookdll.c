d23b2aa1924f9adfdf4a86cb1c87afcaf288672e
thomas <thomas@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2011-01-18 09:23:40 +0000

    Bugfix: Windows applications menus were closed automatically
    git-svn-id: file:///home/code/svn/ovd/trunk@5826 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- ApplicationServer/windows/seamlessrdpshell/HookDll/hookdll.c
+++ ApplicationServer/windows/seamlessrdpshell/HookDll/hookdll.c
@@ -797,7 +797,7 @@ wndprocret_hook_proc(int code, WPARAM cur_thread, LPARAM details)
 	{
 		// GetForegroundWindow() return the parent window of the foreground/focused window
 		HWND foregroundWnd = GetForegroundWindow();
-		if ((foregroundWnd != hwnd) && (hwnd != get_parent(foregroundWnd)))
+		if ((foregroundWnd != hwnd) && (hwnd != get_parent(foregroundWnd)) && (foregroundWnd != get_parent(hwnd)))
 			SetForegroundWindow(hwnd);
 
 		vchannel_write("ACK", "%u", g_blocked_focus_serial);
