54c70b7029fb68f2816465fddaed13951558dff7
samuel <samuel@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2011-03-14 17:15:11 +0000

    Bugfix: fix a regression inserted by the commit 6242: do not test the geometry option content anymore
    git-svn-id: file:///home/code/svn/ovd/trunk@6381 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- client/java/src/org/ulteo/ovd/client/NativeClient.java
+++ client/java/src/org/ulteo/ovd/client/NativeClient.java
@@ -279,6 +279,11 @@ public class NativeClient implements ActionListener, Runnable, org.ulteo.ovd.sm.
 			NativeClient.usage(RETURN_CODE_BAD_ARGUMENTS);
 		}
 
+		if ((opts.sessionMode == Properties.MODE_REMOTEAPPS) && (opts.mask & Options.FLAG_GEOMETRY) != 0) {
+			org.ulteo.Logger.error("You cannot use -g in applications mode");
+			NativeClient.usage(RETURN_CODE_BAD_ARGUMENTS);
+		}
+
 		if ((opts.mask & Options.FLAG_PROFILE_INI) != 0) {
 			if (! opts.getIniProfile(opts.profile))
 				org.ulteo.Logger.warn("The configuration file \""+opts.profile+"\" does not exist.");
@@ -298,10 +303,6 @@ public class NativeClient implements ActionListener, Runnable, org.ulteo.ovd.sm.
 		}
 		if (opts.sessionMode == Properties.MODE_DESKTOP && opts.autopublish) {
 			org.ulteo.Logger.error("You cannot use --auto-integration in desktop mode");
-			NativeClient.usage(RETURN_CODE_BAD_ARGUMENTS);
-		}
-		if (opts.sessionMode == Properties.MODE_REMOTEAPPS && opts.geometry != null) {
-			org.ulteo.Logger.error("You cannot use -g in applications mode");
 			NativeClient.usage(RETURN_CODE_BAD_ARGUMENTS);
 		}
 		if (opts.autostart) {
