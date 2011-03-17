ulteo/ovd

commit ffa31e027a83c21fd02a0c16873cd06525dca6d4

Code cleaning
git-svn-id: file:///home/code/svn/ovd/trunk@6431 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

SessionManager/web/client/start.php
@@ -203,12 +203,13 @@ if (isset($old_session_id)) {
 	$random_server = false;
 	if ($session_mode == Session::MODE_DESKTOP && (isset($remote_desktop_settings) && array_key_exists('desktop_type', $remote_desktop_settings))) {
 		$random_server = $sessionManagement->getDesktopServer($remote_desktop_settings['desktop_type']);
-		if (! $random_server) {
-			Logger::error('main', '(client/start) No desktop server found for User "'.$user->getAttribute('login').'", aborting');
-			throw_response(SERVICE_NOT_AVAILABLE);
-		}
-	} else
+	else
 		$random_server = array_rand($servers[Server::SERVER_ROLE_APS]);
+
+	if (! $random_server) {
+		Logger::error('main', '(client/start) No desktop server found for User "'.$user->getAttribute('login').'", aborting');
+		throw_response(SERVICE_NOT_AVAILABLE);
+	}
 
 	if (get_class($sessionManagement) == 'SessionManagement_internal' && isset($enable_profiles) && $enable_profiles == 1) {
 		$fileservers = Abstract_Server::load_available_by_role(Server::SERVER_ROLE_FS);

commit 537118f3e85d05c4cc5ca811a5ec043377526f14

Bugfix: typo (introduced by r6431)
git-svn-id: file:///home/code/svn/ovd/trunk@6432 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

SessionManager/web/client/start.php
@@ -201,7 +201,7 @@ if (isset($old_session_id)) {
 	$servers = $sessionManagement->servers;
 
 	$random_server = false;
-	if ($session_mode == Session::MODE_DESKTOP && (isset($remote_desktop_settings) && array_key_exists('desktop_type', $remote_desktop_settings))) {
+	if ($session_mode == Session::MODE_DESKTOP && (isset($remote_desktop_settings) && array_key_exists('desktop_type', $remote_desktop_settings)))
 		$random_server = $sessionManagement->getDesktopServer($remote_desktop_settings['desktop_type']);
 	else
 		$random_server = array_rand($servers[Server::SERVER_ROLE_APS]);