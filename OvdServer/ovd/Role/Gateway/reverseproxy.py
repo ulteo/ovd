526e015689ea56d287289c98a3e26bf0777f8441
julien <julien@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2011-04-05 14:34:53 +0000

    Bugfix: freeze when accept new SSL connexion with no data to read (openssl s_client host:port)
    git-svn-id: file:///home/code/svn/ovd/trunk@6662 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- OvdServer/ovd/Role/Gateway/reverseproxy.py
+++ OvdServer/ovd/Role/Gateway/reverseproxy.py
@@ -5,6 +5,7 @@
 # Author Laurent CLOUET <laurent@ulteo.com> 2010-2011
 # Author Arnaud Legrand <arnaud@ulteo.com> 2010
 # Author Samuel BOVEE <samuel@ulteo.com> 2010-2011
+# Author Julien LANGLOIS <julien@ulteo.com> 2011
 #
 # This program is free software; you can redistribute it and/or 
 # modify it under the terms of the GNU General Public License
@@ -61,24 +62,44 @@ class ReverseProxy(asyncore.dispatcher):
 
 	def handle_accept(self):
 		conn, peer = self.accept()
-		addr, port = peer
+		Logger.debug3("ReverseProxy: New connection => %s"%(str(peer)))
+		
+		ProtocolDetectDispatcher(conn, peer, self)
 
-		r = None
-		while r is None:
-			try:
-				r = conn.recv(4096)
-			except (SSL.SysCallError, SSL.ZeroReturnError):
-				conn.close()
-				return
-			except SSL.WantReadError:
-				time.sleep(0.01)
 
+class ProtocolDetectDispatcher(asyncore.dispatcher):
+	def __init__(self, conn, peer, proxyInstance):
+		asyncore.dispatcher.__init__(self, conn)
+		self.peer = peer
+		self.proxyInstance = proxyInstance
+	
+	def writable(self):
+		return False
+		# This class doesn't have to write anything,
+		# It's just use to detect the protocol
+	
+	def handle_read(self):
+		Logger.debug3("ProtocolDetectDispatcher::handle_read on %s"%(repr(self)))
+		try:
+			r = self.recv(4096)
+			while self.socket.pending() > 0:
+				r+= self.recv(4096)
+		
+		except SSL.SysCallError:
+			Logger.debug3("ProtocolDetectDispatcher::handle_read SSL.SysCallError")
+			self.handle_close()
+		except SSL.ZeroReturnError:
+			Logger.debug3("ProtocolDetectDispatcher::handle_read SSL.ZeroReturnError")
+			self.close()
+		except SSL.WantReadError:
+			return
+		
 		request = r.split('\n', 1)[0]
 		utf8_request = request.rstrip('\n\r').decode("utf-8", "replace")
 
 		# find protocol
-		rdp  = self.rdp_ptn.match(request)
-		http = self.http_ptn.match(request)
+		rdp  = self.proxyInstance.rdp_ptn.match(request)
+		http = self.proxyInstance.http_ptn.match(request)
 
 		try:
 			# RDP case
@@ -87,26 +108,27 @@ class ReverseProxy(asyncore.dispatcher):
 				fqdn = digestToken(token)
 				if not fqdn:
 					raise Exception('token authorization failed for: ' + token)
-				sender((fqdn, self.rdp_port), receiver(conn, r))
+				sender((fqdn, self.proxyInstance.rdp_port), receiver(self.socket, r))
+				
 
 			# HTTP case
 			elif http:
-				Logger.debug("Gateway:: request: http %s (%s,%d)" % (utf8_request, addr, port))
+				Logger.debug("ProtocolDetectDispatcher:: request: http %s %s" % (utf8_request, str(self.peer)))
 				path = http.group(2)
 
 				if not (path == '/ovd' or path.startswith("/ovd/")):
 					raise Exception('wrong HTTP path: ' + path)
 
 				if path == "/ovd/client/start.php":
-					rec = receiverXMLRewriter(conn, r)
+					rec = receiverXMLRewriter(self.socket, r)
 				else:
-					rec = receiver(conn, r)
-				senderHTTP(self.sm, rec, self.ssl_ctx)
+					rec = receiver(self.socket, r)
+				senderHTTP(self.proxyInstance.sm, rec, self.proxyInstance.ssl_ctx)
 
 			# protocol error
 			else:
 				raise Exception('bad first request line: ' + request)
 
 		except Exception, err:
-			Logger.debug("ReverseProxy::handle_accept error %s %s"%(type(err), err))
-			conn.close()
+			Logger.debug("ProtocolDetectDispatcher::handle_read error %s %s"%(type(err), err))
+			self.handle_close()
