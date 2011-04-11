14627f86ffbce031ebc3c242a25ed02167c0da38
laurent <laurent@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2011-04-11 13:30:00 +0000

    Typo: author
    git-svn-id: file:///home/code/svn/ovd/trunk@6692 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- SessionManager/web/modules/SessionManagement.php
+++ SessionManager/web/modules/SessionManagement.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * Copyright (C) 2010 Ulteo SAS
+ * Copyright (C) 2010-2011 Ulteo SAS
  * http://www.ulteo.com
- * Author Jeremy DESVAGES <jeremy@ulteo.com>
+ * Author Laurent CLOUET <laurent@ulteo.com> 2011
+ * Author Jeremy DESVAGES <jeremy@ulteo.com> 2010-2011
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
