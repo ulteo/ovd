a01927508e4f53402d41329d643b16977111a1cb
david <david@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2011-04-21 10:15:57 +0000

    Bugfix: compilation failed with ovd packaging
    git-svn-id: file:///home/code/svn/ovd/trunk@6750 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- ApplicationServer/windows/WebdavFS/make64.bat
+++ ApplicationServer/windows/WebdavFS/make64.bat
@@ -4,15 +4,5 @@ call setenv.bat %WINDDK_PATH%  x64 fre WNET no_oacr
 
 cd /D "%INITIAL_DIR%"
 
-cd dokan
 build
-cd ..
 
-
-cd dokan_np
-build
-cd ..
-
-cd dokan_webdav
-build
-cd ..
--- ApplicationServer/windows/WebdavFS/make86.bat
+++ ApplicationServer/windows/WebdavFS/make86.bat
@@ -4,15 +4,5 @@ call setenv.bat %WINDDK_PATH%  x86 fre WXP no_oacr
 
 cd /D "%INITIAL_DIR%"
 
-cd dokan
 build
-cd ..
 
-
-cd dokan_np
-build
-cd ..
-
-cd dokan_webdav
-build
-cd ..
