af55e4bdfc49aff6d62bd7c16121a6a43bbabc76
julien <julien@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2011-05-02 14:46:08 +0000

    Bugfix: if init_role return False, the role cannot continue, creating the folder won't fix the problem ...
    git-svn-id: file:///home/code/svn/ovd/trunk@6837 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- OvdServer/ovd/Role/FileServer/Role.py
+++ OvdServer/ovd/Role/FileServer/Role.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2009-2010 Ulteo SAS
+# Copyright (C) 2009-2011 Ulteo SAS
 # http://www.ulteo.com
-# Author Julien LANGLOIS <julien@ulteo.com> 2009, 2010
+# Author Julien LANGLOIS <julien@ulteo.com> 2009, 2010, 2011
 # Author Samuel BOVEE <samuel@ulteo> 2011
 #
 # This program is free software; you can redistribute it and/or 
@@ -49,8 +49,7 @@ class Role(AbstractRole):
 		Logger.info("FileServer init")
 		
 		if not Config.init_role():
-			Logger.info("FileServer never initialized, creating repository on '%s'"%(Config.spool))
-			os.makedirs(Config.spool)
+			return False
 		
 		if not self.cleanup_samba():
 			Logger.error("FileServer: unable to cleanup samba")
