ulteo/ovd

commit 64065ebe27e987cdf71de0241ca253318fa11804

Feature: add an option to clean_dump_archive or not (default is True)
git-svn-id: file:///home/code/svn/ovd/trunk@6852 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

OvdServer/ovd/Role/ApplicationServer/Config.py
@@ -23,11 +23,15 @@ from ovd.Logger import Logger
 class Config:
 	general = None
 	multithread = False
+	clean_dump_archive = True
 	
 	
 	@staticmethod
 	def init(infos):
 		if infos.has_key("multithread"):
 			Config.multithread = (infos["multithread"].lower() == True)
+		
+		if infos.has_key("clean_dump_archive"):
+			Config.clean_dump_archive = (infos["clean_dump_archive"].lower() == True)
 		
 		return True

OvdServer/ovd/Role/ApplicationServer/Role.py
@@ -95,7 +95,8 @@ class Role(AbstractRole):
 			Logger.error("Unable to purge group")
 			return False
 		
-		self.purgeArchives()
+		if Config.clean_dump_archive:
+			self.purgeArchives()
 		
 		if Config.multithread:
 			cpuInfos = Platform.System.getCPUInfos()

commit 0cce86e3cfbaf9f88064300dc46d97cc81bebd55

Bugfix: the config file is not properly parsed for APS part (caused by the rev r6852, r6851)
git-svn-id: file:///home/code/svn/ovd/trunk@7546 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

OvdServer/examples/slaveserver.conf
@@ -24,7 +24,7 @@ level = error warn info debug
 [ApplicationServer]
 # multithread = 
 # use thread system instead of multi processing
-# default is 0
+# default is true
 
 
 [Gateway]

OvdServer/ovd/Role/ApplicationServer/Config.py
@@ -29,9 +29,9 @@ class Config:
 	@staticmethod
 	def init(infos):
 		if infos.has_key("multithread"):
-			Config.multithread = (infos["multithread"].lower() == True)
+			Config.multithread = (infos["multithread"].lower() == "true")
 		
 		if infos.has_key("clean_dump_archive"):
-			Config.clean_dump_archive = (infos["clean_dump_archive"].lower() == True)
+			Config.clean_dump_archive = (infos["clean_dump_archive"].lower() == "true")
 		
 		return True