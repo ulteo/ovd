b1093a0d6373b1ea98cd25f0cc94c136c4178824
samuel <samuel@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2011-10-17 10:18:18 +0000

    Backport: fix incomplete backport r6475 from trunk r6303
    git-svn-id: file:///home/code/svn/ovd/branches/3.0@7585 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- client/web/config/config.inc.php
+++ client/web/config/config.inc.php
@@ -1,6 +1,6 @@
 <?php
 // IP/Host of the SessionManager to link the Web Client to
-define('SESSIONMANAGER_HOST', '127.0.0.1');
+// define('SESSIONMANAGER_HOST', '127.0.0.1');
 
 // Enable/disable debug mode
 define('DEBUG_MODE', 0);
