8f8a8ee4508fae5b70160ba976d8a0f103f29724
julien <julien@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2011-11-03 09:10:16 +0000

    Bugfix #300: Aajaxplorer was unable to show image thumb when having space character (' ') in filename
    git-svn-id: file:///home/code/svn/ovd/trunk@7671 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- client/web/ajaxplorer/plugins/access.fs/class.fsAccessDriver.php
+++ client/web/ajaxplorer/plugins/access.fs/class.fsAccessDriver.php
@@ -33,6 +33,25 @@
  * 
  * Description : The most used and standard plugin : FileSystem access
  */
+/** 
+ * Copyright (C) 2011 Ulteo SAS
+ * http://www.ulteo.com
+ * Author Julien LANGLOIS <julien@ulteo.com> 2011
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ **/
 require_once(dirname(__FILE__).'/../../../includes/core.inc.php');
 require "HTTP/WebDAV/Client.php";
 
@@ -203,7 +222,7 @@ class fsAccessDriver extends AbstractAccessDriver
 						}
 					}
 					
-					$this->readFile($this->getPath()."/".SystemTextEncoding::fromUTF8($file), "image");
+					$this->readFile($this->getPath().SystemTextEncoding::fromUTF8($this->encodePath($file)), "image");
 				}
 				exit(0);
 			break;
