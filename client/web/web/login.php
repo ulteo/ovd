ulteo/ovd

commit 16c5ca62185180d9cb117c89302880cf5627e509

Improvement: WebClient now automagically handles the presence of an OVD Gateway, no more conf required
git-svn-id: file:///home/code/svn/ovd/trunk@7487 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

client/web/web/login.php
@@ -194,17 +194,24 @@ $dom->appendChild($session_node);
 $_SESSION['ovd-client']['to_SM_start_XML'] = $dom->saveXML();
 
 if (defined('SESSIONMANAGER_HOST')) {
-	if (defined('GATEWAY_WAN_IP') && defined('GATEWAY_WAN_PORT') && defined('GATEWAY_LAN_IP') && defined('GATEWAY_LAN_PORT') && $_SERVER['REMOTE_ADDR'] == GATEWAY_LAN_IP) {
-		$_SESSION['ovd-client']['server'] = GATEWAY_LAN_IP.':'.GATEWAY_LAN_PORT;
-		$_SESSION['ovd-client']['sessionmanager_host'] = GATEWAY_WAN_IP.':'.GATEWAY_WAN_PORT;
-	} else {
-		$_SESSION['ovd-client']['server'] = SESSIONMANAGER_HOST;
-		$_SESSION['ovd-client']['sessionmanager_host'] = SESSIONMANAGER_HOST;
-	}
+	$_SESSION['ovd-client']['server'] = SESSIONMANAGER_HOST;
+	$_SESSION['ovd-client']['sessionmanager_host'] = SESSIONMANAGER_HOST;
 } else {
 	$_SESSION['ovd-client']['server'] = $_POST['sessionmanager_host'];
 	$_SESSION['ovd-client']['sessionmanager_host'] = $_POST['sessionmanager_host'];
 }
+
+$_SESSION['ovd-client']['gateway'] = false;
+$_SESSION['ovd-client']['gateway_first'] = true;
+$headers = apache_request_headers();
+if (is_array($headers) && array_key_exists('OVD-Gateway', $headers)) {
+	$_SESSION['ovd-client']['gateway'] = true;
+	$_SESSION['ovd-client']['gateway_first'] = true;
+
+	$_SESSION['ovd-client']['server'] = $_SERVER['REMOTE_ADDR'].':'.$_POST['requested_port'];
+	$_SESSION['ovd-client']['sessionmanager_host'] = $_SERVER['REMOTE_ADDR'].':'.$_POST['requested_port'];
+}
+
 $_SESSION['ovd-client']['sessionmanager_url'] = 'https://'.$_SESSION['ovd-client']['server'].'/ovd/client';
 $sessionmanager_url = $_SESSION['ovd-client']['sessionmanager_url'];
 
@@ -282,10 +289,9 @@ if ($_SESSION['ovd-client']['session_mode'] == 'desktop')
 	$_SESSION['ovd-client']['desktop_fullscreen'] = ((array_key_exists('desktop_fullscreen', $_POST))?$_POST['desktop_fullscreen']:0);
 $_SESSION['ovd-client']['timeout'] = $session_node->getAttribute('timeout');
 
-$_SESSION['ovd-client']['gateway'] = false;
-if ($session_node->hasAttribute('mode_gateway')) {
-	if ($session_node->getAttribute('mode_gateway') == 'on')
-		$_SESSION['ovd-client']['gateway'] = true;
+if ($session_node->hasAttribute('mode_gateway') && $session_node->getAttribute('mode_gateway') == 'on') {
+	$_SESSION['ovd-client']['gateway'] = true;
+	$_SESSION['ovd-client']['gateway_first'] = false;
 }
 
 $user_node = $session_node->getElementsByTagName('user');

client/web/config/config.inc.php
@@ -2,14 +2,6 @@
 // IP/Host of the SessionManager to link the Web Client to
 // define('SESSIONMANAGER_HOST', '127.0.0.1');
 
-// Public IP/Port of the Gateway (only use this if the WebClient is INSIDE a LAN and BEHIND the Gateway)
-// define('GATEWAY_WAN_IP', '8.8.8.8');
-// define('GATEWAY_WAN_PORT', 443);
-
-// Private IP/Port of the Gateway (only use this if the WebClient is INSIDE a LAN and BEHIND the Gateway)
-// define('GATEWAY_LAN_IP', '127.0.0.1');
-// define('GATEWAY_LAN_PORT', 443);
-
 // Enable/disable debug mode
 define('DEBUG_MODE', 0);
 

client/web/web/includes/core.inc.php
@@ -39,12 +39,9 @@ if (! array_key_exists('ovd-client', $_SESSION))
 	$_SESSION['ovd-client'] = array();
 
 $sessionmanager_url = NULL;
-if (defined('SESSIONMANAGER_HOST')) {
-	if (defined('GATEWAY_WAN_IP') && defined('GATEWAY_WAN_PORT') && defined('GATEWAY_LAN_IP') && defined('GATEWAY_LAN_PORT') && $_SERVER['REMOTE_ADDR'] == GATEWAY_LAN_IP)
-		$sessionmanager_url = 'https://'.GATEWAY_LAN_IP.':'.GATEWAY_LAN_PORT.'/ovd/client';
-	else
-		$sessionmanager_url = 'https://'.SESSIONMANAGER_HOST.'/ovd/client';
-} elseif (array_key_exists('sessionmanager_url', $_SESSION['ovd-client']))
+if (defined('SESSIONMANAGER_HOST'))
+	$sessionmanager_url = 'https://'.SESSIONMANAGER_HOST.'/ovd/client';
+elseif (array_key_exists('sessionmanager_url', $_SESSION['ovd-client']))
 	$sessionmanager_url = $_SESSION['ovd-client']['sessionmanager_url'];
 
 $debug_mode = false;

client/web/web/media/script/common.js
@@ -74,6 +74,8 @@ function startSession() {
 			{
 				method: 'post',
 				parameters: {
+					requested_host: window.location.hostname,
+					requested_port: ((window.location.port !=  '')?window.location.port:'443'),
 					sessionmanager_host: $('sessionmanager_host').value,
 					login: $('user_login').value,
 					password: $('user_password').value,

client/web/web/media/script/daemon.js
@@ -396,7 +396,11 @@ var Daemon = Class.create({
 		new Ajax.Request(
 			'servers.php',
 			{
-				method: 'get',
+				method: 'post',
+				parameters: {
+					requested_host: window.location.hostname,
+					requested_port: ((window.location.port !=  '')?window.location.port:'443')
+				},
 				onSuccess: this.parse_list_servers.bind(this)
 			}
 		);

client/web/web/servers.php
@@ -48,11 +48,11 @@ $server_nodes = $dom->getElementsByTagName('server');
 foreach ($server_nodes as $server_node) {
 	$port = 3389;
 
-	if (array_key_exists('gateway', $_SESSION['ovd-client']) && $_SESSION['ovd-client']['gateway'] === true) {
-		if (defined('GATEWAY_WAN_IP') && defined('GATEWAY_WAN_PORT') && defined('GATEWAY_LAN_IP') && defined('GATEWAY_LAN_PORT') && $_SERVER['REMOTE_ADDR'] == GATEWAY_LAN_IP)
-			$url = 'http://'.GATEWAY_WAN_IP.':'.GATEWAY_WAN_PORT;
-		else
-			$url = 'http://'.$_SESSION['ovd-client']['server'];
+	if (array_key_exists('gateway', $_SESSION['ovd-client']) && $_SESSION['ovd-client']['gateway'] === true && array_key_exists('gateway_first', $_SESSION['ovd-client'])) {
+		if ($_SESSION['ovd-client']['gateway_first'] === true)
+			$url = 'http://'.$_POST['requested_host'].':'.$_POST['requested_port'];
+		elseif ($_SESSION['ovd-client']['gateway_first'] === false)
+			$url = 'http://'.$_SESSION['ovd-client']['sessionmanager_host'];
 
 		$host = parse_url($url, PHP_URL_HOST);
 		$port = parse_url($url, PHP_URL_PORT);

commit 9438e0f80011e26c0d62f980851f406425dcd5c6

Bugfix: invlid SM host in gateway_first mode (usefull for local intgration) (regression from r7487)
git-svn-id: file:///home/code/svn/ovd/trunk@7746 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

client/web/web/login.php
@@ -230,7 +230,7 @@ if (is_array($headers) && array_key_exists('OVD-Gateway', $headers)) {
 	$_SESSION['ovd-client']['gateway_first'] = true;
 
 	$_SESSION['ovd-client']['server'] = $_SERVER['REMOTE_ADDR'].':'.$_POST['requested_port'];
-	$_SESSION['ovd-client']['sessionmanager_host'] = $_SERVER['REMOTE_ADDR'].':'.$_POST['requested_port'];
+	$_SESSION['ovd-client']['sessionmanager_host'] = $_POST['requested_host'].':'.$_POST['requested_port'];
 }
 
 $_SESSION['ovd-client']['sessionmanager_url'] = 'https://'.$_SESSION['ovd-client']['server'].'/ovd/client';