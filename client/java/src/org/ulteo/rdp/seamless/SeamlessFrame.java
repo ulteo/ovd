ulteo/ovd

commit d5cdbe07ac7a0f01eaa278c1d201a1ae0c52671e

Bugfix: disable IME (input method editor) when using chinese session
git-svn-id: file:///home/code/svn/ovd/trunk@7232 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

client/java/src/org/ulteo/rdp/seamless/SeamlessFrame.java
@@ -130,6 +130,8 @@ public class SeamlessFrame extends SeamFrame implements SeamlessMovingResizing, 
 	
 	@Override
 	public void focusGained(FocusEvent e) {
+		((sun.awt.im.InputContext)this.getInputContext()).disableNativeIM();
+
 		if (SeamlessFrame.focusManager != null)
 		{
 			SeamlessFrame.focusManager.performedFocusLost(this);

client/java/src/org/ulteo/rdp/seamless/SeamlessPopup.java
@@ -305,6 +305,8 @@ public class SeamlessPopup extends JDialog implements SeamlessWindow, SeamlessMo
 
 	@Override
 	public void focusGained(FocusEvent e) {	
+		((sun.awt.im.InputContext)this.getInputContext()).disableNativeIM();
+
 		if (SeamlessPopup.focusManager != null)	{
 			SeamlessPopup.focusManager.performedFocusLost(this);
 		}

client/java/src/net/propero/rdp/Input.java
@@ -296,6 +296,8 @@ public abstract class Input {
      * Check locking key states.
      */
     public void gainedFocus() {
+		((sun.awt.im.InputContext)canvas.getInputContext()).disableNativeIM();
+    	
 		doLockKeys(); // ensure lock key states are correct
 	}
 

commit 989a8d8521239f78c92a665d3f184fe3255499f0

Bugfix: do not disable IME on linux (caused by r7232) due to incomplete bugfix from r7328
git-svn-id: file:///home/code/svn/ovd/trunk@7815 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

client/java/src/org/ulteo/rdp/seamless/SeamlessFrame.java
@@ -28,6 +28,7 @@ import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.MouseEvent;
 
+import org.ulteo.ovd.integrated.OSTools;
 import org.ulteo.utils.AbstractFocusManager;
 
 import net.propero.rdp.Common;
@@ -129,7 +130,8 @@ public class SeamlessFrame extends SeamFrame implements SeamlessMovingResizing, 
 	
 	@Override
 	public void focusGained(FocusEvent e) {
-		((sun.awt.im.InputContext)this.getInputContext()).disableNativeIM();
+		if (OSTools.isWindows())
+			((sun.awt.im.InputContext)this.getInputContext()).disableNativeIM();
 
 		if (SeamlessFrame.focusManager != null)
 		{

client/java/src/org/ulteo/rdp/seamless/SeamlessPopup.java
@@ -36,6 +36,7 @@ import java.awt.event.WindowListener;
 import java.awt.event.WindowStateListener;
 import javax.swing.JDialog;
 
+import org.ulteo.ovd.integrated.OSTools;
 import org.ulteo.utils.AbstractFocusManager;
 
 import net.propero.rdp.Common;
@@ -290,7 +291,8 @@ public class SeamlessPopup extends JDialog implements SeamlessWindow, SeamlessMo
 
 	@Override
 	public void focusGained(FocusEvent e) {	
-		((sun.awt.im.InputContext)this.getInputContext()).disableNativeIM();
+		if (OSTools.isWindows())
+			((sun.awt.im.InputContext)this.getInputContext()).disableNativeIM();
 
 		if (SeamlessPopup.focusManager != null)	{
 			SeamlessPopup.focusManager.performedFocusLost(this);