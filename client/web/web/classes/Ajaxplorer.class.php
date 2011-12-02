7238b16bf2094c47bb599575eab9633da8664514
julien <julien@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2011-12-02 12:58:20 +0000

    Code cleaning: move all ajaplorer specific code into a ajaxplorer dedicated class
    git-svn-id: file:///home/code/svn/ovd/trunk@7836 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

---
+++ client/web/web/classes/Ajaxplorer.class.php
@@ -0,0 +1,205 @@
+<?php
+/**
+ * Copyright (C) 2010-2011 Ulteo SAS
+ * http://www.ulteo.com
+ * Author Jeremy DESVAGES <jeremy@ulteo.com> 2010
+ * Author Julien LANGLOIS <julien@ulteo.com> 2011 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ **/
+
+
+class Ajaxplorer {
+	private $session_node;
+	private $pr_nodes;
+	private $sf_nodes;
+	
+	
+	public function __construct($session_node_) {
+		$this->session_node = $session_node_;
+		$this->pr_nodes = $session_node_->getElementsByTagName('profile');
+		$this->sf_nodes = $session_node_->getElementsByTagName('sharedfolder');
+	}
+	
+	
+	public function can_run() {
+		return (is_dir(WEB_CLIENT_ROOT.'/ajaxplorer/'));
+	}
+	
+	
+	public function is_required() {
+		return ($this->pr_nodes->length > 0 || $this->sf_nodes->length > 0);
+	}
+	
+	
+	public function build_data() {
+		$ret = array();
+		$ret['repositories'] = array();
+		$ret['folders'] = array();
+		$ret['applications'] = $this->generateActionsXML($this->session_node->getElementsByTagName('application'));
+		
+		$profile_node = $this->pr_nodes->item(0);
+		if (is_object($profile_node)) {
+			$ret['repositories'][] = $this->generateRepo(_('Profile'), $profile_node);
+			$ret['folders'][] = $profile_node->getAttribute('dir');
+		}
+		
+		foreach ($this->sf_nodes as $sharedfolder_node) {
+			$ret['repositories'][] = $this->generateRepo($sharedfolder_node->getAttribute('name'), $sharedfolder_node);
+			$ret['folders'][] = $sharedfolder_node->getAttribute('dir');
+		}
+		
+		return $ret;
+	}
+	
+	
+	private static function generateRepo($name_, $node_) {
+		return array(
+			'DISPLAY'		=>	$name_,
+			'DRIVER'		=>	'fs',
+			'DRIVER_OPTIONS'	=>	array(
+				'PATH'			=>	'webdav://'.$node_->getAttribute('login').':'.$node_->getAttribute('password').'@'.$node_->getAttribute('server').':1113/ovd/fs/'.$node_->getAttribute('dir').'/',
+				'CREATE'		=>	false,
+				'RECYCLE_BIN'		=>	'',
+				'CHMOD_VALUE'		=>	'0660',
+				'DEFAULT_RIGHTS'	=>	'',
+				'PAGINATION_THRESHOLD'	=>	500,
+				'PAGINATION_NUMBER'	=>	200
+			),
+		);
+	}
+	
+	
+	private static function generateActionsXML($application_nodes_) {
+		$dom = new DomDocument('1.0', 'utf-8');
+		
+		$driver_node = $dom->createElement('driver');
+		$driver_node->setAttribute('name', 'fs');
+		$driver_node->setAttribute('className', 'class.fsDriver.php');
+		$dom->appendChild($driver_node);
+		
+		$actions_node = $dom->createElement('actions');
+		$driver_node->appendChild($actions_node);
+		
+		$actions = array();
+		foreach ($application_nodes_ as $application_node) {
+			$app_id = $application_node->getAttribute('id');
+			$app_name = $application_node->getAttribute('name');
+		
+		$clientcallback_cdata = <<<EOF
+var repository;
+var path;
+if (window.actionArguments && window.actionArguments.length > 0) {
+	repository = 0;
+	path = window.actionArguments[0];
+} else {
+	userSelection = ajaxplorer.getFilesList().getUserSelection();
+	if (userSelection && userSelection.isUnique()) {
+		repository = ajaxplorer.repositoryId;
+		path = userSelection.getUniqueFileName();
+	}
+}
+
+new Ajax.Request(
+	'../start_app.php',
+	{
+		method: 'post',
+		parameters: {
+			id: $app_id,
+			repository: repository,
+			path: path
+		}
+	}
+);
+EOF;
+			
+			$mimes = array();
+			foreach ($application_node->getElementsByTagName('mime') as $mime_node)
+				$mimes[] = $mime_node->getAttribute('type');
+			
+			$actions['ulteo'.$application_node->getAttribute('id')] = array(
+				'id'				=>	$app_id,
+				'text'				=>	$app_name,
+				'mimes'				=>	$mimes,
+				'clientcallback'	=>	$clientcallback_cdata
+			);
+		}
+		
+		foreach ($actions as $k => $v) {
+			$action_node = $dom->createElement('action');
+			$action_node->setAttribute('name', $k);
+			$action_node->setAttribute('fileDefault', 'false');
+			$actions_node->appendChild($action_node);
+			
+			$gui_node = $dom->createElement('gui');
+			$gui_node->setAttribute('text', $v['text']);
+			$gui_node->setAttribute('title', $v['text']);
+			$gui_node->setAttribute('src', '/ovd/icon.php?id='.$v['id']);
+			$gui_node->setAttribute('hasAccessKey', 'false');
+			$action_node->appendChild($gui_node);
+			 
+			$context_node = $dom->createElement('context');
+			$context_node->setAttribute('selection', 'true');
+			$context_node->setAttribute('dir', 'false');
+			$context_node->setAttribute('recycle', 'false');
+			$context_node->setAttribute('actionBar', 'false');
+			$context_node->setAttribute('actionBarGroup', 'get');
+			$context_node->setAttribute('contextMenu', 'true');
+			$context_node->setAttribute('infoPanel', 'true');
+			$context_node->setAttribute('inZip', 'false');
+			$context_node->setAttribute('handleMimeTypes', 'true');
+			$gui_node->appendChild($context_node);
+			
+			foreach ($v['mimes'] as $mime) {
+				$mime_node = $dom->createElement('mime');
+				$mime_node->setAttribute('type', $mime);
+				$context_node->appendChild($mime_node);
+			}
+			
+			$selectioncontext_node = $dom->createElement('selectionContext');
+			$selectioncontext_node->setAttribute('dir', 'false');
+			$selectioncontext_node->setAttribute('file', 'true');
+			$selectioncontext_node->setAttribute('recycle', 'false');
+			$selectioncontext_node->setAttribute('unique', 'true');
+			$gui_node->appendChild($selectioncontext_node);
+			
+			$rightscontext_node = $dom->createElement('rightsContext');
+			$rightscontext_node->setAttribute('noUser', 'true');
+			$rightscontext_node->setAttribute('userLogged', 'only');
+			$rightscontext_node->setAttribute('read', 'true');
+			$rightscontext_node->setAttribute('write', 'false');
+			$rightscontext_node->setAttribute('adminOnly', 'false');
+			$action_node->appendChild($rightscontext_node);
+			
+			$processing_node = $dom->createElement('processing');
+			$action_node->appendChild($processing_node);
+			
+			$clientcallback_node = $dom->createElement('clientCallback');
+			$clientcallback_node->setAttribute('prepareModal', 'true');
+			
+			$clientcallback_cdata_node = $dom->createCDATASection($v['clientcallback']);
+			$clientcallback_node->appendChild($clientcallback_cdata_node);
+			
+			$processing_node->appendChild($clientcallback_node);
+			
+			$servercallback_node = $dom->createElement('serverCallback');
+			$servercallback_node->setAttribute('methodName', 'switchAction');
+			$processing_node->appendChild($servercallback_node);
+		}
+		
+		$xml = $dom->saveXML();
+		return $xml;
+	}
+}
--- client/web/web/login.php
+++ client/web/web/login.php
@@ -26,6 +26,7 @@ if (! is_array($_POST) || count($_POST) == 0) {
 }
 
 require_once(dirname(__FILE__).'/includes/core.inc.php');
+require_once(dirname(__FILE__).'/classes/Ajaxplorer.class.php');
 
 function return_error($errno_, $errstr_, $errmore_=NULL) {
 	$dom = new DomDocument('1.0', 'utf-8');
@@ -46,127 +47,7 @@ function return_popup($location_='about:blank') {
 	return $dom->saveXML();
 }
 
-function generateAjaxplorerActionsXML($application_nodes_) {
-	$dom = new DomDocument('1.0', 'utf-8');
 
-	$driver_node = $dom->createElement('driver');
-	$driver_node->setAttribute('name', 'fs');
-	$driver_node->setAttribute('className', 'class.fsDriver.php');
-	$dom->appendChild($driver_node);
-
-	$actions_node = $dom->createElement('actions');
-	$driver_node->appendChild($actions_node);
-
-	$actions = array();
-	foreach ($application_nodes_ as $application_node) {
-		$app_id = $application_node->getAttribute('id');
-		$app_name = $application_node->getAttribute('name');
-
-	$clientcallback_cdata = <<<EOF
-var repository;
-var path;
-if (window.actionArguments && window.actionArguments.length > 0) {
-	repository = 0;
-	path = window.actionArguments[0];
-} else {
-	userSelection = ajaxplorer.getFilesList().getUserSelection();
-	if (userSelection && userSelection.isUnique()) {
-		repository = ajaxplorer.repositoryId;
-		path = userSelection.getUniqueFileName();
-	}
-}
-
-new Ajax.Request(
-	'../start_app.php',
-	{
-		method: 'post',
-		parameters: {
-			id: $app_id,
-			repository: repository,
-			path: path
-		}
-	}
-);
-EOF;
-
-		$mimes = array();
-		foreach ($application_node->getElementsByTagName('mime') as $mime_node)
-			$mimes[] = $mime_node->getAttribute('type');
-
-		$actions['ulteo'.$application_node->getAttribute('id')] = array(
-			'id'				=>	$app_id,
-			'text'				=>	$app_name,
-			'mimes'				=>	$mimes,
-			'clientcallback'	=>	$clientcallback_cdata
-		);
-	}
-
-	foreach ($actions as $k => $v) {
-		$action_node = $dom->createElement('action');
-		$action_node->setAttribute('name', $k);
-		$action_node->setAttribute('fileDefault', 'false');
-		$actions_node->appendChild($action_node);
-
-		$gui_node = $dom->createElement('gui');
-		$gui_node->setAttribute('text', $v['text']);
-		$gui_node->setAttribute('title', $v['text']);
-		$gui_node->setAttribute('src', '/ovd/icon.php?id='.$v['id']);
-		$gui_node->setAttribute('hasAccessKey', 'false');
-		$action_node->appendChild($gui_node);
-
-			$context_node = $dom->createElement('context');
-			$context_node->setAttribute('selection', 'true');
-			$context_node->setAttribute('dir', 'false');
-			$context_node->setAttribute('recycle', 'false');
-			$context_node->setAttribute('actionBar', 'false');
-			$context_node->setAttribute('actionBarGroup', 'get');
-			$context_node->setAttribute('contextMenu', 'true');
-			$context_node->setAttribute('infoPanel', 'true');
-			$context_node->setAttribute('inZip', 'false');
-			$context_node->setAttribute('handleMimeTypes', 'true');
-			$gui_node->appendChild($context_node);
-
-			foreach ($v['mimes'] as $mime) {
-				$mime_node = $dom->createElement('mime');
-				$mime_node->setAttribute('type', $mime);
-				$context_node->appendChild($mime_node);
-			}
-
-			$selectioncontext_node = $dom->createElement('selectionContext');
-			$selectioncontext_node->setAttribute('dir', 'false');
-			$selectioncontext_node->setAttribute('file', 'true');
-			$selectioncontext_node->setAttribute('recycle', 'false');
-			$selectioncontext_node->setAttribute('unique', 'true');
-			$gui_node->appendChild($selectioncontext_node);
-
-		$rightscontext_node = $dom->createElement('rightsContext');
-		$rightscontext_node->setAttribute('noUser', 'true');
-		$rightscontext_node->setAttribute('userLogged', 'only');
-		$rightscontext_node->setAttribute('read', 'true');
-		$rightscontext_node->setAttribute('write', 'false');
-		$rightscontext_node->setAttribute('adminOnly', 'false');
-		$action_node->appendChild($rightscontext_node);
-
-		$processing_node = $dom->createElement('processing');
-		$action_node->appendChild($processing_node);
-
-			$clientcallback_node = $dom->createElement('clientCallback');
-			$clientcallback_node->setAttribute('prepareModal', 'true');
-
-			$clientcallback_cdata_node = $dom->createCDATASection($v['clientcallback']);
-			$clientcallback_node->appendChild($clientcallback_cdata_node);
-
-			$processing_node->appendChild($clientcallback_node);
-
-			$servercallback_node = $dom->createElement('serverCallback');
-			$servercallback_node->setAttribute('methodName', 'switchAction');
-			$processing_node->appendChild($servercallback_node);
-	}
-
-	$xml = $dom->saveXML();
-
-	return $xml;
-}
 
 $_SESSION['ovd-client']['interface'] = array();
 $_SESSION['ovd-client']['interface']['debug'] = $_POST['debug'];
@@ -361,65 +242,11 @@ if (count($server_nodes) < 1) {
 	die();
 }
 
-$_SESSION['ovd-client']['explorer'] = false;
-
-$profile_node = $session_node->getElementsByTagName('profile')->item(0);
-$sharedfolder_node = $session_node->getElementsByTagName('sharedfolder')->item(0);
-$sharedfolder_nodes = $session_node->getElementsByTagName('sharedfolder');
-if (is_object($profile_node) || (is_object($sharedfolder_node) && is_object($sharedfolder_nodes))) {
-	if (is_dir(dirname(__FILE__).'/ajaxplorer/'))
-		$_SESSION['ovd-client']['explorer'] = true;
-}
-
+$aj = new Ajaxplorer($session_node);
+$_SESSION['ovd-client']['explorer'] = ($aj->can_run() && $aj->is_required());
 if ($_SESSION['ovd-client']['explorer'] === true) {
-	$_SESSION['ovd-client']['ajxp'] = array();
-	$_SESSION['ovd-client']['ajxp']['repositories'] = array();
-	$_SESSION['ovd-client']['ajxp']['folders'] = array();
-	$_SESSION['ovd-client']['ajxp']['applications'] = generateAjaxplorerActionsXML($session_node->getElementsByTagName('application'));
-
-	if (is_object($profile_node)) {
-		$_SESSION['ovd-client']['ajxp']['repositories'][] = array(
-			'DISPLAY'					=>	_('Profile'),
-			'DRIVER'					=>	'fs',
-			'DRIVER_OPTIONS'			=>	array(
-				'PATH'					=>	'webdav://'.$profile_node->getAttribute('login').':'.$profile_node->getAttribute('password').'@'.$profile_node->getAttribute('server').':1113/ovd/fs/'.$profile_node->getAttribute('dir').'/',
-				'CREATE'				=>	false,
-				'RECYCLE_BIN'			=>	'',
-				'CHMOD_VALUE'			=>	'0660',
-				'DEFAULT_RIGHTS'		=>	'',
-				'PAGINATION_THRESHOLD'	=>	500,
-				'PAGINATION_NUMBER'		=>	200
-			),
-		);
-
-		$_SESSION['ovd-client']['ajxp']['folders'][] = $profile_node->getAttribute('dir');
-	}
-
-	if (is_object($sharedfolder_node) && is_object($sharedfolder_nodes)) {
-		foreach ($sharedfolder_nodes as $sharedfolder_node) {
-			if (! is_object($sharedfolder_node))
-				continue;
-
-			$_SESSION['ovd-client']['ajxp']['repositories'][] = array(
-				'DISPLAY'					=>	$sharedfolder_node->getAttribute('name'),
-				'DRIVER'					=>	'fs',
-				'DRIVER_OPTIONS'			=>	array(
-					'PATH'					=>	'webdav://'.$sharedfolder_node->getAttribute('login').':'.$sharedfolder_node->getAttribute('password').'@'.$sharedfolder_node->getAttribute('server').':1113/ovd/fs/'.$sharedfolder_node->getAttribute('dir').'/',
-					'CREATE'				=>	false,
-					'RECYCLE_BIN'			=>	'',
-					'CHMOD_VALUE'			=>	'0660',
-					'DEFAULT_RIGHTS'		=>	'',
-					'PAGINATION_THRESHOLD'	=>	500,
-					'PAGINATION_NUMBER'		=>	200
-				),
-			);
-
-			$_SESSION['ovd-client']['ajxp']['folders'][] = $sharedfolder_node->getAttribute('dir');
-		}
-	}
-}
-
-if (array_key_exists('explorer', $_SESSION['ovd-client']) && $_SESSION['ovd-client']['explorer'] === true) {
+	$_SESSION['ovd-client']['ajxp'] = $aj->build_data();
+	
 	$explorer_node = $dom->createElement('explorer');
 	$explorer_node->setAttribute('enabled', (($_SESSION['ovd-client']['explorer'] === true)?1:0));
 	$session_node->appendChild($explorer_node);
