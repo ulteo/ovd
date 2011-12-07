2609be82f4425550ba794d6bb204b4aab4e086b5
julien <julien@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2011-12-07 15:02:38 +0000

    Remove useless import since r7861
    git-svn-id: file:///home/code/svn/ovd/trunk@7867 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- ApplicationServer/OvdShells/ovd_shells/Platform.Linux/ExternalAppsClient.py
+++ ApplicationServer/OvdShells/ovd_shells/Platform.Linux/ExternalAppsClient.py
@@ -18,8 +18,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-import os
-
 import _platform as Platform
 
 from ovd_shells.ExternalAppsClient import ExternalAppsClient as AbstractExternalAppsClient
