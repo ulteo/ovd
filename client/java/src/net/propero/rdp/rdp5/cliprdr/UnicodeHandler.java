2a0730574d02353e6a6988be979d85c47df7fcf5
julien <julien@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2011-12-27 10:25:53 +0000

    Backport from trunk rev7358: Bugfix: when is exception in clipboard, to not send it to the server
    git-svn-id: file:///home/code/svn/ovd/branches/3.0@7992 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- client/java/src/net/propero/rdp/rdp5/cliprdr/UnicodeHandler.java
+++ client/java/src/net/propero/rdp/rdp5/cliprdr/UnicodeHandler.java
@@ -82,7 +82,7 @@ public class UnicodeHandler extends TypeHandler {
 				s = (String)(in.getTransferData(DataFlavor.stringFlavor));
 			} 
 			catch (Exception e) {
-				s = e.toString();
+				s = "";
 			}
 			
 			// Linux use \n instead \r\n 
