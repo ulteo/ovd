ulteo/ovd

commit c47805daf9ebf4b059da2d876df154d2b18826a3

Code split: split OvdAppChannel, InstanceManager and create RemoteAppsManager to have common code between OvdAppChannel apps management and application to start management
git-svn-id: file:///home/code/svn/ovd/trunk@7634 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

ApplicationServer/OvdShells/ovd_shells/RemoteAppsManager.py
@@ -1,0 +1,186 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2011 Ulteo SAS
+# http://www.ulteo.com
+# Author Julien LANGLOIS <julien@ulteo.com> 2011
+#
+# This program is free software; you can redistribute it and/or 
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# of the License
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+import locale
+import time
+import threading
+
+from ovd_shells.OvdAppChannel import OvdAppChannel
+
+class RemoteAppsManager(threading.Thread):
+	def __init__(self, instance_manager, vchannel, drives):
+		threading.Thread.__init__(self)
+		self.im = instance_manager
+		self.vchannel = vchannel
+		self.drives = drives
+		
+		self.jobs = []
+		
+		try:
+			self.encoding = locale.getpreferredencoding()
+		except locale.Error, err:
+			self.encoding = "UTF-8"
+	
+	
+	def loop(self):
+		while True:
+			# Read a complete packet
+			# so we assume a maximum packet size is 2048
+			packet = self.vchannel.Read(2048)
+			if packet is None:
+				print "error at read"
+				time.sleep(0.5)
+				continue
+			
+			(order, data) = OvdAppChannel.parse_packet(packet)
+			if order is None:
+				print "OvdAppChannel error: %s"%(data)
+			
+			
+			if order == OvdAppChannel.ORDER_START:
+				(token, app_id) = data
+				
+				print "recv startapp order %d %d"%(token, app_id)
+				self.pushJob((order, token, app_id))
+			
+			elif order == OvdAppChannel.ORDER_START_WITH_ARGS:
+				(token, app_id, dir_type, share, path) = data
+				
+				if dir_type == OvdAppChannel.DIR_TYPE_SHARED_FOLDER:
+					dir_type2 = self.im.DIR_TYPE_SHARED_FOLDER
+				elif dir_type == OvdAppChannel.DIR_TYPE_RDP_DRIVE:
+					dir_type2 = self.im.DIR_TYPE_RDP_DRIVE
+				elif dir_type == OvdAppChannel.DIR_TYPE_KNOWN_DRIVES:
+					dir_type2 = self.im.DIR_TYPE_KNOWN_DRIVES
+				elif dir_type == OvdAppChannel.DIR_TYPE_HTTP_URL:
+					dir_type2 = self.im.DIR_TYPE_HTTP_URL
+				else:
+					print "Message ORDER_START_WITH_ARGS: unknown dir type %X"%(dir_type)
+					return
+				
+				print "recv startapp order %d %d %d %s %s"%(token, app_id, dir_type2, share, path)
+				self.pushJob((order, token, app_id, dir_type2, share, path))
+			
+			elif order == OvdAppChannel.ORDER_STOP:
+				(token) = data
+				
+				print "recv stop order %d"%(token)
+				self.pushJob((order, token))
+			
+			elif order == OvdAppChannel.ORDER_EXIT:
+				# logoff
+				print "recv exit order"
+				return
+	
+	
+	def run(self):
+		self.drives.rebuild()
+		self.vchannel.Write(OvdAppChannel.getDrivesMessage(self.drives.getListUID()))
+		
+		t_init = 0
+		while True:
+			t0 = time.time()
+			job = self.popJob()
+			if job is not None:
+				print "RemoteApps got job",job
+				
+				order = job[0]
+				if order == OvdAppChannel.ORDER_START:
+					(token, app) = job[1:3]
+					
+					if not self.im.start_app_empty(token, app):
+						self.vchannel.Write(OvdAppChannel.build_packet_ORDER_CANT_START(token))
+						continue
+					
+					self.vchannel.Write(OvdAppChannel.build_packet_ORDER_STARTED(token))
+				  
+				  
+				elif order == OvdAppChannel.ORDER_START_WITH_ARGS:
+					(token, app, dir_type, share, local_path) = job[1:6]
+					
+					if not self.im.start_app_with_arg(token, app, dir_type, local_path, share):
+						self.vchannel.Write(OvdAppChannel.build_packet_ORDER_CANT_START(token))
+						continue
+					
+					self.vchannel.Write(OvdAppChannel.build_packet_ORDER_STARTED(token))
+				
+				
+				elif order == OvdAppChannel.ORDER_STOP:
+					token = job[1]
+					
+					if not self.im.stop_app(token):
+						continue
+					
+					self.vchannel.Write(OvdAppChannel.build_packet_ORDER_STOPPED(token))
+			
+			
+			tokens = self.im.get_exited_instances()
+			if tokens is not None:
+				for token in tokens:
+					self.vchannel.Write(OvdAppChannel.build_packet_ORDER_STOPPED(token))
+			
+			if job is None and tokens is None:
+				time.sleep(0.1)
+			
+			
+			t1 = time.time()
+			t_init+= (t1 - t0)
+			if t_init > 5:
+				# We send channel init time to time to manage the reconnection
+				self.vchannel.Write(OvdAppChannel.getInitPacket())
+				
+				if self.drives.rebuild():
+					self.vchannel.Write(OvdAppChannel.getDrivesMessage(self.drives.getListUID()))
+				
+				t_init = 0
+	
+	
+	def stop(self):
+		if self.isAlive():
+			self._Thread__stop()
+		
+		t0 = time.time()
+		self.im.kill_all_apps()
+		while self.im.has_running_instances():
+			if time.time() - t0 > 20:
+				print "Still running instances after 20 seconds, exiting anyway ..."
+				break
+			
+			tokens = self.im.get_exited_instances()
+			for token in tokens:
+				self.vchannel.Write(OvdAppChannel.build_packet_ORDER_STOPPED(token))
+			
+			if self.im.has_running_instances():
+				self.im.kill_all_apps()
+				time.sleep(1)
+	
+	
+	def pushJob(self, job):
+		# todo mutex lock
+		self.jobs.append(job)
+		# todo mutex unlock
+	
+	
+	def popJob(self):
+		# todo mutex lock
+		if len(self.jobs) == 0:
+			return None
+		# todo mutex unlock
+		return self.jobs.pop()

ApplicationServer/OvdShells/OvdDesktop
@@ -1,11 +1,11 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2009-2010 Ulteo SAS
+# Copyright (C) 2009-2011 Ulteo SAS
 # http://www.ulteo.com
 # Author Laurent CLOUET <laurent@ulteo.com> 2010
 # Author Gauvain POCENTEK <gauvain@ulteo.com> 2009
-# Author Julien LANGLOIS <julien@ulteo.com> 2009-2010
+# Author Julien LANGLOIS <julien@ulteo.com> 2009, 2010, 2011
 #
 # This program is free software; you can redistribute it and/or 
 # modify it under the terms of the GNU General Public License
@@ -24,9 +24,11 @@
 import ConfigParser
 import os
 import sys
+import time
 
 from ovd_shells.Platform import _platform as Platform
 from ovd_shells.Platform.Folders import Folders
+from ovd_shells.Platform.InstancesManager import InstancesManager
 from ovd_shells import Util
 
 def generateConfFile(path_file_, sm_, token_):
@@ -119,13 +121,27 @@ def main():
 		except Exception, err:
 			print "Unable to start external apps", err
 	
+	im = InstancesManager()
+	im.setSharedFolderDB(folders)
 	
 	# Applications to auto starts
-	Util.manageAutoStartApplication(user_sdir)
+	Util.manageAutoStartApplication(user_sdir, im)
 	
 	Util.startModules()
 	
 	Platform.startDesktop()
+	
+	t0 = time.time()
+	im.kill_all_apps()
+	while im.has_running_instances():
+		if time.time() - t0 > 20:
+			print "Still running instances after 20 seconds, exiting anyway ..."
+			break
+		
+		im.get_exited_instances()
+		if im.has_running_instances():
+			im.kill_all_apps()
+			time.sleep(1)
 	
 	sys.exit(0)
 

ApplicationServer/OvdShells/OvdRemoteApps
@@ -1,10 +1,10 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2009 Ulteo SAS
+# Copyright (C) 2009-2011 Ulteo SAS
 # http://www.ulteo.com
-# Author Gauvain POCENTEK <gauvain@ulteo.com>
-# Author Julien LANGLOIS <julien@ulteo.com>
+# Author Gauvain POCENTEK <gauvain@ulteo.com> 2009
+# Author Julien LANGLOIS <julien@ulteo.com> 2010, 2011
 #
 # This program is free software; you can redistribute it and/or 
 # modify it under the terms of the GNU General Public License
@@ -30,6 +30,7 @@ from ovd_shells.Platform.Drives import Drives
 from ovd_shells.Platform.Folders import Folders
 from ovd_shells.Platform.InstancesManager import InstancesManager
 from ovd_shells.Platform.VirtualChannel import VirtualChannel
+from ovd_shells.RemoteAppsManager import RemoteAppsManager
 from ovd_shells import Util
 
 def main():
@@ -68,18 +69,21 @@ def main():
 	
 	vchannel.Write(OvdAppChannel.getInitPacket())
 	
-	im = InstancesManager(vchannel, folders, drives)
-	im.start()
+	im = InstancesManager()
+	im.setDrivesDB(drives)
+	im.setSharedFolderDB(folders)
 	
 	# Applications to auto starts
-	Util.manageAutoStartApplication(user_sdir)
+	Util.manageAutoStartApplication(user_sdir, im)
+	
+	manager = RemoteAppsManager(im, vchannel, drives)
+	manager.start()
 	
 	Util.startModules()
 	
-	app_channel = OvdAppChannel(im)
-	app_channel.run(vchannel)
+	manager.loop()
 	
-	im.stop()
+	manager.stop()
 	
 	vchannel.Close()
 	

ApplicationServer/OvdShells/ovd_shells/InstancesManager.py
@@ -20,36 +20,32 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 import os
-import struct
-import threading
-import time
 
 from HttpFile import HttpFile
-from OvdAppChannel import OvdAppChannel
 
-class InstancesManager(threading.Thread):
-	def __init__(self, vchannel, folders, drives):
-		threading.Thread.__init__(self)
+
+class InstancesManager:
+	DIR_TYPE_NATIVE        = 0
+	DIR_TYPE_SHARED_FOLDER = 1
+	DIR_TYPE_RDP_DRIVE     = 2
+	DIR_TYPE_KNOWN_DRIVES  = 3
+	DIR_TYPE_HTTP_URL      = 4
+	
+	
+	def __init__(self):
+		self.known_drives = None
+		self.shared_folders = None
 		
-		self.vchannel = vchannel
-		self.folders = folders
-		self.drives = drives
-		self.jobs = []
 		self.instances = []
-
-
-	def pushJob(self, job):
-		# todo mutex lock
-		self.jobs.append(job)
-		# todo mutex unlock
 	
 	
-	def popJob(self):
-		# todo mutex lock
-		if len(self.jobs) == 0:
-			return None
-		# todo mutex unlock
-		return self.jobs.pop()
+	def setDrivesDB(self, known_drives):
+		self.known_drives = known_drives
+	
+	
+	def setSharedFolderDB(self, shared_folders):
+		self.shared_folders = shared_folders
+	
 	
 	def getInstanceByToken(self, token):
 		for instance in self.instances:
@@ -58,102 +54,119 @@ class InstancesManager(threading.Thread):
 		
 		return None
 	
-	def run(self):
-		self.drives.rebuild()
-		self.vchannel.Write(OvdAppChannel.getDrivesMessage(self.drives.getListUID()))
-		
-		t_init = 0
-		while True:
-			t0 = time.time()
-			job = self.popJob()
-			if job is not None:
-				print "IM got job",job
-				
-				order = job[0]
-				if order == OvdAppChannel.ORDER_START:
-					(token, app) = job[1:3]
-					cmd = "startovdapp %d"%(app)
-					extra = None
-					
-					if len(job)>3:
-						dir_type = job[3]
-						local_path = None
-						
-						if dir_type == OvdAppChannel.DIR_TYPE_RDP_DRIVE:
-							local_path = self.shareName2path(job[4])
-						
-						elif dir_type == OvdAppChannel.DIR_TYPE_KNOWN_DRIVES:
-							local_path = self.drives.getPath(job[4])
-							if local_path is None:
-								 print "Unknown drive ID %s"%(job[4])
-								 continue
-						
-						elif dir_type == OvdAppChannel.DIR_TYPE_HTTP_URL:
-							http = HttpFile(job[4], job[5])
-							if not http.recv():
-								print "Unable to get file by HTTP"
-								continue
-							
-							local_path = os.path.dirname(http.path)
-							extra = http
-						
-						else:
-							local_path = self.folders.getPathFromID(job[4])
-						
-						if local_path is not None:
-							arg = os.path.join(local_path, job[5].replace("/", os.path.sep))
-							cmd+=' "%s"'%(arg)
-					instance = self.launch(cmd)
-					
-					# ToDo: sleep 0.5s and check if the process exist
-					# with startovdapp return status, get the error
-					
-					buf = struct.pack("<B", OvdAppChannel.ORDER_STARTED)
-					buf+= struct.pack("<I", token)
-					self.vchannel.Write(buf)
-					
-					self.instances.append((instance, token, extra))
-				
-				elif order == OvdAppChannel.ORDER_STOP:
-					token = job[1]
-					instance = self.getInstanceByToken(token)
-					
-					if instance is None:
-						print "Not existing token",token
-						continue
-					
-					self.kill(instance[0])
-					self.onInstanceExited(instance)
-			
-			ret = self.wait()
-			
-			if job is None and ret is False:
-				time.sleep(0.1)
-			
-					
-			t1 = time.time()
-			t_init+= (t1 - t0)
-			if t_init > 5:
-				# We send channel init time to time to manage the reconnection
-				self.vchannel.Write(OvdAppChannel.getInitPacket())
-				
-				if self.drives.rebuild():
-					self.vchannel.Write(OvdAppChannel.getDrivesMessage(self.drives.getListUID()))
-				
-				t_init = 0
 	
-	def stop(self):
-		if self.isAlive():
-			self._Thread__stop()
+	def start_app_empty(self, token, app_id):
+		cmd = "startovdapp %d"%(app_id)
+		extra = None
 		
+		instance = self.launch(cmd)
+		if token is None:
+			token = instance
+		
+		self.instances.append((instance, token, extra))
+		
+		return True
+	
+	
+	def start_app_with_arg(self, token, app_id, dir_type, f_path, f_share):
+		extra = None
+		
+		if dir_type == self.DIR_TYPE_NATIVE:
+			arg = f_path
+		
+		elif dir_type == self.DIR_TYPE_SHARED_FOLDER:
+			if self.shared_folders is None:
+				print "No Shared folders registered"
+				return False
+			
+			arg = os.path.join(local_path, f_path.replace("/", os.path.sep))
+		
+		elif dir_type == self.DIR_TYPE_RDP_DRIVE:
+			local_path = self.shareName2path(f_share)
+			arg = os.path.join(local_path, f_path.replace("/", os.path.sep))
+		
+		elif dir_type == self.DIR_TYPE_KNOWN_DRIVES:
+			if self.known_drives is None:
+				print "No Known drives registered"
+				return False
+			
+			local_path = self.known_drives.getPath(f_share)
+			if local_path is None:
+				  print "Unknown drive ID %s"%(f_share)
+				  return False
+			
+			arg = os.path.join(local_path, f_path.replace("/", os.path.sep))
+		
+		elif dir_type == self.DIR_TYPE_HTTP_URL:
+			http = HttpFile(f_share, f_path)
+			if not http.recv():
+				print "Unable to get file by HTTP"
+				return False
+			
+			local_path = os.path.dirname(http.path)
+			arg = os.path.join(local_path, f_path.replace("/", os.path.sep))
+			extra = http
+		
+		else:
+			print "Unknown type"
+			return False
+		
+		
+		cmd = 'startovdapp %d "%s"'%(app_id, arg)
+		
+		instance = self.launch(cmd)
+		if token is None:
+			token = instance
+		
+		# ToDo: sleep 0.5s and check if the process exist
+		# with startovdapp return status, get the error
+		
+		self.instances.append((instance, token, extra))
+		return True
+	
+	
+	def stop_app(self, token):
+		instance = self.getInstanceByToken(token)
+		if instance is None:
+			print "Not existing token",token
+			return False
+		
+		self.kill(instance[0])
+		
+		self.instances.remove(instance)
+		
+		return True
+	
+	
+	def has_running_instances(self):
+		return (len(self.instances) != 0)
+	
+	
+	def kill_all_apps(self):
 		for instance in self.instances:
 			self.kill(instance[0])
-			
-			buf = struct.pack("<B", OvdAppChannel.ORDER_STOPPED)
-			buf+= struct.pack("<I", instance[1])
-			self.vchannel.Write(buf)
 		
-		self.instances = []
+		return True
+	
+	
+	def get_exited_instances(self):
+		instances = self.wait()
+		
+		if len(instances) == 0:
+			return []
+		
+		tokens = []
+		
+		for instance in instances:
+			self.instances.remove(instance)
+			tokens.append(instance[1])
+			
+			if instance[2] is not None:
+				# Backup file
+				instance[2].send()
+		
+		return tokens
+	
 	
 	def wait(self):
 		"""
@@ -170,19 +183,3 @@ class InstancesManager(threading.Thread):
 	@staticmethod
 	def shareName2path(share):
 		raise NotImplementedError("must be redeclared")
-	
-	def onInstanceNotAvailable(self, token):
-		buf = struct.pack("<B", OvdAppChannel.ORDER_CANT_START)
-		buf+= struct.pack("<I", token)
-		self.vchannel.Write(buf)
-	
-	def onInstanceExited(self, instance):
-		self.instances.remove(instance)
-		
-		buf = struct.pack("<B", OvdAppChannel.ORDER_STOPPED)
-		buf+= struct.pack("<I", instance[1])
-		self.vchannel.Write(buf)
-		
-		if instance[2] is not None:
-			# Backup file
-			instance[2].send()

ApplicationServer/OvdShells/ovd_shells/OvdAppChannel.py
@@ -41,13 +41,6 @@ class OvdAppChannel:
 	DIR_TYPE_KNOWN_DRIVES  = 0x03
 	DIR_TYPE_HTTP_URL      = 0x10
 	
-	def __init__(self, instance_manager):
-		self.im = instance_manager
-		try:
-			self.encoding = locale.getpreferredencoding()
-		except locale.Error, err:
-			self.encoding = "UTF-8"
-	
 	
 	@staticmethod
 	def getInitPacket():
@@ -66,43 +59,53 @@ class OvdAppChannel:
 		return buf
 	
 	
-	def run(self, vchannel):
-		while True:
-			# Read a complete packet
-			# so we assume a maximum packet size is 2048
-			packet = vchannel.Read(2048)
-			if packet is None:
-				print "error at read"
-				time.sleep(0.5)
-				continue
-			
-			if len(packet) < 1:
-				print "Packet length error"
-				continue
-		  
-			if len(packet) < 1:
-				print "Packet length error"
-				return None
-			
-			order = struct.unpack('>B', packet[0])[0]
-			if order == self.ORDER_START:
-				self.packet_ORDER_START(packet)
-			
-			elif order == self.ORDER_START_WITH_ARGS:
-				self.packet_ORDER_START_WITH_ARGS(packet)
-			
-			elif order == self.ORDER_STOP:
-				self.packet_ORDER_STOP(packet)
-			
-			elif order == self.ORDER_EXIT:
-				# logoff
-				return
-			
-			else:
-				print "unknown message %X"%(order)
+	@classmethod
+	def build_packet_ORDER_CANT_START(cls, token):
+		buf = struct.pack("<B", cls.ORDER_CANT_START)
+		buf+= struct.pack("<I", token)
+		
+		return buf
 	
 	
-	def packet_ORDER_START(self, packet):
+	@classmethod
+	def build_packet_ORDER_STARTED(cls, token):
+		buf = struct.pack("<B", cls.ORDER_STARTED)
+		buf+= struct.pack("<I", token)
+		
+		return buf
+	
+	
+	@classmethod
+	def build_packet_ORDER_STOPPED(cls, token):
+		buf = struct.pack("<B", cls.ORDER_STOPPED)
+		buf+= struct.pack("<I", token)
+		
+		return buf
+	
+	
+	@classmethod
+	def parse_packet(cls, packet):
+		if len(packet) < 1:
+			return (None, "Packet length error")
+		
+		order = struct.unpack('>B', packet[0])[0]
+		if order == OvdAppChannel.ORDER_START:
+			return cls.packet_ORDER_START(packet)
+		
+		elif order == OvdAppChannel.ORDER_START_WITH_ARGS:
+			return cls.packet_ORDER_START_WITH_ARGS(packet)
+		
+		elif order == OvdAppChannel.ORDER_STOP:
+			return cls.packet_ORDER_STOP(packet)
+		
+		elif order == OvdAppChannel.ORDER_EXIT:
+			return (OvdAppChannel.ORDER_EXIT, None)
+		
+		return (None, "unknown message %X"%(order))
+	
+	
+	@classmethod
+	def packet_ORDER_START(cls, packet):
 		if len(packet) < 9:
 			print "Packet length error"
 			return
@@ -110,21 +113,26 @@ class OvdAppChannel:
 		token = struct.unpack('<I', packet[1:5])[0]
 		app_id = struct.unpack('<I', packet[5:9])[0]
 		
-		print "recv startapp order %d %d"%(token, app_id)
-		self.im.pushJob((self.ORDER_START, token, app_id))
+		return (cls.ORDER_START, (token, app_id))
 	
 	
-	def packet_ORDER_STOP(self, packet):
+	@classmethod
+	def packet_ORDER_STOP(cls, packet):
 		if len(packet) < 5:
 			print "Packet length error"
 			return
 		token = struct.unpack('<I', packet[1:5])[0]
 		
-		print "recv stop order %d"%(token)
-		self.im.pushJob((self.ORDER_STOP, token))
+		return (cls.ORDER_STOP, (token))
 	
 	
-	def packet_ORDER_START_WITH_ARGS(self, packet):
+	@classmethod
+	def packet_ORDER_START_WITH_ARGS(cls, packet):
+		try:
+			encoding = locale.getpreferredencoding()
+		except locale.Error, err:
+			encoding = "UTF-8"
+		
 		if len(packet) < 13 + 4 + 4:
 			print "Packet length error"
 			return
@@ -132,7 +140,7 @@ class OvdAppChannel:
 		app_id = struct.unpack('<I', packet[5:9])[0]
 		
 		dir_type = struct.unpack('>B', packet[9])[0]
-		if dir_type not in [self.DIR_TYPE_SHARED_FOLDER, self.DIR_TYPE_RDP_DRIVE, self.DIR_TYPE_KNOWN_DRIVES, self.DIR_TYPE_HTTP_URL]:
+		if dir_type not in [cls.DIR_TYPE_SHARED_FOLDER, cls.DIR_TYPE_RDP_DRIVE, cls.DIR_TYPE_KNOWN_DRIVES, cls.DIR_TYPE_HTTP_URL]:
 			print "Message ORDER_START_WITH_ARGS: unknown dir type %X"%(dir_type)
 			return
 		
@@ -146,7 +154,7 @@ class OvdAppChannel:
 		share = packet[ptr:ptr+l]
 		try:
 			share = share.decode("UTF-16LE")
-			share = share.encode(self.encoding)
+			share = share.encode(encoding)
 		except:
 			print "Message ORDER_START_WITH_ARGS: share argument is not UTF-16-LE srting"
 			return
@@ -172,12 +180,10 @@ class OvdAppChannel:
 		path = packet[ptr:ptr+l]
 		try:
 			path = path.decode("UTF-16LE")
-			path = path.encode(self.encoding)
+			path = path.encode(encoding)
 		except:
 			print "Message ORDER_START_WITH_ARGS: path argument is not UTF-16-LE srting"
 			return
 		ptr+=l
 		
-		job = (self.ORDER_START, token, app_id, dir_type, share, path)
-		print "recv startapp order %d %d %d %s %s"%(token, app_id, dir_type, share, path)
-		self.im.pushJob(job)
+		return (cls.ORDER_START_WITH_ARGS, (token, app_id, dir_type, share, path))

ApplicationServer/OvdShells/ovd_shells/Platform.Linux/InstancesManager.py
@@ -37,7 +37,8 @@ class InstancesManager(AbstractInstancesManager):
 	
 	
 	def wait(self):
-		haveWorked = False
+		exited_instance = []
+		
 		for instance in self.instances:
 			pid = instance[0]
 			
@@ -47,10 +48,9 @@ class InstancesManager(AbstractInstancesManager):
 			if os.path.isdir(path):
 				continue
 			
-			self.onInstanceExited(instance)
-			haveWorked = True
+			exited_instance.append(instance)
 		
-		return haveWorked
+		return exited_instance
 	
 	
 	def kill(self, pid):

ApplicationServer/OvdShells/ovd_shells/Platform.Windows/InstancesManager.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2009,2010 Ulteo SAS
+# Copyright (C) 2009-2011 Ulteo SAS
 # http://www.ulteo.com
-# Author Julien LANGLOIS <julien@ulteo.com> 2009, 2010
+# Author Julien LANGLOIS <julien@ulteo.com> 2009, 2010, 2011
 #
 # This program is free software; you can redistribute it and/or 
 # modify it under the terms of the GNU General Public License
@@ -34,25 +34,25 @@ class InstancesManager(AbstractInstancesManager):
 	
 	def wait(self):
 		if len(self.instances) == 0:
-			return False
+			return []
 		
 		handleList = [instance[0] for instance in self.instances] 
 		
 		res = win32event.WaitForMultipleObjects(handleList, False, 0)
 		
 		if res in [win32event.WAIT_TIMEOUT, win32event.WAIT_FAILED]:
-			return False
+			return []
 		
 		if res > win32event.WAIT_ABANDONED_0:
 			# todo: understand what it means!
-			return False
+			return []
 		
 		index = res - win32event.WAIT_OBJECT_0
 		
 		win32file.CloseHandle(handleList[index])
 		
-		self.onInstanceExited(self.instances[index])
-		return True
+		return [self.instances[index]]
+		
 	
 	def kill(self, handle):
 		ppid = win32process.GetProcessId(handle)

ApplicationServer/OvdShells/ovd_shells/Util.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2010 Ulteo SAS
+# Copyright (C) 2010-2011 Ulteo SAS
 # http://www.ulteo.com
-# Author Julien LANGLOIS <julien@ulteo.com> 2010
+# Author Julien LANGLOIS <julien@ulteo.com> 2010, 2011
 #
 # This program is free software; you can redistribute it and/or 
 # modify it under the terms of the GNU General Public License
@@ -60,17 +60,27 @@ def loadUserEnv(d):
 		
 		os.environ[key] = value
 
-def manageAutoStartApplication(d):
+def manageAutoStartApplication(d, im):
 	for path in glob.glob(os.path.join(d, "to_start", "*")):
 		f = file(path, "r")
 		lines = f.readlines()
 		f.close()
 		
-		cmd = "startovdapp %s"%(lines[0].strip())
-		if len(lines) > 1:
-			cmd+= ' "%s"'%(lines[1].strip())
+		try:
+			app_id = int(lines[0].strip())
+		except ValueError, err:
+			print "Invalid application id '%s'"%(lines[0].strip())
+			continue
 		
-		Platform.launch(cmd)
+		if len(lines)>1:
+			dir_type = im.DIR_TYPE_NATIVE
+			share = None
+			path  = lines[1].strip()
+			
+			im.start_app_with_arg(None, app_id, dir_type, share, path)
+			continue
+		
+		im.start_app_empty(None, app_id)
 
 def startModules():
 	novell = Novell()

commit 83d039ed09a2894745d6bc02e4c59807e0a32369

Bugfix infinite loop (introduced by r7634)
git-svn-id: file:///home/code/svn/ovd/trunk@8016 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

ApplicationServer/OvdShells/ovd_shells/RemoteAppsManager.py
@@ -146,7 +146,7 @@ class RemoteAppsManager(threading.Thread):
 				for token in tokens:
 					self.vchannel.Write(OvdAppChannel.build_packet_ORDER_STOPPED(token))
 			
-			if job is None and tokens is None:
+			if job is None and len(tokens) == 0:
 				time.sleep(0.1)
 			
 			