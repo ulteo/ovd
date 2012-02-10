eaf1359b4881747d51574d124117e95954a42209
thomas <thomas@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2012-02-10 14:59:33 +0000

    Bugfix: Seamless vchannel have to be reset for session recovery
    git-svn-id: file:///home/code/svn/ovd/trunk@8067 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- ApplicationServer/windows/seamlessrdpshell/main.c
+++ ApplicationServer/windows/seamlessrdpshell/main.c
@@ -21,6 +21,25 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
+/*
+ * Copyright (C) 2012 Ulteo SAS
+ * http://www.ulteo.com
+ * Author Thomas MOUTON <thomas@ulteo.com> 2012
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
 #define WINVER 0x0501
 
@@ -419,6 +438,11 @@ WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmdline, int cmdshow)
 		if (connected && !g_connected)
 		{
 			int flags;
+
+			// We have to force reopening channel
+			SeamlessChannel_uninit();
+			SeamlessChannel_init();
+
 			/* These get reset on each reconnect */
 			SystemParametersInfo(SPI_SETDRAGFULLWINDOWS, TRUE, NULL, 0);
 			SystemParametersInfo(SPI_SETSCREENSAVEACTIVE, FALSE, NULL,
