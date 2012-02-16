ulteo/ovd

commit 79cf2c8d6008ad4fa32922a9647d9a412a1e400a

code cleaning: centralize applets .jar name and main class into an only one function
git-svn-id: file:///home/code/svn/ovd/trunk@8068 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

client/web/web/media/script/daemon.js
@@ -1,8 +1,8 @@
 /**
- * Copyright (C) 2009-2011 Ulteo SAS
+ * Copyright (C) 2009-2012 Ulteo SAS
  * http://www.ulteo.com
- * Author Jeremy DESVAGES <jeremy@ulteo.com>
- * Author Julien LANGLOIS <julien@ulteo.com> 2011
+ * Author Jeremy DESVAGES <jeremy@ulteo.com> 2009-2011
+ * Author Julien LANGLOIS <julien@ulteo.com> 2011, 2012
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -22,9 +22,6 @@
 var Daemon = Class.create({
 	i18n: new Array(),
 	context: null,
-
-	applet_version: '',
-	applet_main_class: '',
 
 	sessionmanager: null,
 
@@ -517,5 +514,9 @@ var Daemon = Class.create({
 			
 			this.settings.set(name, value);
 		}
+	},
+	
+	buildAppletNode: function(mode_, params_) {
+		return buildAppletNode('ulteoapplet', 'org.ulteo.ovd.applet.'+mode_, 'jpedal.jar,log4j-1.2.jar,ulteo-applet.jar', params_);
 	}
 });

client/web/web/media/script/common.js
@@ -1,8 +1,8 @@
 /**
- * Copyright (C) 2010-2011 Ulteo SAS
+ * Copyright (C) 2010-2012 Ulteo SAS
  * http://www.ulteo.com
- * Author Jeremy DESVAGES <jeremy@ulteo.com>
- * Author Julien LANGLOIS <julien@ulteo.com> 2011
+ * Author Jeremy DESVAGES <jeremy@ulteo.com> 2010-2011
+ * Author Julien LANGLOIS <julien@ulteo.com> 2011, 2012
  * Author Omar AKHAM <oakham@ulteo.com> 2011
  *
  * This program is free software; you can redistribute it and/or
@@ -247,9 +247,9 @@ function onStartSessionSuccess(xml_) {
 
 	setTimeout(function() {
 		if (session_mode == 'Desktop')
-			daemon = new Desktop('ulteo-applet.jar', 'org.ulteo.ovd.applet.Desktop', debug);
+			daemon = new Desktop(debug);
 		else
-			daemon = new Applications('ulteo-applet.jar', 'org.ulteo.ovd.applet.Applications', debug);
+			daemon = new Applications(debug);
 
 		daemon.sessionmanager = sessionmanager_host;
 
@@ -1029,9 +1029,9 @@ function onStartExternalSessionSuccess(xml_) {
 
 	setTimeout(function() {
 		if (session_mode == 'Desktop')
-			daemon = new Desktop('ulteo-applet.jar', 'org.ulteo.ovd.applet.Desktop', false);
+			daemon = new Desktop(false);
 		else
-			daemon = new External('ulteo-applet.jar', 'org.ulteo.ovd.applet.Applications', false);
+			daemon = new External(false);
 
 		daemon.sessionmanager = sessionmanager_host;
 		daemon.keymap = user_keymap;

client/web/web/media/script/daemon_applications.js
@@ -1,8 +1,8 @@
 /**
- * Copyright (C) 2009-2011 Ulteo SAS
+ * Copyright (C) 2009-2012 Ulteo SAS
  * http://www.ulteo.com
- * Author Jeremy DESVAGES <jeremy@ulteo.com>
- * Author Julien LANGLOIS <julien@ulteo.com> 2011
+ * Author Jeremy DESVAGES <jeremy@ulteo.com> 2009-2011
+ * Author Julien LANGLOIS <julien@ulteo.com> 2011, 2012
  * Author Omar AKHAM <oakham@ulteo.com> 2011
  *
  * This program is free software; you can redistribute it and/or
@@ -37,8 +37,8 @@ var Applications = Class.create(Daemon, {
 
 	progress_bar_step: 50,
 
-	initialize: function(applet_version_, applet_main_class_, debug_) {
-		Daemon.prototype.initialize.apply(this, [applet_version_, applet_main_class_, debug_]);
+	initialize: function(debug_) {
+		Daemon.prototype.initialize.apply(this, [debug_]);
 
 		$('applicationsAppletContainer').innerHTML = '';
 
@@ -111,7 +111,7 @@ var Applications = Class.create(Daemon, {
 			applet_params.set('setting_'+pair.key, pair.value);
 		});
 
-		var applet = buildAppletNode('ulteoapplet', this.applet_main_class, 'jpedal.jar,log4j-1.2.jar,'+this.applet_version, applet_params);
+		var applet = this.buildAppletNode('Applications', applet_params);
 		$('applicationsAppletContainer').show();
 		$('applicationsAppletContainer').appendChild(applet);
 

client/web/web/media/script/daemon_desktop.js
@@ -1,8 +1,8 @@
 /**
- * Copyright (C) 2009-2011 Ulteo SAS
+ * Copyright (C) 2009-2012 Ulteo SAS
  * http://www.ulteo.com
- * Author Jeremy DESVAGES <jeremy@ulteo.com>
- * Author Julien LANGLOIS <julien@ulteo.com> 2011
+ * Author Jeremy DESVAGES <jeremy@ulteo.com> 2009-2011
+ * Author Julien LANGLOIS <julien@ulteo.com> 2011, 2012
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -25,8 +25,8 @@ var Desktop = Class.create(Daemon, {
 
 	fullscreen: false,
 
-	initialize: function(applet_version_, applet_main_class_, debug_) {
-		Daemon.prototype.initialize.apply(this, [applet_version_, applet_main_class_, debug_]);
+	initialize: function(debug_) {
+		Daemon.prototype.initialize.apply(this, [debug_]);
 
 		$('desktopAppletContainer').innerHTML = '';
 	},
@@ -72,7 +72,7 @@ var Desktop = Class.create(Daemon, {
 				applet_params.set('setting_'+pair.key, pair.value);
 			});
 			
-			var applet = buildAppletNode('ulteoapplet', this.applet_main_class, 'jpedal.jar,log4j-1.2.jar,'+this.applet_version, applet_params);
+			var applet = this.buildAppletNode('Desktop', applet_params);
 			applet.setAttribute('width', applet_width);
 			applet.setAttribute('height', applet_height);
 			$('desktopAppletContainer').setStyle({width: applet_width+'px', height: applet_height+'px', top: 0+'px', left: 0+'px'});

client/web/web/media/script/daemon_external.js
@@ -1,7 +1,8 @@
 /**
- * Copyright (C) 2009-2010 Ulteo SAS
+ * Copyright (C) 2009-2012 Ulteo SAS
  * http://www.ulteo.com
- * Author Jeremy DESVAGES <jeremy@ulteo.com>
+ * Author Jeremy DESVAGES <jeremy@ulteo.com> 2009-2010
+ * Author Julien LANGLOIS <julien@ulteo.com> 2012
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -19,8 +20,8 @@
  **/
 
 var External = Class.create(Applications, {
-	initialize: function(applet_version_, applet_main_class_, debug_) {
-		Daemon.prototype.initialize.apply(this, [applet_version_, applet_main_class_, debug_]);
+	initialize: function(debug_) {
+		Daemon.prototype.initialize.apply(this, [debug_]);
 
 		$('applicationsAppletContainer').innerHTML = '';
 

commit c1872680ac30368a07d8378e463a02b7bdd9a5d6

Bugfix debug panel regression from r8068
git-svn-id: file:///home/code/svn/ovd/trunk@8072 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

client/web/web/media/script/daemon.js
@@ -63,13 +63,10 @@ var Daemon = Class.create({
 
 	application_token: 0,
 
-	initialize: function(applet_version_, applet_main_class_, debug_) {
+	initialize: function(debug_) {
 		this.settings = new Hash();
 		this.servers = new Hash();
 		this.liaison_server_applications = new Hash();
-
-		this.applet_version = applet_version_;
-		this.applet_main_class = applet_main_class_;
 
 		this.debug = debug_;
 