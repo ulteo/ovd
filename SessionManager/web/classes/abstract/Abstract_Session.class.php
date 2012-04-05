ulteo/ovd

commit 1cd842055313c3c1710222446c31ea60c845702e

Improvement: store the available applications in the session object
git-svn-id: file:///home/code/svn/ovd/trunk@8157 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

SessionManager/web/classes/abstract/Abstract_Session.class.php
@@ -108,8 +108,12 @@ class Abstract_Session {
 				return false;
 			}
 		}
+		
+		$data = array();
+		$data['applications'] = $session_->getPublishedApplications();
+		$data['runnig_applications'] = $session_->running_applications;
 
-		$SQL->DoQuery('UPDATE @1 SET @2=%3,@4=%5,@6=%7,@8=%9,@10=%11,@12=%13,@14=%15,@16=%17,@18=%19,@20=%21,@22=%23 WHERE @24 = %25 LIMIT 1', $SQL->prefix.'sessions', 'server', $session_->server, 'mode', $session_->mode, 'type', $session_->type, 'status', $session_->status, 'settings', serialize($session_->settings), 'user_login', $session_->user_login, 'user_displayname', $session_->user_displayname, 'servers', serialize($session_->servers), 'applications', serialize($session_->running_applications), 'start_time', $session_->start_time, 'timestamp', time(), 'id', $id);
+		$SQL->DoQuery('UPDATE @1 SET @2=%3,@4=%5,@6=%7,@8=%9,@10=%11,@12=%13,@14=%15,@16=%17,@18=%19,@20=%21,@22=%23 WHERE @24 = %25 LIMIT 1', $SQL->prefix.'sessions', 'server', $session_->server, 'mode', $session_->mode, 'type', $session_->type, 'status', $session_->status, 'settings', serialize($session_->settings), 'user_login', $session_->user_login, 'user_displayname', $session_->user_displayname, 'servers', serialize($session_->servers), 'applications', serialize($data), 'start_time', $session_->start_time, 'timestamp', time(), 'id', $id);
 
 		return true;
 	}
@@ -170,7 +174,13 @@ class Abstract_Session {
 		$buf->user_login = (string)$user_login;
 		$buf->user_displayname = (string)$user_displayname;
 		$buf->servers = unserialize($servers);
-		$buf->running_applications = unserialize($applications);
+		
+		$data = unserialize($applications);
+		if (array_key_exists('applications', $data))
+			$buf->setPublishedApplications($data['applications']);
+		if (array_key_exists('runnig_applications', $data))
+			$buf->running_applications = $data['runnig_applications'];
+		
 		$buf->start_time = (string)$start_time;
 		$buf->timestamp = (int)$timestamp;
 

SessionManager/web/admin/sessions.php
@@ -5,6 +5,7 @@
  * Author Laurent CLOUET <laurent@ulteo.com> 2010
  * Author Jeremy DESVAGES <jeremy@ulteo.com> 2008
  * Author David PHAM-VAN <d.pham-van@ulteo.com> 2012
+ * Author Julien LANGLOIS <julien@ulteo.com> 2012
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -120,6 +121,26 @@ if (isset($_GET['info'])) {
 			echo '</table>';
 			echo '</ul>';
 		}
+		
+		echo '<div>';
+		echo '<h2>'._('Published applications').'</h2>';
+		
+		$available_apps = $session->getPublishedApplications();
+		if (count($available_apps) == 0)
+			echo _('No published application');
+		else {
+			echo '<ul>';
+			echo '<table>';
+			foreach ($available_apps as $app_id => $myapp) {
+				echo '<tr><td>';
+				echo '<img class="icon32" src="media/image/cache.php?id='.$myapp->getAttribute('id').'" alt="" title="" /> <a href="applications.php?action=manage&id='.$myapp->getAttribute('id').'">'.$myapp->getAttribute('name').'</a>';
+				echo '</td></tr>';
+			}
+			
+			echo '</table>';
+			echo '</ul>';
+		}
+		echo '</div>';
 	}
 
 	echo '<h2>'._('Kill this session').'</h2>';

SessionManager/web/classes/Session.class.php
@@ -60,6 +60,7 @@ class Session {
 	public $start_time = 0;
 	public $timestamp = 0;
 	public $servers = array();
+	public $published_applications = array();
 	public $running_applications = array();
 
 	public function __construct($id_) {
@@ -558,5 +559,13 @@ class Session {
 		$this->running_applications[$fqdn_] = $running_apps_;
 
 		return true;
+	}
+	
+	public function setPublishedApplications($applications_) {
+		$this->published_applications = $applications_;
+	}
+	
+	public function getPublishedApplications() {
+		return $this->published_applications;
 	}
 }

SessionManager/web/client/remote_apps.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * Copyright (C) 2010 Ulteo SAS
+ * Copyright (C) 2010-2012 Ulteo SAS
  * http://www.ulteo.com
  * Author Laurent CLOUET <laurent@ulteo.com> 2010
  * Author Jeremy DESVAGES <jeremy@ulteo.com> 2010
+ * Author Julien LANGLOIS <julien@ulteo.com> 2012
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -104,7 +105,7 @@ foreach ($session->servers[Server::SERVER_ROLE_APS] as $fqdn => $data) {
 	$server_node->setAttribute('fqdn', $server->getAttribute('external_name'));
 	$server_node->setAttribute('login', $session->settings['aps_access_login']);
 	$server_node->setAttribute('password', $session->settings['aps_access_password']);
-	foreach ($user->applications() as $application) {
+	foreach ($session->getPublishedApplications() as $application) {
 		if ($application->getAttribute('type') != $server->getAttribute('type'))
 			continue;
 

SessionManager/web/client/start.php
@@ -241,6 +241,7 @@ if (isset($old_session_id)) {
 		throw_response(INTERNAL_ERROR);
 	}
 	$session->setStatus(Session::SESSION_STATUS_CREATED);
+	$session->setPublishedApplications($sessionManagement->applications);
 
 	Logger::info('main', '(client/start) Creating new session for '.$user->getAttribute('login').' ('.$session->id.')');
 }
@@ -273,7 +274,7 @@ if (isset($_REQUEST['start_apps']) && is_array($_REQUEST['start_apps'])) {
 			throw_response(SERVICE_NOT_AVAILABLE);
 		}
 
-		$apps = $user->applications();
+		$apps = $session->getPublishedApplications();
 
 		$ok = false;
 		foreach ($apps as $user_app) {
@@ -439,7 +440,7 @@ if (! isset($old_session_id)) {
 			}
 		}
 
-		foreach ($user->applications() as $application) {
+		foreach ($session->getPublishedApplications() as $application) {
 			if ($application->getAttribute('type') != $server->getAttribute('type'))
 				continue;
 
@@ -546,7 +547,7 @@ if ($session->mode == Session::MODE_DESKTOP) {
 	$server_node->setAttribute('fqdn', $server->getAttribute('external_name'));
 	$server_node->setAttribute('login', $user_login_aps);
 	$server_node->setAttribute('password', $user_password_aps);
-	foreach ($user->applications() as $application) {
+	foreach ($session->getPublishedApplications() as $application) {
 		if ($application->getAttribute('type') != $server->getAttribute('type'))
 			continue;
 
@@ -588,7 +589,7 @@ if ($session->mode == Session::MODE_DESKTOP) {
 		$server_node->setAttribute('login', $user_login_aps);
 		$server_node->setAttribute('password', $user_password_aps);
 
-		foreach ($user->applications() as $application) {
+		foreach ($session->getPublishedApplications() as $application) {
 			if ($application->getAttribute('type') != $server->getAttribute('type'))
 				continue;
 

SessionManager/web/modules/SessionManagement.php
@@ -30,6 +30,7 @@ abstract class SessionManagement extends Module {
 	public $desktop_server = false;
 	public $servers = array();
 	public $credentials = array();
+	public $applications = array();
 
 	public static function getInstance() {
 		if (is_null(self::$instance)) {
@@ -545,7 +546,9 @@ abstract class SessionManagement extends Module {
 			// We take the desktop servers applications first
 			$servers[]= $this->desktop_server;
 			
-			$this->removeApplicationsAvailableOnServer($applications, $this->desktop_server);
+			$application_choosen = $this->removeApplicationsAvailableOnServer($applications, $this->desktop_server);
+			foreach($application_choosen as $application)
+				$this->applications[$application->getAttribute('id')] = $application;
 		}
 		
 		foreach($servers_ordered as $fqdn => $server) {
@@ -553,8 +556,12 @@ abstract class SessionManagement extends Module {
 				break;
 			
 			$application_choosen = $this->removeApplicationsAvailableOnServer($applications, $server);
-			if (count($application_choosen) > 0)
-				$servers[$fqdn] = $server;
+			if (count($application_choosen) == 0)
+				continue;
+			
+			$servers[$fqdn] = $server;
+			foreach($application_choosen as $application)
+				$this->applications[$application->getAttribute('id')] = $application;
 		}
 		
 		$remote_desktop_settings = $this->user->getSessionSettings('session_settings_defaults');

commit df4996a0e2fce716c55e3e296b76c9effa8822df

Typo (introduced in r8157)
git-svn-id: file:///home/code/svn/ovd/trunk@8158 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

SessionManager/web/classes/abstract/Abstract_Session.class.php
@@ -111,7 +111,7 @@ class Abstract_Session {
 		
 		$data = array();
 		$data['applications'] = $session_->getPublishedApplications();
-		$data['runnig_applications'] = $session_->running_applications;
+		$data['running_applications'] = $session_->running_applications;
 
 		$SQL->DoQuery('UPDATE @1 SET @2=%3,@4=%5,@6=%7,@8=%9,@10=%11,@12=%13,@14=%15,@16=%17,@18=%19,@20=%21,@22=%23 WHERE @24 = %25 LIMIT 1', $SQL->prefix.'sessions', 'server', $session_->server, 'mode', $session_->mode, 'type', $session_->type, 'status', $session_->status, 'settings', serialize($session_->settings), 'user_login', $session_->user_login, 'user_displayname', $session_->user_displayname, 'servers', serialize($session_->servers), 'applications', serialize($data), 'start_time', $session_->start_time, 'timestamp', time(), 'id', $id);
 
@@ -178,8 +178,8 @@ class Abstract_Session {
 		$data = unserialize($applications);
 		if (array_key_exists('applications', $data))
 			$buf->setPublishedApplications($data['applications']);
-		if (array_key_exists('runnig_applications', $data))
-			$buf->running_applications = $data['runnig_applications'];
+		if (array_key_exists('running_applications', $data))
+			$buf->running_applications = $data['running_applications'];
 		
 		$buf->start_time = (string)$start_time;
 		$buf->timestamp = (int)$timestamp;