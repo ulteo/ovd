ulteo/ovd

commit 3b16051d624f726c46b447c01640767ccddb3bba

Bugfix #255: Java cannot give the focus to SeamlessWindow when the application is launched from a desktop shortcut. This is because OVDIntegratedLauncher is the focus owner. OVDIntegratedLauncher have to allow java to give the focus.
git-svn-id: file:///home/code/svn/ovd/trunk@8084 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

client/OVDIntegratedLauncher/main.c
@@ -1,7 +1,7 @@
 /*
- * Copyright (C) 2009 Ulteo SAS
+ * Copyright (C) 2009-2012 Ulteo SAS
  * http://www.ulteo.com
- * Author Thomas MOUTON <thomas@ulteo.com> 2010
+ * Author Thomas MOUTON <thomas@ulteo.com> 2010-2012
  * Author Julien LANGLOIS <julien@ulteo.com> 2010
  *
  * This program is free software; you can redistribute it and/or
@@ -40,6 +40,48 @@
 
 #define OVDIntegratedClientPath "ulteo\\ovd\\remoteApps"
 
+// WINDOWS FOCUS HACK BEGIN
+#define JVM_PID_FILENAME "pid"
+
+BOOL getJVM_PID_FilePath(LPSTR path, LPSTR arg) {
+	if (getRemoteAppsPath(path, arg) == FALSE)
+		return FALSE;
+
+	if (PathAppend(path, JVM_PID_FILENAME) == FALSE)
+		return FALSE;
+
+	return TRUE;
+}
+
+BOOL allowJVMToSetFocus(LPSTR arg) {
+	TCHAR pidFile[MAX_PATH];
+	FILE *fd;
+	int pid = 0;
+	int ret;
+	
+	if (getJVM_PID_FilePath(pidFile, arg) == FALSE)
+		return FALSE;
+	
+	fd = fopen(pidFile, "r");
+	if (! fd) {
+		fprintf(stderr, "[ERROR] JVM pid file does not exists.\n", fd);
+		return FALSE;
+	}
+
+	ret = fscanf(fd, "%d", &pid);
+	fclose(fd);
+	
+	if (ret != 1 || pid == 0) {
+		fprintf(stderr, "[ERROR] Failed to get the JVM pid.\n", fd);
+		return FALSE;
+	}
+
+	AllowSetForegroundWindow(pid);
+	
+	return TRUE;
+}
+// WINDOWS FOCUS HACK END
+
 #else //POSIX
 
 #include <string.h>
@@ -47,6 +89,10 @@
 #include <unistd.h>
 
 #define OVDIntegratedClientPath ".ulteo/ovd/remoteApps"
+
+BOOL allowJVMToSetFocus(LPSTR arg) {
+	return TRUE;
+}
 
 #endif //WINDOWS/POSIX
 
@@ -88,6 +134,8 @@ int main(int argc, LPSTR argv[]) {
     if (argc > 3) {
         args = argv[3];
     }
+    
+    allowJVMToSetFocus(argv[1]);
 
     instance = spool_instance_create(spool, argv[2], args);
     if (instance == -1) {

client/java/src/org/ulteo/ovd/integrated/Spool.java
@@ -1,7 +1,7 @@
 /*
- * Copyright (C) 2010-2011 Ulteo SAS
+ * Copyright (C) 2010-2012 Ulteo SAS
  * http://www.ulteo.com
- * Author Thomas MOUTON <thomas@ulteo.com> 2010
+ * Author Thomas MOUTON <thomas@ulteo.com> 2010-2012
  * Author Samuel BOVEE <samuel@ulteo.com> 2011
  *
  * This program is free software; you can redistribute it and/or
@@ -25,6 +25,7 @@ import org.ulteo.ovd.ApplicationInstance;
 import org.ulteo.ovd.Application;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -37,6 +38,7 @@ import org.ulteo.gui.SwingTools;
 import org.ulteo.ovd.client.OvdClient;
 import org.ulteo.utils.FilesOp;
 import org.ulteo.utils.I18n;
+import org.ulteo.utils.SystemUtils;
 
 public class Spool extends Thread {
 	private final static String PREFIX_ID = "id = ";
@@ -53,6 +55,22 @@ public class Spool extends Thread {
 		this.id = UUID.randomUUID().toString();
 	}
 
+	private void savePID(File baseDirecotory_) {
+		int jvm_pid = SystemUtils.getPID();
+		File f = new File(baseDirecotory_.getPath()+File.separator+"pid");
+		if (f.exists())
+			f.delete();
+		
+		try {
+			FileWriter fw = new FileWriter(f);
+			fw.write(""+jvm_pid);
+			fw.flush();
+			fw.close();
+		} catch (IOException ex) {
+			org.ulteo.Logger.error("Failed to store JVM PID to '"+f.getAbsolutePath()+"': "+ex.getMessage());
+		}
+	}
+
 	@Override
 	public void run() {
 		File baseDirectory = new File(Constants.PATH_REMOTE_APPS + File.separator + this.id);
@@ -64,6 +82,9 @@ public class Spool extends Thread {
 				new File(Constants.PATH_ICONS), new File(Constants.PATH_SHORTCUTS) };
 		for (File f: toCreateDirs)
 			f.mkdirs();
+		
+		if (OSTools.isWindows())
+			this.savePID(baseDirectory);
 
 		this.logger.info("Spool thread started");
 

commit 36e12fb792179b9c1a35fc4f63ce35da71feff71

Bugfix GCC 4.6 compilation errors (introduced in r8084)
git-svn-id: file:///home/code/svn/ovd/trunk@8197 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

client/OVDIntegratedLauncher/main.c
@@ -2,7 +2,7 @@
  * Copyright (C) 2009-2012 Ulteo SAS
  * http://www.ulteo.com
  * Author Thomas MOUTON <thomas@ulteo.com> 2010-2012
- * Author Julien LANGLOIS <julien@ulteo.com> 2010
+ * Author Julien LANGLOIS <julien@ulteo.com> 2010, 2012
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -20,6 +20,10 @@
  */
 
 #ifdef WIN32 //WINDOWS
+
+#ifndef WINVER
+#define WINVER 0x0500
+#endif //WINVER
 
 #ifndef _WIN32_IE
 #define _WIN32_IE 0x0500