ulteo/ovd

commit 91eefd7bf1e75bb1c0c88554b614db5e4fa0ce36

Improvement: no longer return the command output as default behavior
git-svn-id: file:///home/code/svn/ovd/trunk@8191 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

OvdServer/ovd/Role/FileServer/Share.py
@@ -76,27 +76,27 @@ class Share:
 	
 	def delete(self):
 		cmd = "rm -rf %s"%(self.directory)
-		s, o = commands.getstatusoutput(cmd)
-		if s is not 0:
+		p = commands.execute(cmd)
+		if p.returncode is not 0:
 			Logger.error("FS: unable to del share")
-			Logger.debug("FS: command '%s' return %d: %s"%(cmd, s, o.decode("UTF-8")))
+			Logger.debug("FS: command '%s' return %d: %s"%(cmd, p.returncode, p.stdout.read().decode("UTF-8")))
 		
 		return s == 0
 	
 	
 	def enable(self):
 		cmd = "groupadd  %s"%(self.group)
-		s, o = commands.getstatusoutput(cmd)
-		if s is not 0:
+		p = commands.execute(cmd)
+		if p.returncode is not 0:
 			Logger.error("FS: unable to create group")
-			Logger.debug("FS: command '%s' return %d: %s"%(cmd, s, o.decode("UTF-8")))
+			Logger.debug("FS: command '%s' return %d: %s"%(cmd, p.returncode, p.stdout.read().decode("UTF-8")))
 			return False
 		
 		cmd = 'net usershare add %s "%s" %s %s:f,Everyone:d'%(self.name, self.directory, self.name, self.group)
-		s, o = commands.getstatusoutput(cmd)
-		if s is not 0:
+		p = commands.execute(cmd)
+		if p.returncode is not 0:
 			Logger.error("FS: unable to add share")
-			Logger.debug("FS: command '%s' return %d: %s"%(cmd, s, o.decode("UTF-8")))
+			Logger.debug("FS: command '%s' return %d: %s"%(cmd, p.returncode, p.stdout.read().decode("UTF-8")))
 			return False
 		
 		path = os.path.join(self.directory, ".htaccess")
@@ -132,20 +132,20 @@ class Share:
 		
 		
 		cmd = "net usershare delete %s"%(self.name)
-		s, o = commands.getstatusoutput(cmd)
+		p = commands.execute(cmd)
 		ret = True
-		if s is not 0:
+		if p.returncode is not 0:
 			ret = False
 			Logger.error("FS: unable to del share")
-			Logger.debug("FS: command '%s' return %d: %s"%(cmd, s, o.decode("UTF-8")))
+			Logger.debug("FS: command '%s' return %d: %s"%(cmd, p.returncode, p.stdout.read().decode("UTF-8")))
 		
 		
 		cmd = "groupdel  %s"%(self.group)
-		s, o = commands.getstatusoutput(cmd)
-		if s is not 0:
+		p = commands.execute(cmd)
+		if p.returncode is not 0:
 			ret = False
 			Logger.error("FS: unable to del group")
-			Logger.debug("FS: command '%s' return %d: %s"%(cmd, s, o.decode("UTF-8")))
+			Logger.debug("FS: command '%s' return %d: %s"%(cmd, p.returncode, p.stdout.read().decode("UTF-8")))
 		
 		self.do_right_normalization()
 		
@@ -155,14 +155,14 @@ class Share:
 	
 	def do_right_normalization(self):
 		cmd = 'chown -R %s:%s "%s"'%(Config.uid, Config.gid, self.directory)
-		s, o = commands.getstatusoutput(cmd)
-		if s is not 0:
-			Logger.debug("FS: following command '%s' returned %d => %s"%(cmd, s, o))
+		p = commands.execute(cmd)
+		if p.returncode is not 0:
+			Logger.debug("FS: following command '%s' returned %d => %s"%(cmd, p.returncode, p.stdout.read()))
 		
 		cmd = 'chmod -R u=rwX,g=rwX,o-rwx "%s"'%(self.directory)
-		s, o = commands.getstatusoutput(cmd)
-		if s is not 0:
-			Logger.debug("FS: following command '%s' returned %d => %s"%(cmd, s, o))
+		p = commands.execute(cmd)
+		if p.returncode is not 0:
+			Logger.debug("FS: following command '%s' returned %d => %s"%(cmd, p.returncode, p.stdout.read()))
 	
 	
 	def add_user(self, user):
@@ -170,10 +170,10 @@ class Share:
 			self.enable()
 		
 		cmd = "adduser %s %s"%(user, self.group)
-		s, o = commands.getstatusoutput(cmd)
-		if s is not 0:
+		p = commands.execute(cmd)
+		if p.returncode is not 0:
 			Logger.error("FS: unable to add user in group")
-			Logger.debug("FS: command '%s' return %d: %s"%(cmd, s, o.decode("UTF-8")))
+			Logger.debug("FS: command '%s' return %d: %s"%(cmd, p.returncode, p.stdout.read().decode("UTF-8")))
 			return False
 		
 		self.users.append(user)
@@ -186,11 +186,11 @@ class Share:
 		
 		ret = True
 		cmd = "deluser %s %s"%(user, self.group)
-		s, o = commands.getstatusoutput(cmd)
-		if s is not 0:
+		p = commands.execute(cmd)
+		if p.returncode is not 0:
 			ret = False
 			Logger.error("FS: unable to del user in group")
-			Logger.debug("FS: command '%s' return %d: %s"%(cmd, s, o.decode("UTF-8")))
+			Logger.debug("FS: command '%s' return %d: %s"%(cmd, p.returncode, p.stdout.read().decode("UTF-8")))
 		
 		self.users.remove(user)
 		if len(self.users) == 0:

OvdServer/ovd/Platform/Linux/System.py
@@ -225,9 +225,9 @@ class System(AbstractSystem):
 	def groupCreate(name_):
 		cmd = "groupadd %s"%(name_)
 		
-		s,o = commands.getstatusoutput(cmd)
-		if s != 0:
-			Logger.error("groupCreate return %d (%s)"%(s, o))
+		p = commands.execute(cmd)
+		if p.returncode != 0:
+			Logger.error("groupCreate return %d (%s)"%(p.returncode, p.stdout.read()))
 			return False
 		
 		return True
@@ -258,11 +258,11 @@ class System(AbstractSystem):
 	def userRemove(name_):
 		cmd = "userdel --force  --remove %s"%(name_)
 		
-		s,o = commands.getstatusoutput(cmd)
-		if s == 3072:
-			Logger.debug("mail dir error: '%s' return %d => %s"%(cmd, s, o))
-		elif s != 0:
-			Logger.error("userRemove return %d (%s)"%(s, o))
+		p = commands.execute(cmd)
+		if p.returncode == 3072:
+			Logger.debug("mail dir error: '%s' return %d => %s"%(cmd, p.returncode, p.stdout.read()))
+		elif p.returncode != 0:
+			Logger.error("userRemove return %d (%s)"%(p.returncode, p.stdout.read()))
 			return False
 		
 		return True
@@ -271,22 +271,22 @@ class System(AbstractSystem):
 	@staticmethod
 	def userAdd(login_, displayName_, password_, groups_):
 		cmd = "useradd -m -k /dev/null %s"%(login_)
-		s,o = commands.getstatusoutput(cmd)
-		if s != 0:
-			Logger.error("userAdd return %d (%s)"%(s, o))
+		p = commands.execute(cmd)
+		if p.returncode != 0:
+			Logger.error("userAdd return %d (%s)"%(p.returncode, p.stdout.read()))
 			return False
 		
 		cmd = 'echo "%s:%s" | chpasswd'%(login_, password_)
-		s,o = commands.getstatusoutput(cmd)
-		if s != 0:
-			Logger.error("userAdd return %d (%s)"%(s, o))
+		p = commands.execute(cmd)
+		if p.returncode != 0:
+			Logger.error("userAdd return %d (%s)"%(p.returncode, p.stdout.read()))
 			return False
 		
 		for group in groups_:
 			cmd = "adduser %s %s"%(login_, group)
-			s,o = commands.getstatusoutput(cmd)
-			if s != 0:
-				Logger.error("userAdd return %d (%s)"%(s, o))
+			p = commands.execute(cmd)
+			if p.returncode != 0:
+				Logger.error("userAdd return %d (%s)"%(p.returncode, p.stdout.read()))
 				return False
 		
 		return True

OvdServer/ovd/Role/ApplicationServer/Apt.py
@@ -1,8 +1,8 @@
 # -*- coding: UTF-8 -*-
 
-# Copyright (C) 2009-2011 Ulteo SAS
+# Copyright (C) 2009-2012 Ulteo SAS
 # http://www.ulteo.com
-# Author Julien LANGLOIS <julien@ulteo.com> 2009, 2011
+# Author Julien LANGLOIS <julien@ulteo.com> 2009, 2011, 2012
 # Author David LECHEVALIER <david@ulteo.com> 2011
 # Author Samuel BOVEE <samuel@ulteo.com> 2011
 #
@@ -46,7 +46,7 @@ class Apt(Thread):
 		if not os.path.exists(self.directory):
 			os.makedirs(self.directory)
 		else:
-			s,o = commands.getstatusoutput("rm -rf %s/*"%(self.directory))
+			commands.execute("rm -rf %s/*"%(self.directory))
 	
 	
 	def add(self, request):
@@ -138,13 +138,13 @@ class Request_Packages(Request):
 			command = "autoremove --purge "+" ".join(self.packages)
 		
 		cmd = "apt-get update >>%s/stdout 2>>%s/stderr"%(self.directory, self.directory)
-		ret,o = commands.getstatusoutput(cmd)
-		if ret != 0:
+		p = commands.execute(cmd)
+		if p.returncode != 0:
 			return False
 		
 		cmd = "DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical DEBCONF_NONINTERACTIVE_SEEN=true apt-get --yes --force-yes --option DPkg::Options::=--force-confold %s >>%s/stdout 2>>%s/stderr"%(command, self.directory, self.directory)
-		ret, o = commands.getstatusoutput(cmd)
-		if ret != 0:
+		p = commands.execute(cmd)
+		if p.returncode != 0:
 			return False
 		
 		return True

OvdServer/ovd/Role/ApplicationServer/Platform/Linux/ApplicationsDetection.py
@@ -3,7 +3,7 @@
 # Copyright (C) 2008-2012 Ulteo SAS
 # http://www.ulteo.com
 # Author Laurent CLOUET <laurent@ulteo.com> 2010
-# Author Julien LANGLOIS <julien@ulteo.com> 2008, 2010, 2011
+# Author Julien LANGLOIS <julien@ulteo.com> 2008, 2010, 2011, 2012
 # Author David PHAM-VAN <d.pham-van@ulteo.com> 2012
 #
 # This program is free software; you can redistribute it and/or 
@@ -143,9 +143,9 @@ class ApplicationsDetection():
 		
 		bufFile = tempfile.mktemp(".png")		
 		cmd = 'convert -resize 32x32 "%s" "%s"'%(iconPath, bufFile)
-		s,o = commands.getstatusoutput(cmd)
-		if s != 0:
-			Logger.debug("getIcon cmd '%s' returned (%d): %s"%(cmd, s, o))
+		p = commands.execute(cmd)
+		if p.returncode != 0:
+			Logger.debug("getIcon cmd '%s' returned (%d): %s"%(cmd, p.returncode, p.stdout.read()))
 			Logger.error("getIcon: imagemagick error")
 			if os.path.exists(bufFile):
 				os.remove(bufFile)
@@ -167,10 +167,11 @@ class ApplicationsDetection():
 			
 			cmd = 'dpkg -S "%s"'%(application["filename"])
 			
-			status,out = commands.getstatusoutput(cmd)
-			if status != 0:
+			p = commands.execute(cmd)
+			if p.returncode != 0:
 				continue
 			
+			out = p.stdout.read()
 			if not ":" in out:
 				continue
 			

OvdServer/ovd/Role/ApplicationServer/Platform/Linux/Profile.py
@@ -3,7 +3,7 @@
 # Copyright (C) 2010-2012 Ulteo SAS
 # http://www.ulteo.com
 # Author Laurent CLOUET <laurent@ulteo.com> 2010
-# Author Julien LANGLOIS <julien@ulteo.com> 2010, 2011
+# Author Julien LANGLOIS <julien@ulteo.com> 2010, 2011, 2012
 # Author David LECHEVALIER <david@ulteo.com> 2012
 #
 # This program is free software; you can redistribute it and/or 
@@ -52,10 +52,10 @@ class Profile(AbstractProfile):
 			cmd = "mount -t cifs -o username=%s,password=%s,uid=%s,gid=0,umask=077 //%s/%s %s"%(self.profile["login"], self.profile["password"], self.session.user.name, self.profile["server"], self.profile["dir"], self.profile_mount_point)
 			cmd = self.transformToLocaleEncoding(cmd)
 			Logger.debug("Profile mount command: '%s'"%(cmd))
-			s,o = commands.getstatusoutput(cmd)
-			if s != 0:
+			p = commands.execute(cmd)
+			if p.returncode != 0:
 				Logger.error("Profile mount failed")
-				Logger.debug("Profile mount failed (status: %d) => %s"%(s, o))
+				Logger.debug("Profile mount failed (status: %d) => %s"%(p.returncode, p.stdout.read()))
 				os.rmdir(self.profile_mount_point)
 			else:
 				self.profileMounted = True
@@ -69,10 +69,10 @@ class Profile(AbstractProfile):
 			cmd = "mount -t cifs -o username=%s,password=%s,uid=%s,gid=0,umask=077 //%s/%s %s"%(sharedFolder["login"], sharedFolder["password"], self.session.user.name, sharedFolder["server"], sharedFolder["dir"], dest)
 			cmd = self.transformToLocaleEncoding(cmd)
 			Logger.debug("Profile, sharedFolder mount command: '%s'"%(cmd))
-			s,o = commands.getstatusoutput(cmd)
-			if s != 0:
+			p = commands.execute(cmd)
+			if p.returncode != 0:
 				Logger.error("Profile sharedFolder mount failed")
-				Logger.debug("Profile sharedFolder mount failed (status: %d) => %s"%(s, o))
+				Logger.debug("Profile sharedFolder mount failed (status: %d) => %s"%(p.returncode, p.stdout.read()))
 				os.rmdir(dest)
 			else:
 				sharedFolder["mountdest"] = dest
@@ -90,10 +90,10 @@ class Profile(AbstractProfile):
 				cmd = "mount -o bind \"%s\" \"%s\""%(dest, dst)
 				cmd = self.transformToLocaleEncoding(cmd)
 				Logger.debug("Profile bind dir command '%s'"%(cmd))
-				s,o = commands.getstatusoutput(cmd)
-				if s != 0:
+				p = commands.execute(cmd)
+				if p.returncode != 0:
 					Logger.error("Profile bind dir failed")
-					Logger.error("Profile bind dir failed (status: %d) %s"%(s, o))
+					Logger.error("Profile bind dir failed (status: %d) %s"%(p.returncode, p.stdout.read()))
 				else:
 					sharedFolder["local_path"] = dst
 					self.folderRedirection.append(dst)
@@ -117,10 +117,10 @@ class Profile(AbstractProfile):
 				cmd = "mount -o bind \"%s\" \"%s\""%(src, dst)
 				cmd = self.transformToLocaleEncoding(cmd)
 				Logger.debug("Profile bind dir command '%s'"%(cmd))
-				s,o = commands.getstatusoutput(cmd)
-				if s != 0:
+				p = commands.execute(cmd)
+				if p.returncode != 0:
 					Logger.error("Profile bind dir failed")
-					Logger.error("Profile bind dir failed (status: %d) %s"%(s, o))
+					Logger.error("Profile bind dir failed (status: %d) %s"%(p.returncode, p.stdout.read()))
 				else:
 					self.folderRedirection.append(dst)
 			
@@ -141,20 +141,20 @@ class Profile(AbstractProfile):
 			cmd = "umount \"%s\""%(d)
 			cmd = self.transformToLocaleEncoding(cmd)
 			Logger.debug("Profile bind dir command: '%s'"%(cmd))
-			s,o = commands.getstatusoutput(cmd)
-			if s != 0:
+			p = commands.execute(cmd)
+			if p.returncode != 0:
 				Logger.error("Profile bind dir failed")
-				Logger.error("Profile bind dir failed (status: %d) %s"%(s, o))
+				Logger.error("Profile bind dir failed (status: %d) %s"%(p.returncode, p.stdout.read()))
 		
 		for sharedFolder in self.sharedFolders:
 			if sharedFolder.has_key("mountdest"):
 				cmd = """umount "%s" """%(sharedFolder["mountdest"])
 				cmd = self.transformToLocaleEncoding(cmd)
 				Logger.debug("Profile sharedFolder umount dir command: '%s'"%(cmd))
-				s,o = commands.getstatusoutput(cmd)
-				if s != 0:
+				p = commands.execute(cmd)
+				if p.returncode != 0:
 					Logger.error("Profile sharedFolder umount dir failed")
-					Logger.error("Profile sharedFolder umount dir failed (status: %d) %s"%(s, o))
+					Logger.error("Profile sharedFolder umount dir failed (status: %d) %s"%(p.returncode, p.stdout.read()))
 				
 				os.rmdir(sharedFolder["mountdest"])
 		
@@ -162,10 +162,10 @@ class Profile(AbstractProfile):
 			cmd = "umount %s"%(self.profile_mount_point)
 			cmd = self.transformToLocaleEncoding(cmd)
 			Logger.debug("Profile umount command: '%s'"%(cmd))
-			s,o = commands.getstatusoutput(cmd)
-			if s != 0:
+			p = commands.execute(cmd)
+			if p.returncode != 0:
 				Logger.error("Profile umount failed")
-				Logger.debug("Profile umount failed (status: %d) => %s"%(s, o))
+				Logger.debug("Profile umount failed (status: %d) => %s"%(p.returncode, p.stdout.read()))
 			
 			try:
 				os.rmdir(self.profile_mount_point)
@@ -190,10 +190,10 @@ class Profile(AbstractProfile):
 		cmd = self.getRsyncMethod(d, self.homeDir, True)
 		Logger.debug("rsync cmd '%s'"%(cmd))
 		
-		s,o = commands.getstatusoutput(cmd)
-		if s is not 0:
+		p = commands.execute(cmd)
+		if p.returncode is not 0:
 			Logger.error("Unable to copy conf from profile")
-			Logger.debug("Unable to copy conf from profile, cmd '%s' return %d: %s"%(cmd, s, o))
+			Logger.debug("Unable to copy conf from profile, cmd '%s' return %d: %s"%(cmd, p.returncode, p.stdout.read()))
 	
 	
 	def copySessionStop(self):
@@ -212,10 +212,10 @@ class Profile(AbstractProfile):
 		cmd = self.getRsyncMethod(self.homeDir, d)
 		Logger.debug("rsync cmd '%s'"%(cmd))
 		
-		s,o = commands.getstatusoutput(cmd)
-		if s is not 0:
+		p = commands.execute(cmd)
+		if p.returncode is not 0:
 			Logger.error("Unable to copy conf to profile")
-			Logger.debug("Unable to copy conf to profile, cmd '%s' return %d: %s"%(cmd, s, o))
+			Logger.debug("Unable to copy conf to profile, cmd '%s' return %d: %s"%(cmd, p.returncode, p.stdout.read()))
 	
 	
 	@staticmethod

OvdServer/ovd/Role/ApplicationServer/Platform/Linux/User.py
@@ -71,22 +71,22 @@ class User(AbstractUser):
 			if retry < 0:
 				  Logger.error("ERROR: unable to add a new user")
 			lock.acquire()
-			s,o = commands.getstatusoutput(System.local_encode(cmd))
+			p = commands.execute(System.local_encode(cmd))
 			lock.release()
-			if s == 0:
+			if p.returncode == 0:
 				break
 			
 			Logger.debug("Add user :retry %i"%(6-retry))
-			if s == 2304: # user already exist
+			if p.returncode == 2304: # user already exist
 				Logger.error("User %s already exist"%(self.name))
 				break;
-			if s == 256: # an other process is creating a user
+			if p.returncode == 256: # an other process is creating a user
 				Logger.error("An other process is creating a user")
 				retry -=1
 				time.sleep(0.2)
 				continue
-			if s != 0:
-				Logger.error("UserAdd return %d (%s)"%(s, o))
+			if p.returncode != 0:
+				Logger.error("UserAdd return %d (%s)"%(p.returncode, p.stdout.read()))
 				return False
 		
 		
@@ -98,19 +98,19 @@ class User(AbstractUser):
 					Logger.error("ERROR: unable to add a new user")
 					return False
 				lock.acquire()
-				s,o = commands.getstatusoutput(cmd)
+				p = commands.execute(cmd)
 				lock.release()
-				if s == 0:
+				if p.returncode == 0:
 					break
 				
 				Logger.debug("Chpasswd of %s:retry %i"%(self.name, 6-retry))
-				if s == 256 or s == 2560: # an other process is creating a user
+				if p.returncode == 256 or p.returncode == 2560: # an other process is creating a user
 					Logger.debug("An other process is creating a user")
 					retry -=1
 					time.sleep(0.2)
 					continue
-				if s != 0:
-					Logger.error("chpasswd return %d (%s)"%(s, o))
+				if p.returncode != 0:
+					Logger.error("chpasswd return %d (%s)"%(p.returncode, p.stdout.read()))
 					return False
 		
 		return self.post_create()
@@ -147,22 +147,22 @@ class User(AbstractUser):
 		retry = 5
 		while retry !=0:
 			lock.acquire()
-			s,o = commands.getstatusoutput(cmd)
+			p = commands.execute(cmd)
 			lock.release()
-			if s == 0:
+			if p.returncode == 0:
 				return True
-			if s == 12:
-				Logger.debug("mail dir error: '%s' return %d => %s"%(str(cmd), s, o))
+			if p.returncode == 12:
+				Logger.debug("mail dir error: '%s' return %d => %s"%(str(cmd), p.returncode, p.stdout.read()))
 				return True
 			
 			Logger.debug("User delete of %s: retry %i"%(self.name, 6-retry))
-			if s == 256 or s == 2560: # an other process is creating a user
+			if p.returncode == 256 or s == 2560: # an other process is creating a user
 				Logger.debug("An other process is creating a user")
 				retry -=1
 				time.sleep(0.2)
 				continue
-			if s != 0:
-				Logger.error("userdel return %d (%s)"%(s, o))
+			if p.returncode != 0:
+				Logger.error("userdel return %d (%s)"%(p.returncode, p.stdout.read()))
 				return False
 		
 		return True

OvdServer/ovd/Role/FileServer/Role.py
@@ -99,9 +99,9 @@ class Role(AbstractRole):
 		
 		for usershare in self.get_enabled_usershares():
 			Logger.debug("FileServer:: Removing samba usershare '%s'"%(usershare))
-			s, o = commands.getstatusoutput("net usershare delete %s"%(usershare))
-			if s is not 0:
-				Logger.error("FS: unable to 'net usershare delete': %d => %s"%(s, o))
+			p = commands.execute("net usershare delete %s"%(usershare))
+			if p.returncode is not 0:
+				Logger.error("FS: unable to 'net usershare delete': %d => %s"%(p.returncode, p.stdout.read()))
 				ret = False
 		
 		return ret
@@ -109,15 +109,15 @@ class Role(AbstractRole):
 	
 	def cleanup_repository(self):
 		cmd = 'chown -R %s:%s "%s"'%(Config.uid, Config.gid, Config.spool)
-		s, o = commands.getstatusoutput(cmd)
-		if s is not 0:
-			Logger.debug("FS: following command '%s' returned %d => %s"%(cmd, s, o))
+		p = commands.execute(cmd)
+		if p.returncode is not 0:
+			Logger.debug("FS: following command '%s' returned %d => %s"%(cmd, p.returncode, p.stdout.read()))
 			return False
 		
 		cmd = 'chmod -R u=rwX,g=rwX,o-rwx "%s"'%(Config.spool)
-		s, o = commands.getstatusoutput(cmd)
-		if s is not 0:
-			Logger.debug("FS: following command '%s' returned %d => %s"%(cmd, s, o))
+		p = commands.execute(cmd)
+		if p.returncode is not 0:
+			Logger.debug("FS: following command '%s' returned %d => %s"%(cmd, p.returncode, p.stdout.read()))
 			return False
 		
 		return True
@@ -158,12 +158,12 @@ class Role(AbstractRole):
 	
 	
 	def get_enabled_usershares(self):
-		s, o = commands.getstatusoutput("net usershare list")
-		if s is not 0:
-			Logger.error("FS: unable to 'net usershare list': %d => %s"%(s, o))
+		p = commands.execute("net usershare list")
+		if p.returncode is not 0:
+			Logger.error("FS: unable to 'net usershare list': %d => %s"%(p.returncode, p.stdout.read()))
 			return []
 		
-		names = [s.strip() for s in o.splitlines()]
+		names = [s.strip() for s in p.stdout.read().splitlines()]
 		
 		return names
 	

OvdServer/ovd/Role/FileServer/User.py
@@ -33,17 +33,17 @@ class User:
 	
 	def create(self, password):
 		cmd = "useradd -d /dev/null -s /bin/false -G %s %s"%(Config.group, self.login)
-		s,o = commands.getstatusoutput(cmd)
-		if s == 2304:
+		p = commands.execute(cmd)
+		if p.returncode == 2304:
 			Logger.warn("FS: unable to create user: already exists")
 			return False
-		elif s != 0:
+		elif p.returncode != 0:
 			Logger.error("FS: unable to create user")
-			Logger.debug("FS: command '%s' return %d: %s"%(cmd, s, o.decode("UTF-8")))
+			Logger.debug("FS: command '%s' return %d: %s"%(cmd, p.returncode, p.stdout.read().decode("UTF-8")))
 			return False
 		
 		cmd = 'smbpasswd -s -a %s'%(self.login)
-		p = commands.execute_no_wait(cmd)
+		p = commands.execute(cmd, wait = False)
 		p.stdin.write("%s\n%s\n"%(password, password))
 		p.wait()
 		
@@ -53,10 +53,10 @@ class User:
 			return False
 		
 		cmd = 'htpasswd -b %s "%s" "%s"'%(Config.dav_passwd_file, self.login, password)
-		s,o = commands.getstatusoutput(cmd)
-		if s != 0:
+		p = commands.execute(cmd)
+		if p.returncode != 0:
 			Logger.error("FS: unable to update apache auth file")
-			Logger.debug("FS: command '%s' return %d: %s"%(cmd, s, o.decode("UTF-8")))
+			Logger.debug("FS: command '%s' return %d: %s"%(cmd, p.returncode, p.stdout.read().decode("UTF-8")))
 			return False
 		
 		return True
@@ -66,24 +66,24 @@ class User:
 		ret = True
 		
 		cmd = 'htpasswd -D %s "%s"'%(Config.dav_passwd_file, self.login)
-		s,o = commands.getstatusoutput(cmd)
-		if s != 0:
+		p = commands.execute(cmd)
+		if p.returncode != 0:
 			Logger.error("FS: unable to update apache auth file")
-			Logger.debug("FS: command '%s' return %d: %s"%(cmd, s, o.decode("UTF-8")))
+			Logger.debug("FS: command '%s' return %d: %s"%(cmd, p.returncode, p.stdout.read().decode("UTF-8")))
 			return False
 		
 		cmd = 'smbpasswd -x %s'%(self.login)
-		s,o = commands.getstatusoutput(cmd)
-		if s != 0:
+		p = commands.execute(cmd)
+		if p.returncode != 0:
 			Logger.error("FS: unable to del smb password")
-			Logger.debug("FS: command '%s' return %d: %s"%(cmd, s, o.decode("UTF-8")))
+			Logger.debug("FS: command '%s' return %d: %s"%(cmd, p.returncode, p.stdout.read().decode("UTF-8")))
 			ret = False
 		
 		cmd = "userdel -f %s"%(self.login)
-		s,o = commands.getstatusoutput(cmd)
-		if s != 0:
+		p = commands.execute(cmd)
+		if p.returncode != 0:
 			Logger.error("FS: unable to del user")
-			Logger.debug("FS: command '%s' return %d: %s"%(cmd, s, o.decode("UTF-8")))
+			Logger.debug("FS: command '%s' return %d: %s"%(cmd, p.returncode, p.stdout.read().decode("UTF-8")))
 			ret = False
 		
 		return ret
@@ -91,22 +91,22 @@ class User:
 	
 	def clean(self):
 		cmd = 'htpasswd -D %s "%s"'%(Config.dav_passwd_file, self.login)
-		s,o = commands.getstatusoutput(cmd)
-		if s != 0:
+		p = commands.execute(cmd)
+		if p.returncode != 0:
 			Logger.warn("FS: unable to remove user %s in 'clean' process"%(self.login))
-			Logger.debug("FS: command '%s' return %d: %s"%(cmd, s, o.decode("UTF-8")))
+			Logger.debug("FS: command '%s' return %d: %s"%(cmd, p.returncode, p.stdout.read().decode("UTF-8")))
 		
 		cmd = 'smbpasswd -x %s'%(self.login)
-		s,o = commands.getstatusoutput(cmd)
-		if s != 0:
+		p = commands.execute(cmd)
+		if p.returncode != 0:
 			Logger.warn("FS: unable to remove user %s in 'clean' process"%(self.login))
-			Logger.debug("FS: command '%s' return %d: %s"%(cmd, s, o.decode("UTF-8")))
+			Logger.debug("FS: command '%s' return %d: %s"%(cmd, p.returncode, p.stdout.read().decode("UTF-8")))
 		
 		cmd = "userdel -f %s"%(self.login)
-		s,o = commands.getstatusoutput(cmd)
-		if s != 0:
+		p = commands.execute(cmd)
+		if p.returncode != 0:
 			Logger.warn("FS: unable to remove user %s in 'clean' process"%(self.login))
-			Logger.debug("FS: command '%s' return %d: %s"%(cmd, s, o.decode("UTF-8")))
+			Logger.debug("FS: command '%s' return %d: %s"%(cmd, p.returncode, p.stdout.read().decode("UTF-8")))
 	
 	
 	def existSomeWhere(self):
@@ -117,8 +117,8 @@ class User:
 			pass
 		
 		cmd = "smbpasswd -e %s"%(self.login)
-		s,o = commands.getstatusoutput(cmd)
-		if s == 0:
+		p = commands.execute(cmd)
+		if p.returncode == 0:
 			return True
 		
 		accessOK = False

commit 61a0ba260b6b902d063a140c65a30844946f84fe

Bugfix #551: python exception appears when deleting a profile (introduced in r8191)
git-svn-id: file:///home/code/svn/ovd/trunk@8254 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

OvdServer/ovd/Role/FileServer/Share.py
@@ -80,8 +80,9 @@ class Share:
 		if p.returncode is not 0:
 			Logger.error("FS: unable to del share")
 			Logger.debug("FS: command '%s' return %d: %s"%(cmd, p.returncode, p.stdout.read().decode("UTF-8")))
+			return False
 		
-		return s == 0
+		return True
 	
 	
 	def enable(self):