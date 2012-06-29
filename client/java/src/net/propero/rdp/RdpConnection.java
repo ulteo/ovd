ulteo/ovd

commit 0c8305956bbe9a708c27886efc67639a89949b23

Bugfix: OVDApp channel were not reset at disconnection
git-svn-id: file:///home/code/svn/ovd/trunk@8284 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

client/java/src/org/ulteo/rdp/RdpConnectionOvd.java
@@ -2,7 +2,7 @@
  * Copyright (C) 2010-2012 Ulteo SAS
  * http://www.ulteo.com
  * Author David LECHEVALIER <david@ulteo.com> 2011, 2012
- * Author Thomas MOUTON <thomas@ulteo.com> 2010
+ * Author Thomas MOUTON <thomas@ulteo.com> 2010, 2012
  * Author Arnaud LEGRAND <arnaud@ulteo.com> 2010
  * Author Julien LANGLOIS <julien@ulteo.com> 2011
  *
@@ -288,6 +288,9 @@ public class RdpConnectionOvd extends RdpConnection {
 			RdpConnectionOvd.printerManager.stop();
 			RdpConnectionOvd.printerManager = null;
 		}
+		
+		if (this.ovdAppChannel != null)
+			this.ovdAppChannel = null;
 	}
 
 	public void addApp(Application app_) {

commit 0f2c335628cc0ce552e0ba79c7b34ed1f958436c

Bugfix: Virtuals Channels were deleted before the RDP listeners were called. (Exceptions introduced by r8284)
git-svn-id: file:///home/code/svn/ovd/trunk@8291 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

client/java/src/net/propero/rdp/RdpConnection.java
@@ -622,7 +622,9 @@ public class RdpConnection implements SeamListener, Runnable{
 	public synchronized void stop() {
 		this.keep_running = false;
 		this.disconnect();
+	}
 
+	protected void closeVChannels() {
 		if (this.opt.seamlessEnabled && this.seamChannel != null) {
 			this.seamChannel.closeAllWindows();
 			this.seamChannel.clearDatas();
@@ -669,6 +671,8 @@ public class RdpConnection implements SeamListener, Runnable{
 		for(RdpListener list : this.listener) {
 			list.failed(this, this.failedMsg);
 		}
+		
+		this.closeVChannels();
 	}
 	
 	protected synchronized void fireDisconnected() {
@@ -678,6 +682,8 @@ public class RdpConnection implements SeamListener, Runnable{
 		for(RdpListener list : this.listener) {
 			list.disconnected(this);
 		}
+		
+		this.closeVChannels();
 	}
 	
 	protected void fireSeamlessEnabled() {

client/java/src/org/ulteo/rdp/RdpConnectionOvd.java
@@ -277,8 +277,8 @@ public class RdpConnectionOvd extends RdpConnection {
 	}
 	
 	@Override
-	public void stop() {
-		super.stop();
+	protected void closeVChannels() {
+		super.closeVChannels();
 
 		if (RdpConnectionOvd.diskManager != null) {
 			RdpConnectionOvd.diskManager.stop();