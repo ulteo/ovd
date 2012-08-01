ulteo/ovd

commit e86cce0b2b8c223bf015911c9826a3ca1aaadc80

Bugfix: show session panel and hide splash only when sure the session is ready instead of after a hardcoded time
git-svn-id: file:///home/code/svn/ovd/trunk@8279 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

client/web/web/media/script/common.js
@@ -310,14 +310,6 @@ function onStartSessionSuccess(xml_) {
 	}, 2500);
 
 	setTimeout(function() {
-		if (session_mode == 'Desktop')
-				new Effect.Move($('desktopModeContainer'), { x: 0, y: my_height });
-		else
-				new Effect.Move($('applicationsModeContainer'), { x: 0, y: my_height });
-	}, 3000);
-
-	setTimeout(function() {
-		hideSplash();
 		enableLogin();
 	}, 5000);
 

client/web/web/media/script/daemon.js
@@ -277,6 +277,11 @@ var Daemon = Class.create({
 
 			Logger.info('[daemon] check_status_post() - Now starting session');
 
+			new Effect.Move($(this.mode+'ModeContainer'), { x: 0, y: my_height });
+			setTimeout(function() {
+				hideSplash();
+			}, 2000);
+			
 			this.start();
 
 			this.started = true;

commit 50b750fa0263dcd65bfe432a6bb5af699ff7fa2f

Bugfix: session panel is not displayed anymore in external mode (regression from r8279)
git-svn-id: file:///home/code/svn/ovd/trunk@8363 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

client/web/web/media/script/common.js
@@ -294,6 +294,17 @@ function onStartSessionSuccess(xml_) {
 			else
 				$('applicationsModeContainer').style.height = daemon.my_height+'px';
 		}
+		
+		daemon.add_session_ready_callback(function onSessionReady(d_) {
+			if (session_mode == 'Desktop')
+				new Effect.Move($('desktopModeContainer'), { x: 0, y: my_height });
+			else
+				new Effect.Move($('applicationsModeContainer'), { x: 0, y: my_height });
+			
+			setTimeout(function() {
+				hideSplash();
+			}, 2000);
+		});
 
 		daemon.prepare();
 

client/web/web/media/script/daemon.js
@@ -62,6 +62,8 @@ var Daemon = Class.create({
 	progress_bar_step: 20,
 
 	application_token: 0,
+	  
+	session_ready_callback: new Array(),
 
 	initialize: function(debug_) {
 		this.settings = new Hash();
@@ -277,10 +279,8 @@ var Daemon = Class.create({
 
 			Logger.info('[daemon] check_status_post() - Now starting session');
 
-			new Effect.Move($(this.mode+'ModeContainer'), { x: 0, y: my_height });
-			setTimeout(function() {
-				hideSplash();
-			}, 2000);
+			for (var i=0; i<this.session_ready_callback.length; i++)
+				this.session_ready_callback[i](this);
 			
 			this.start();
 
@@ -303,6 +303,10 @@ var Daemon = Class.create({
 
 			this.stopped = true;
 		}
+	},
+	
+	add_session_ready_callback: function(callback_) {
+		this.session_ready_callback.push(callback_);
 	},
 
 	req_logout: function(mode_) {