ulteo/ovd

commit 30a82a89bf6973a21d4a7dffc2ae8e5a2c0fdb76

Code cleaning: split the GUI part of Application class into a dedicated ApplicationItem class
git-svn-id: file:///home/code/svn/ovd/trunk@8384 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

client/web/web/media/script/application.js
@@ -24,80 +24,11 @@ var Application = Class.create({
 	id: 0,
 	name: '',
 	server_id: 0,
-	node: null,
-
-	app_span: null,
 
 	initialize: function(id_, name_, server_id_) {
 		this.id = id_;
 		this.name = name_;
 		this.server_id = server_id_;
-	},
-
-	update: function() {
-		this.repaintNode();
-	},
-
-	initNode: function() {
-		var tr = new Element('tr');
-
-		var td_icon = new Element('td');
-		var icon = new Element('img');
-		icon.setAttribute('src', this.getIconURL());
-		icon.setAttribute('width', 32);
-		td_icon.appendChild(icon);
-		tr.appendChild(td_icon);
-
-		var td_app = new Element('td');
-		this.app_span = new Element('span');
-		td_app.appendChild(this.app_span);
-		tr.appendChild(td_app);
-
-		var td_running = new Element('td');
-		td_running.setAttribute('id', 'running_'+this.id);
-		td_running.setAttribute('style', 'width: 15px; text-align: right; font-size: 0.9em; font-style: italic;');
-		tr.appendChild(td_running);
-
-		this.repaintNode();
-
-		return tr;
-	},
-
-	repaintNode: function() {
-		this.app_span.innerHTML = '';
-
-		var server = daemon.servers.get(this.server_id);
-
-		if (server.ready) {
-			var node = new Element('a');
-			node.observe('click', this.onClick.bind(this));
-			node.setAttribute('href', 'javascript:;');
-			node.innerHTML = this.name;
-			this.app_span.appendChild(node);
-
-			this.app_span.parentNode.parentNode.setAttribute('style', 'opacity: 1.00; filter: alpha(opacity=100); -moz-opacity: 1.00;');
-		} else {
-			var node = new Element('span');
-			node.setAttribute('style', 'font-weight: bold;');
-			node.innerHTML = this.name;
-			this.app_span.appendChild(node);
-
-			this.app_span.parentNode.parentNode.setAttribute('style', 'opacity: 0.40; filter: alpha(opacity=40); -moz-opacity: 0.40;');
-		}
-
-		return true;
-	},
-
-	getNode: function() {
-		if (this.node == null)
-			this.node = this.initNode();
-
-		return this.node;
-	},
-
- 	onClick: function(event) {
-		this.launch();
-		event.stop();
 	},
 
 	launch: function() {
@@ -110,10 +41,6 @@ var Application = Class.create({
 		var server = daemon.servers.get(this.server_id);
 		$('ulteoapplet').startApplicationWithFile(++daemon.application_token, this.id, server.java_id, type_, path_, share_);
 		daemon.liaison_runningapplicationtoken_application.set(daemon.application_token, this.id);
-	},
-
-	getIconURL: function() {
-		return 'icon.php?id='+this.id;
 	}
 });
 
@@ -203,5 +130,86 @@ var ApplicationsPanel = Class.create({
 		try {
 			this.node.removeChild(app_.getNode());
 		} catch(e) {}
+	}
+});
+
+var ApplicationItem = Class.create({
+	application_: null,
+	node: null,
+
+	app_span: null,
+
+	initialize: function(application_) {
+		this.application = application_;
+	},
+
+	on_server_status_change: function(server_, status_) {
+		this.repaintNode();
+	},
+
+	initNode: function() {
+		var tr = new Element('tr');
+
+		var td_icon = new Element('td');
+		var icon = new Element('img');
+		icon.setAttribute('src', this.getIconURL());
+		icon.setAttribute('width', 32);
+		td_icon.appendChild(icon);
+		tr.appendChild(td_icon);
+
+		var td_app = new Element('td');
+		this.app_span = new Element('span');
+		td_app.appendChild(this.app_span);
+		tr.appendChild(td_app);
+
+		var td_running = new Element('td');
+		td_running.setAttribute('id', 'running_'+this.application.id);
+		td_running.setAttribute('style', 'width: 15px; text-align: right; font-size: 0.9em; font-style: italic;');
+		tr.appendChild(td_running);
+
+		this.repaintNode();
+
+		return tr;
+	},
+
+	repaintNode: function() {
+		this.app_span.innerHTML = '';
+
+		var server = daemon.servers.get(this.application.server_id);
+
+		if (server.ready) {
+			var node = new Element('a');
+			node.observe('click', this.onClick.bind(this));
+			node.setAttribute('href', 'javascript:;');
+			node.innerHTML = this.application.name;
+			this.app_span.appendChild(node);
+
+			this.app_span.parentNode.parentNode.setAttribute('style', 'opacity: 1.00; filter: alpha(opacity=100); -moz-opacity: 1.00;');
+		} else {
+			var node = new Element('span');
+			node.setAttribute('style', 'font-weight: bold;');
+			node.innerHTML = this.application.name;
+			this.app_span.appendChild(node);
+
+			this.app_span.parentNode.parentNode.setAttribute('style', 'opacity: 0.40; filter: alpha(opacity=40); -moz-opacity: 0.40;');
+		}
+
+		return true;
+	},
+
+	getNode: function() {
+		if (this.node == null)
+			this.node = this.initNode();
+
+		return this.node;
+	},
+
+ 	onClick: function(event) {
+		this.application.launch();
+		event.stop();
+	},
+
+	getIconURL: function() {
+		return 'icon.php?id='+this.application.id;
 	}
 });

client/web/web/media/script/daemon_applications.js
@@ -148,8 +148,17 @@ var Applications = Class.create(Daemon, {
 				
 				var application = new Application(applicationNodes[j].getAttribute('id'), applicationNodes[j].getAttribute('name'), server_.id);
 				this.applications.set(application.id, application);
-				this.applicationsPanel.add(application);
+				
 				this.liaison_server_applications.get(server_.id).push(application.id);
+				
+				var application_item = new ApplicationItem(application);
+				this.servers.each(function(pair) {
+					var server = pair.value;
+					server.add_status_changed_callback(application_item.on_server_status_change.bind(application_item));
+				});
+				
+				this.applicationsPanel.add(application_item);
+			
 			} catch(e) {
 				Logger.error('[applications] parse_list_servers(transport@list_servers()) - Invalid XML (Missing argument for "application" node '+j+')');
 				return false;

client/web/web/media/script/server.js
@@ -40,6 +40,8 @@ var Server = Class.create({
 
 	connected: false,
 	ready: false,
+	
+	status_changed_callbacks: null, // Array
 
 	/** Constructor : initialized with a <server> XML node.
 	 *
@@ -57,6 +59,8 @@ var Server = Class.create({
 		this.port = xml_.getAttribute('port');
 		this.username = xml_.getAttribute('login');
 		this.password = xml_.getAttribute('password');
+		
+		this.status_changed_callbacks = new Array();
 	},
 
 	/// In case we use the token auth.
@@ -125,13 +129,14 @@ var Server = Class.create({
 			daemon.logout();
 		}
 
-		var applications = daemon.liaison_server_applications.get(this.id);
-		for (var i=0; i < applications.length; i++) {
-			var application = daemon.applications.get(applications[i]);
-			application.update();
-		}
+		for (var i=0; i < this.status_changed_callbacks.length; i++)
+			this.status_changed_callbacks[i](this, status_);
 
 		return true;
+	},
+	
+	add_status_changed_callback: function(callback_) {
+		this.status_changed_callbacks.push(callback_);
 	}
 });
 

commit fa92638a3de4e19127166191c717c970bfbe7e2b

Typo (introduced in r8384)
git-svn-id: file:///home/code/svn/ovd/trunk@8395 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

client/web/web/media/script/application.js
@@ -114,7 +114,7 @@ var ApplicationsPanel = Class.create({
 });
 
 var ApplicationItem = Class.create({
-	application_: null,
+	application: null,
 	node: null,
 
 	app_span: null,