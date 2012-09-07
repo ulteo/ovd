86b32d118fb73c9911cd61b8e531d1c9bf38288f
julien <julien@2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3>
2012-09-07 13:50:34 +0000

    Code cleaning: remove useless test (equivalent code is already done by locale2unix function)
    git-svn-id: file:///home/code/svn/ovd/trunk@8448 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

--- client/web/web/index.php
+++ client/web/web/index.php
@@ -72,8 +72,6 @@ elseif (isset($_COOKIE['ovd-client']['session_mode']))
 if (OPTION_FORCE_LANGUAGE !== true && isset($_COOKIE['ovd-client']['session_language'])) {
 	$user_language = (string)$_COOKIE['ovd-client']['session_language'];
 }
-if (strlen($user_language) == 2)
-	$user_language = $user_language.'-'.$user_language;
 
 if (OPTION_FORCE_KEYMAP !== true && isset($_COOKIE['ovd-client']['session_keymap']))
 	$user_keymap = (string)$_COOKIE['ovd-client']['session_keymap'];
