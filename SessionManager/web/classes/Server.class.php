ulteo/ovd

commit 5dfe7b39864f28276da7344285634c62d1eccf93

Remove application icon if exists when create new application
git-svn-id: file:///home/code/svn/ovd/trunk@8498 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

SessionManager/web/classes/Server.class.php
@@ -1227,6 +1227,11 @@ class Server {
 						//echo 'app '.$app_name." not insert<br>\n";
 						return false;
 					}
+					
+					if ($a->haveIcon()) {
+						// We remove the application icon if already exists because it shouldn't be
+						$a->delIcon();
+					}
 				}
 			}
 			if ($applicationDB->isWriteable() == true){

commit 900b26e80b2f60468aca9134b9ab69185df11908

Typo (introduced in r8498)
git-svn-id: file:///home/code/svn/ovd/trunk@8568 2b2718be-6ee5-4ec5-ad1a-3fa14f9c7ca3

SessionManager/web/classes/Server.class.php
@@ -1232,7 +1232,7 @@ class Server {
 					$done_modifications++;
 					
 					if ($a->haveIcon()) {
-						// We remove the application icon if already exists because it shouldn't be
+						// We remove the application icon if already exists because it shouldn't
 						$a->delIcon();
 					}
 				}