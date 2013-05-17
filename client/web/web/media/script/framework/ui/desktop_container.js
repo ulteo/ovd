248dc17f37cf990f1bccb3852486b0b2b322084c
Alexandre CONFIANT-LATOUR <a.confiant@ulteo.com>
2013-05-17 10:22:39 +0200

    Improvement : clean DesktopContainer on sessionEnd

--- client/web/web/media/script/framework/ui/desktop_container.js
+++ client/web/web/media/script/framework/ui/desktop_container.js
@@ -1,19 +1,27 @@
 function DesktopContainer(session_management, node) {
 	this.node = jQuery(node);
+	this.desktop = null;
 	this.session_management = session_management;
 
 	/* register events listeners */
 	var self = this; /* closure */
-	this.session_management.addCallback("ovd.rdpProvider.desktopPanel", function(type, source, params) {
+	this.session_management.addCallback("ovd.*", function(type, source, params) {
 		self.handleEvents(type, source, params);
 	});                                         
 }
 
 DesktopContainer.prototype.handleEvents = function(type, source, params) {
 	if(type == "ovd.rdpProvider.desktopPanel") {
-		var name         = params["name"];
-		var desktop_node = params["node"];
+		var name     = params["name"];
+		this.desktop = params["node"];
 
-		this.node.append(desktop_node);
+		this.node.append(this.desktop);
+	}
+
+	if(type == "ovd.ajaxProvider.sessionEnd") { /* Clean content even if the sessionEnd request failed */
+		if(this.desktop) {
+			jQuery(this.desktop).remove();
+			this.desktop = null;
+		}
 	}
 }
