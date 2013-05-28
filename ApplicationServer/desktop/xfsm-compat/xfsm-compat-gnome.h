d1d2de25fce184efaff68405388a307cd3f385d8
David PHAM-VAN <d.pham-van@ulteo.com>
2013-05-28 09:57:07 +0200

    Update mis-used GPL3 licence blocks to GPL2

--- ApplicationServer/desktop/xfsm-compat/xfsm-compat-gnome.h
+++ ApplicationServer/desktop/xfsm-compat/xfsm-compat-gnome.h
@@ -1,10 +1,12 @@
 /**
- * Copyright (C) 2009 Gauvain Pocentek <gpocentek@linutop.com>
+ * Copyright (C) 2009 Ulteo SAS
+ * http://www.ulteo.com
+ * Author Gauvain Pocentek <gpocentek@linutop.com> 2009
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,8 +14,9 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ **/
 
 #ifndef __XFSM_COMPAT_GNOME_H__
 #define __XFSM_COMPAT_GNOME_H__
