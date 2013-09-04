ulteo/ovd

commit f35449195ea7509497a9200616869bcd90ea0451

Globalize the user(group)db->imports function to decrease the number of requests

SessionManager/web/classes/Profile.class.php
@@ -36,13 +36,10 @@ class Profile extends NetworkFolder {
 		
 		$users = array();
 		foreach ($liaisons as $liaison) {
-			$user = $userDB->import($liaison->element);
-			if (! is_object($user))
-				continue;
-			
-			$users[$user->getAttribute('login')] = $user;
+			array_merge($users, $liaison->element);
 		}
-		return $users;
+		
+		return $userDB->imports($users);
 	}
 	
 	public function addUser($user_) {

SessionManager/web/admin/api.php
@@ -2011,14 +2011,19 @@ class OvdAdminSoap {
 		
 		$liaisons = Abstract_Liaison::load('UsersGroupApplicationsGroup', NULL, $id_);
 		if (count($liaisons)) {
-			$g['usersgroups'] = array();
+			$group_ids = array();
 			foreach ($liaisons as $liaison) {
-				$obj = $userGroupDB->import($liaison->element);
-				if (! $obj) {
-					continue;
-				}
-				
-				$g['usersgroups'][$liaison->element] = $obj->name;
+				array_push($group_ids, $liaison->element);
+			}
+			
+			$groups = $userGroupDB->imports($group_ids);
+			$exported_groups = array();
+			foreach($groups as $group_id => $group) {
+				$exported_groups[$group_id] = $group->name;
+			}
+			
+			if (count($exported_groups) > 0) {
+				$g['usersgroups'] = $exported_groups;
 			}
 		}
 		
@@ -2555,13 +2560,10 @@ class OvdAdminSoap {
 		$users = $group->usersLogin();
 		if (count($users) > 0) {
 			$userDB = UserDB::getInstance();
+			$users2 = $userDB->imports($users);
 			
 			$s['users'] = array();
-			foreach($users as $user_id) {
-				$user = $userDB->import($user_id);
-				if (! $user)
-					continue;
-				
+			foreach($users2 as $user) {
 				$g['users'][$user->getAttribute('login')] = $user->getAttribute('displayname');
 			}
 		}

SessionManager/web/classes/AppsGroup.class.php
@@ -46,11 +46,10 @@ class AppsGroup {
 		if (is_array($groups)) {
 			$result = array();
 			foreach ($groups as $UGAG_liaison){
-				$g = $UserGroupDB->import($UGAG_liaison->element);
-				if (is_object($g))
-					$result[$UGAG_liaison->element]= $g;
+				array_push($result, $UGAG_liaison->element);
 			}
-			return $result;
+			
+			return $UserGroupDB->imports($result);
 		}
 		else {
 			Logger::error('main', 'APPSGROUPS::userGroups (for id='.$this->id.') load liaison liaison failed');

SessionManager/web/modules/UserDB/sql.php
@@ -51,6 +51,39 @@ class UserDB_sql extends UserDB  {
 		return NULL;
 	}
 	
+	public function imports($logins_) {
+		$sql2 = SQL::getInstance();
+		
+		$logins2 = array();
+		foreach($logins_ as $login) {
+			array_push($logins2, '"'.$sql2->CleanValue($login).'"');
+		}
+		
+		$request = 'SELECT * FROM #1 WHERE @2 IN ('.implode(', ',$logins2).')';
+		$res = $sql2->DoQuery($request, self::table, 'login');
+		if ($res === false){
+			Logger::error('main', 'USERDB::MYSQL::getList_nocache failed (sql query failed)');
+			// not the right argument
+			return NULL;
+		}
+		
+		$result = array();
+		$rows = $sql2->FetchAllResults($res);
+		foreach ($rows as $row){
+			$u = $this->generateUserFromRow($row);
+			if ($this->isOK($u))
+				$result []= $u;
+			else {
+				if (isset($row['login']))
+					Logger::info('main', 'USERDB::MYSQL::imports user \''.$row['login'].'\' not ok');
+				else
+					Logger::info('main', 'USERDB::MYSQL::imports user does not have login');
+			}
+		}
+		
+		return $result;
+	}
+	
 	public function isWriteable(){
 		return true;
 	}

commit 6c84954c2d82078a2236c133c903c5c334cb7dda

Bugfix: typo in called function (introduced in f35449195ea)

SessionManager/web/classes/Profile.class.php
@@ -34,10 +34,9 @@ class Profile extends NetworkFolder {
 		}
 		
 		$userDB = UserDB::getInstance();
-		
 		$users = array();
 		foreach ($liaisons as $liaison) {
-			array_merge($users, $liaison->element);
+			array_push($users, $liaison->element);
 		}
 		
 		return $userDB->imports($users);