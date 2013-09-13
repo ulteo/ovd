ulteo/ovd

commit 97a9ce35cbba6181b959b01bcaa85f0532e548fe

Replace UsersDB getList call from webservices by a request that return a partial content

SessionManager/web/client/userlist.php
@@ -4,6 +4,7 @@
  * http://www.ulteo.com
  * Author Jeremy DESVAGES <jeremy@ulteo.com> 2008
  * Author Laurent CLOUET <laurent@ulteo.com> 2010
+ * Author Julien LANGLOIS <julien@ulteo.com> 2013
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -33,19 +34,15 @@ if (! array_key_exists('show_list_users', $web_interface_settings) || $web_inter
 	die();
 }
 
-$userDB = UserDB::getInstance();
-
-$users = array();
-if ($userDB->canShowList()) {
-	$users = $userDB->getList();
-	
-	if (! is_array($users))
-		$users = array();
-}
+$search_limit = $prefs->get('general', 'max_items_per_page');
+list($users, $partial) = $userDB->getUsersContains('', array('login'), $search_limit);
 
 $dom = new DomDocument('1.0', 'utf-8');
 
 $users_node = $dom->createElement('users');
+if ($partial) {
+	$users_node->setAttribute('partial', 'true');
+}
 foreach ($users as $user) {
 	$user_node = $dom->createElement('user');
 	$user_node->setAttribute('login', $user->getAttribute('login'));

commit 21d6e681c308933be1af3615069d6dc5c2044ebe

Bugfix: client/userslist do not work anymore (regression from 97a9ce35c)

SessionManager/web/client/userlist.php
@@ -35,6 +35,7 @@ if (! array_key_exists('show_list_users', $web_interface_settings) || $web_inter
 }
 
 $search_limit = $prefs->get('general', 'max_items_per_page');
+$userDB = UserDB::getInstance();
 list($users, $partial) = $userDB->getUsersContains('', array('login'), $search_limit);
 
 $dom = new DomDocument('1.0', 'utf-8');