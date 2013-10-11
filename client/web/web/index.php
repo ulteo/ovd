b89c2146377578176335ebcf3c20d85a03481ae0
David PHAM-VAN <d.pham-van@ulteo.com>
2013-10-11 16:20:59 +0200

    Dissociate browser detected languages in select box

--- client/web/web/index.php
+++ client/web/web/index.php
@@ -502,8 +502,22 @@ if ($gateway_first) {
 										<div id="session_language_flag" style="display:inline-block;" ></div>
 										<select id="session_language" <?php if (OPTION_FORCE_LANGUAGE === true) echo ' disabled="disabled"';?>>
 											<?php
+												$browser_languages = detectBrowserLanguage($languages);
+												if (count($browser_languages) > 0) {
+													foreach ($browser_languages as $browser_language) {
+														foreach ($languages as $language) {
+															if ($browser_language == $language['id']) {
+																echo '<option value="'.$language['id'].'" style="background: url(\'media/image/flags/'.$language['id'].'.png\') no-repeat right;"'.(($language['id'] == $user_language || $language['id'] == substr($user_language, 0, 2))?' selected="selected"':'').'>'.$language['english_name'].((array_key_exists('local_name', $language))?' - '.$language['local_name']:'').'</option>';
+																break;
+															}
+														}
+													}
+													echo '<option disabled="disabled"></option>';
+												}
 												foreach ($languages as $language) {
-													echo '<option value="'.$language['id'].'" style="background: url(\'media/image/flags/'.$language['id'].'.png\') no-repeat right;"'.(($language['id'] == $user_language || $language['id'] == substr($user_language, 0, 2))?' selected="selected"':'').'>'.$language['english_name'].((array_key_exists('local_name', $language))?' - '.$language['local_name']:'').'</option>';
+													if (!in_array($language['id'], $browser_languages)) {
+														echo '<option value="'.$language['id'].'" style="background: url(\'media/image/flags/'.$language['id'].'.png\') no-repeat right;"'.(($language['id'] == $user_language || $language['id'] == substr($user_language, 0, 2))?' selected="selected"':'').'>'.$language['english_name'].((array_key_exists('local_name', $language))?' - '.$language['local_name']:'').'</option>';
+													}
 												}
 											?>
 										</select>
