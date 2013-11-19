a544a0fc84f06489b330b0ffa9c9a4fec7f6333e
David PHAM-VAN <d.pham-van@ulteo.com>
2013-11-19 14:20:04 +0100

    BugFix: Restriced menu problem with env

--- ApplicationServer/desktop/restricted-menu/main.c
+++ ApplicationServer/desktop/restricted-menu/main.c
@@ -168,12 +168,14 @@ rm_read (RmPlugin *rm)
     rm->icon_normal = g_strdup (xfce_rc_read_entry (rc, "icon_normal", DEFAULT_ICON));
     rm->icon_hover = g_strdup (xfce_rc_read_entry (rc, "icon_hover", DEFAULT_ICON));
     tmp = g_strdup (xfce_rc_read_entry (rc, "from_env", NULL));
-    if (!tmp)
+    if (tmp)
+    {
+        rm->ddir = getenv (tmp);
+    }
+    if (!rm->ddir)
     {
         tmp = g_strdup (xfce_rc_read_entry (rc, "spool_tpl", DEFAULT_TPL));
         rm->ddir = build_ddir (tmp);
-    } else {
-        rm->ddir = getenv (tmp);
     }
 
     rm->cdir = g_strdup (xfce_rc_read_entry (rc, "spool_common", DEFAULT_CDIR));
