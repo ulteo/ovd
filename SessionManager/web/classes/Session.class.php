ulteo/ovd

commit 47b4846c1b45d4fdc7c94e716c6050188c47f916

Bugfix: solve some notice introduce by webapp role

SessionManager/web/classes/Session.class.php
@@ -207,7 +207,13 @@ class Session {
 				break;
 			case Session::SESSION_STATUS_READY:
 				$all_ready = true;
-				$all_servers = array_merge($this->servers[Server::SERVER_ROLE_APS], $this->servers[Server::SERVER_ROLE_WEBAPPS]);
+				$all_servers = array();
+				if (array_key_exists(Server::SERVER_ROLE_APS, $this->servers))
+					array_merge($all_servers, $this->servers[Server::SERVER_ROLE_APS]);
+				
+				if (array_key_exists(Server::SERVER_ROLE_WEBAPPS, $this->servers))
+					 array_merge($all_servers, $this->servers[Server::SERVER_ROLE_WEBAPPS]);
+				
 				foreach ($all_servers as $server_id => $data) {
 					if ($server_id != $server_ && $data['status'] != Session::SESSION_STATUS_READY) {
 						$all_ready = false;
@@ -242,7 +248,13 @@ class Session {
 				break;
 			case Session::SESSION_STATUS_DESTROYED:
 				$all_destroyed = true;
-				$all_servers = array_merge($this->servers[Server::SERVER_ROLE_APS], $this->servers[Server::SERVER_ROLE_WEBAPPS]);
+				$all_servers = array();
+				if (array_key_exists(Server::SERVER_ROLE_APS, $this->servers))
+					array_merge($all_servers, $this->servers[Server::SERVER_ROLE_APS]);
+				
+				if (array_key_exists(Server::SERVER_ROLE_WEBAPPS, $this->servers))
+					array_merge($all_servers, $this->servers[Server::SERVER_ROLE_WEBAPPS]);
+				
 				foreach ($all_servers as $server_id => $data) {
 					if ($server_id != $server_ && $data['status'] != Session::SESSION_STATUS_DESTROYED) {
 						$all_destroyed = false;
@@ -502,23 +514,25 @@ class Session {
 			$this->setStatus(Session::SESSION_STATUS_DESTROYED, $reason_);
 		else
 			$this->setStatus(Session::SESSION_STATUS_DESTROYING, $reason_);
-
-		foreach ($this->servers[Server::SERVER_ROLE_WEBAPPS] as $server_id => $data) {
-			$session_server = Abstract_Server::load($server_id);
-			if (! $session_server) {
-				Logger::error('main', 'Session::orderDeletion Unable to load server \''.$server_id.'\'');
-				return false;
-			}
-
-			if (is_array($session_server->roles)) {
-				if (array_key_exists(Server::SERVER_ROLE_WEBAPPS, $session_server->roles)) {
-					$buf = $session_server->orderSessionDeletion($this->id, 'webapps');
-					if (! $buf) {
-						Logger::warning('main', 'Session::orderDeletion Session \''.$this->id.'\' already destroyed on server \''.$session_server->fqdn.'\'');
-						$this->setServerStatus($session_server->id, Session::SESSION_STATUS_DESTROYED);
-						$destroyed++;
-					} else
+		
+		if (array_key_exists(Server::SERVER_ROLE_WEBAPPS, $session_server->roles)) {
+			foreach ($this->servers[Server::SERVER_ROLE_WEBAPPS] as $server_id => $data) {
+				$session_server = Abstract_Server::load($server_id);
+				if (! $session_server) {
+					Logger::error('main', 'Session::orderDeletion Unable to load server \''.$server_id.'\'');
+					return false;
+				}
+				
+				if (is_array($session_server->roles)) {
+					if (array_key_exists(Server::SERVER_ROLE_WEBAPPS, $session_server->roles)) {
+						$buf = $session_server->orderSessionDeletion($this->id, 'webapps');
+						if (! $buf) {
+							Logger::warning('main', 'Session::orderDeletion Session \''.$this->id.'\' already destroyed on server \''.$session_server->fqdn.'\'');
+							$this->setServerStatus($session_server->id, Session::SESSION_STATUS_DESTROYED);
+							$destroyed++;
+						} else
 						$this->setServerStatus($session_server->id, Session::SESSION_STATUS_DESTROYING);
+					}
 				}
 			}
 		}

SessionManager/web/client/start.php
@@ -802,87 +802,91 @@ if (array_key_exists(Server::SERVER_ROLE_FS, $session->servers)) {
 }
 
 $defined_apps = array();
-foreach ($session->servers[Server::SERVER_ROLE_APS] as $server_id => $data) {
-	if ($session->mode == Session::MODE_DESKTOP && $server_id != $session->server)
-		continue;
+if (array_key_exists(Server::SERVER_ROLE_APS, $session->servers)) {
+	foreach ($session->servers[Server::SERVER_ROLE_APS] as $server_id => $data) {
+		if ($session->mode == Session::MODE_DESKTOP && $server_id != $session->server)
+			continue;
 	
-	$server = Abstract_Server::load($server_id);
-	if (! $server)
-		throw_response(INTERNAL_ERROR);
+		$server = Abstract_Server::load($server_id);
+		if (! $server)
+			throw_response(INTERNAL_ERROR);
 
-	if (! array_key_exists(Server::SERVER_ROLE_APS, $server->getRoles()))
-		throw_response(INTERNAL_ERROR);
+		if (! array_key_exists(Server::SERVER_ROLE_APS, $server->getRoles()))
+			throw_response(INTERNAL_ERROR);
 
-	$server_applications = $server->getApplications();
-	if (! is_array($server_applications))
-		$server_applications = array();
+		$server_applications = $server->getApplications();
+		if (! is_array($server_applications))
+			$server_applications = array();
 
-	$available_applications = array();
-	foreach ($server_applications as $server_application)
-		$available_applications[] = $server_application->getAttribute('id');
+		$available_applications = array();
+		foreach ($server_applications as $server_application)
+			$available_applications[] = $server_application->getAttribute('id');
 
-	$server_node = $dom->createElement('server');
-	$server_node->setAttribute('type', $server->getAttribute('type'));
-	$server_node->setAttribute('fqdn', $server->getExternalName());
-	if ($server->getApSRDPPort() != Server::DEFAULT_RDP_PORT)
-		$server_node->setAttribute('port', $server->getApSRDPPort());
-	$server_node->setAttribute('login', $user_login_aps);
-	$server_node->setAttribute('password', $user_password_aps);
+		$server_node = $dom->createElement('server');
+		$server_node->setAttribute('type', $server->getAttribute('type'));
+		$server_node->setAttribute('fqdn', $server->getExternalName());
+		if ($server->getApSRDPPort() != Server::DEFAULT_RDP_PORT)
+			$server_node->setAttribute('port', $server->getApSRDPPort());
+		$server_node->setAttribute('login', $user_login_aps);
+		$server_node->setAttribute('password', $user_password_aps);
+	
+		foreach ($session->getPublishedApplications() as $application) {
+			if ($application->getAttribute('type') != $server->getAttribute('type'))
+				continue;
 
-	foreach ($session->getPublishedApplications() as $application) {
-		if ($application->getAttribute('type') != $server->getAttribute('type'))
-			continue;
+			if (! in_array($application->getAttribute('id'), $available_applications))
+				continue;
 
-		if (! in_array($application->getAttribute('id'), $available_applications))
-			continue;
-
-		if (in_array($application->getAttribute('id'), $defined_apps))
-			continue;
+			if (in_array($application->getAttribute('id'), $defined_apps))
+				continue;
 		
-		$defined_apps[] = $application->getAttribute('id');
+			$defined_apps[] = $application->getAttribute('id');
 
-		$application_node = $dom->createElement('application');
-		$application_node->setAttribute('id', $application->getAttribute('id'));
-		$application_node->setAttribute('name', $application->getAttribute('name'));
-		foreach ($application->getMimeTypes() as $mimetype) {
-			$mimetype_node = $dom->createElement('mime');
-			$mimetype_node->setAttribute('type', $mimetype);
-			$application_node->appendChild($mimetype_node);
+			$application_node = $dom->createElement('application');
+			$application_node->setAttribute('id', $application->getAttribute('id'));
+			$application_node->setAttribute('name', $application->getAttribute('name'));
+			foreach ($application->getMimeTypes() as $mimetype) {
+				$mimetype_node = $dom->createElement('mime');
+				$mimetype_node->setAttribute('type', $mimetype);
+				$application_node->appendChild($mimetype_node);
+			}
+			$server_node->appendChild($application_node);
 		}
-		$server_node->appendChild($application_node);
+		$session_node->appendChild($server_node);
 	}
-	$session_node->appendChild($server_node);
 }
 
-foreach ($session->servers[Server::SERVER_ROLE_WEBAPPS] as $server_id => $data) {
-	if ($session->mode == Session::MODE_DESKTOP && $server_id != $session->server)
-		continue;
-
-	$server = Abstract_Server::load($server_id);
-	if (! $server)
-		throw_response(INTERNAL_ERROR);
-
-	if (! array_key_exists(Server::SERVER_ROLE_WEBAPPS, $server->getRoles()))
-		throw_response(INTERNAL_ERROR);
-
-	$server_node = $dom->createElement('webapp-server');
-	$server_node->setAttribute('type', 'webapps');
-	$server_node->setAttribute('base-url', $server->getBaseURL());
-	$server_node->setAttribute('webapps-url', $webapps_url);
-	$server_node->setAttribute('login', $user_login_webapps);
-	$server_node->setAttribute('password', $user_password_webapps);
-
-	foreach ($session->getPublishedApplications() as $application) {
-		if ($application->getAttribute('type') != 'webapp')
+if (array_key_exists(Server::SERVER_ROLE_WEBAPPS, $session->servers)) {
+	foreach ($session->servers[Server::SERVER_ROLE_WEBAPPS] as $server_id => $data) {
+		if ($session->mode == Session::MODE_DESKTOP && $server_id != $session->server)
 			continue;
 
-		$application_node = $dom->createElement('application');
-		$application_node->setAttribute('id', $application->getAttribute('id'));
-		$application_node->setAttribute('type', 'webapp');
-		$application_node->setAttribute('name', $application->getAttribute('name'));
-		$server_node->appendChild($application_node);
+		$server = Abstract_Server::load($server_id);
+		if (! $server)
+			throw_response(INTERNAL_ERROR);
+
+		if (! array_key_exists(Server::SERVER_ROLE_WEBAPPS, $server->getRoles()))
+			throw_response(INTERNAL_ERROR);
+
+		$server_node = $dom->createElement('webapp-server');
+		$server_node->setAttribute('type', 'webapps');
+		$server_node->setAttribute('base-url', $server->getBaseURL());
+		$server_node->setAttribute('webapps-url', $webapps_url);
+		$server_node->setAttribute('login', $user_login_webapps);
+		$server_node->setAttribute('password', $user_password_webapps);
+
+		foreach ($session->getPublishedApplications() as $application) {
+			if ($application->getAttribute('type') != 'webapp')
+				continue;
+
+			$application_node = $dom->createElement('application');
+			$application_node->setAttribute('id', $application->getAttribute('id'));
+			$application_node->setAttribute('type', 'webapp');
+			$application_node->setAttribute('name', $application->getAttribute('name'));
+			$server_node->appendChild($application_node);
+		}
+		$session_node->appendChild($server_node);
 	}
-	$session_node->appendChild($server_node);
 }
 
 $dom->appendChild($session_node);

commit 68f50a28cc964c640ecb40f4d9661acbd02f52fd

Bugfix: wrong use of array_merge. bug introduced by rev 47b4846c

SessionManager/web/classes/Session.class.php
@@ -213,10 +213,10 @@ class Session {
 				$all_ready = true;
 				$all_servers = array();
 				if (array_key_exists(Server::SERVER_ROLE_APS, $this->servers))
-					array_merge($all_servers, $this->servers[Server::SERVER_ROLE_APS]);
+					$all_servers = array_merge($all_servers, $this->servers[Server::SERVER_ROLE_APS]);
 				
 				if (array_key_exists(Server::SERVER_ROLE_WEBAPPS, $this->servers))
-					 array_merge($all_servers, $this->servers[Server::SERVER_ROLE_WEBAPPS]);
+					 $all_servers = array_merge($all_servers, $this->servers[Server::SERVER_ROLE_WEBAPPS]);
 				
 				foreach ($all_servers as $server_id => $data) {
 					if ($server_id != $server_ && $data['status'] != Session::SESSION_STATUS_READY) {
@@ -254,10 +254,10 @@ class Session {
 				$all_destroyed = true;
 				$all_servers = array();
 				if (array_key_exists(Server::SERVER_ROLE_APS, $this->servers))
-					array_merge($all_servers, $this->servers[Server::SERVER_ROLE_APS]);
+					$all_servers = array_merge($all_servers, $this->servers[Server::SERVER_ROLE_APS]);
 				
 				if (array_key_exists(Server::SERVER_ROLE_WEBAPPS, $this->servers))
-					array_merge($all_servers, $this->servers[Server::SERVER_ROLE_WEBAPPS]);
+					$all_servers = array_merge($all_servers, $this->servers[Server::SERVER_ROLE_WEBAPPS]);
 				
 				foreach ($all_servers as $server_id => $data) {
 					if ($server_id != $server_ && $data['status'] != Session::SESSION_STATUS_DESTROYED) {