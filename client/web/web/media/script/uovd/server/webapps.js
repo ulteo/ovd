2f5f69da1cba0c9f61dc433bd26769c7cd8be353
Alexandre CONFIANT-LATOUR <a.confiant@ulteo.com>
2014-03-25 10:53:45 +0100

    Bugfix : emulate a "connected" state for webapps server

--- client/web/web/media/script/uovd/server/webapps.js
+++ client/web/web/media/script/uovd/server/webapps.js
@@ -10,6 +10,16 @@ uovd.server.WebApps = function(session, xml) {
 	xml.find("application").each( function() {
 		self.applications.push(new uovd.Application(self, jQuery(this)));
 	});
+
+	/* Override setStatus function to emulate "connected" state */
+	this.super_setStatus = jQuery.proxy(this.setStatus, this);
+	this.setStatus = function(status) {
+		if(status == uovd.SERVER_STATUS_READY) {
+			self.super_setStatus(uovd.SERVER_STATUS_CONNECTED);
+		}
+
+		self.super_setStatus(status);
+	}
 };
 
 uovd.server.WebApps.prototype = new uovd.server.Base();
