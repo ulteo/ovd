ulteo/ovd

commit 72d55a0e608cd1b39f78885cf462da5f9c00eba1

Bugfix #897: Administration Console is broken when Admin API changed after an upgrade

AdminConsole/configure.ac.in
@@ -94,6 +94,7 @@ web/media/script/lib/prototype/Makefile
 web/media/script/lib/scriptaculous/Makefile
 web/media/script/lib/Makefile
 web/media/style/Makefile
+web/tools/Makefile
 web/media/image/loadbalancing/Makefile
 web/media/image/menu/Makefile
 web/media/image/Makefile

AdminConsole/web/Makefile.am
@@ -21,7 +21,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-SUBDIRS = ajax media
+SUBDIRS = ajax media tools
 
 scripts_DATA = actions.php \
                action-reporting.php \

AdminConsole/web/tools/update_wsdl_cache.php
@@ -1,0 +1,1 @@
+../../../SessionManager/web/tools/update_wsdl_cache.php

AdminConsole/web/tools/Makefile.am
@@ -1,0 +1,5 @@
+scripts_DATA = update_wsdl_cache.php
+
+scriptsdir = $(datadir)/ulteo/administration_console/tools
+
+EXTRA_DIST = $(scripts_DATA)

SessionManager/web/tools/Makefile.am
@@ -1,5 +1,6 @@
 scripts_DATA = can_update.php \
-               update_database.php
+               update_database.php \
+               update_wsdl_cache.php
 
 scriptsdir = $(datadir)/ulteo/sessionmanager/tools
 

SessionManager/web/tools/update_wsdl_cache.php
@@ -1,0 +1,33 @@
+<?php
+/**
+ * Copyright (C) 2014 Ulteo SAS
+ * http://www.ulteo.com
+ * Author David LECHEVALIER <david@ulteo.com> 2014
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ **/
+
+if (! defined('STDIN')) {
+	echo("This script cannot be used in CGI mode.\n");
+	exit(1);
+}
+
+// Cleanup wsdl files
+$wsdl_cache_dir = ini_get('soap.wsdl_cache_dir');
+foreach (glob($wsdl_cache_dir.DIRECTORY_SEPARATOR."wsdl-*") as $filename) {
+	unlink($filename);
+}
+
+exit(0);

packaging/debian/ovd-administration-console/lucid/ulteo-ovd-administration-console.postinst
@@ -5,6 +5,7 @@
 CONFDIR=/etc/ulteo/administration_console
 CONFFILE=$CONFDIR/config.inc.php
 SPOOLDIR=/var/spool/ulteo/administration_console
+INSTALLDIR=/usr/share/ulteo/administration_console
 
 ############################# APACHE CONFIGURATION #############################
 
@@ -15,6 +16,14 @@ A2USER=www-data
 # Alias admin
 if [ ! -e $A2CONFDIR/ovd-administration-console.conf ]; then
     ln -sfT $CONFDIR/apache2.conf $A2CONFDIR/ovd-administration-console.conf
+fi
+
+# Update wsdl
+if [ -f $INSTALLDIR/tools/update_wsdl_cache.php ]
+then
+   echo "Purging wsdl cache files."
+   php $INSTALLDIR/tools/update_wsdl_cache.php 2>/dev/null
+   [ $? -ne 0 ] && exit 1
 fi
 
 # restart apache server

packaging/debian/ovd-session-manager-premium/lucid/ulteo-ovd-session-manager-premium.postinst
@@ -12,6 +12,14 @@ then
    [ $? -ne 0 ] && exit 1
 fi
 
+# Update wsdl
+if [ -f $INSTALLDIR/tools/update_wsdl_cache.php ]
+then
+   echo "Purging wsdl cache files."
+   php $INSTALLDIR/tools/update_wsdl_cache.php 2>/dev/null
+   [ $? -ne 0 ] && exit 1
+fi
+
 db_stop
 
 #DEBHELPER#

commit 05a6940cc35e67def6f4c6febd1436e6026db3eb

Bugfix: wrong location for tools directory in administration console:introduced by the commit 72d55a0e608cd1b39f78885cf462da5f9c00eba1

AdminConsole/Makefile.am
@@ -18,7 +18,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-SUBDIRS = web config classes includes
+SUBDIRS = web config classes includes tools
 
 DIST_SUBDIRS = $(SUBDIRS)
 

AdminConsole/configure.ac.in
@@ -84,6 +84,7 @@ Makefile
 config/Makefile
 classes/Makefile
 includes/Makefile
+tools/Makefile
 web/Makefile
 web/ajax/Makefile
 web/media/script/Makefile
@@ -94,7 +95,6 @@ web/media/script/lib/prototype/Makefile
 web/media/script/lib/scriptaculous/Makefile
 web/media/script/lib/Makefile
 web/media/style/Makefile
-web/tools/Makefile
 web/media/image/loadbalancing/Makefile
 web/media/image/menu/Makefile
 web/media/image/Makefile

AdminConsole/tools/update_wsdl_cache.php
@@ -1,0 +1,1 @@
+../../SessionManager/web/tools/update_wsdl_cache.php

AdminConsole/web/Makefile.am
@@ -21,7 +21,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-SUBDIRS = ajax media tools
+SUBDIRS = ajax media
 
 scripts_DATA = actions.php \
                action-reporting.php \

AdminConsole/web/tools/update_wsdl_cache.php
@@ -1,1 +1,0 @@
-../../../SessionManager/web/tools/update_wsdl_cache.php