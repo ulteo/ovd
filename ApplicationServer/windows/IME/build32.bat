67721f4c2d92f60ec4cc368f3ba348bf96640264
David LECHEVALIER <david@ulteo.com>
2014-04-03 14:59:49 +0200

    Feature: add ulteo-tsf

---
+++ ApplicationServer/windows/IME/build32.bat
@@ -0,0 +1,7 @@
+REM IF EXIST build32 RD /Q /S build32
+
+REM md build32
+cd build32
+cmake -DCMAKE_INSTALL_PREFIX=..\dist -G "Visual Studio 10" ..
+cmake --build . --config Release --target install
+
