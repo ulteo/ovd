bd144100375acb2b97fd64b8770d74be00da0a8a
David LECHEVALIER <david@ulteo.com>
2014-04-08 09:51:05 +0200

    Improvement: manage 32 and 64 bits build

--- ApplicationServer/windows/IME/build32.bat
+++ ApplicationServer/windows/IME/build32.bat
@@ -2,6 +2,6 @@ REM IF EXIST build32 RD /Q /S build32
 
 REM md build32
 cd build32
-cmake -DCMAKE_INSTALL_PREFIX=..\dist -G "Visual Studio 10" ..
+cmake -DCMAKE_INSTALL_PREFIX=..\dist\32 -G "Visual Studio 10" ..
 cmake --build . --config Release --target install
 
