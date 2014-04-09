d8dd325abcba1064c04f904783edb9f6b7967613
Alexandre CONFIANT-LATOUR <a.confiant@ulteo.com>
2014-04-09 17:46:26 +0200

    Bugfix 934: Problem opening files from Ajaxplorer when filenames contains non ascii characters in HTML5 Portal Mode

--- client/web/web/media/script/uovd/provider/applications/html5.js
+++ client/web/web/media/script/uovd/provider/applications/html5.js
@@ -5,6 +5,17 @@ uovd.provider.applications.Html5 = function(rdp_provider) {
 	this.rdp_provider = rdp_provider;
 	this.connections = this.rdp_provider.connections;
 
+	/* Constants */
+	this.ORDER_INIT = 0x00;
+	this.ORDER_STARTED = 0x02;
+	this.ORDER_STOPPED = 0x03;
+	this.ORDER_CANT_START = 0x03;
+	this.ORDER_KNOWN_DRIVES = 0x20;
+	this.ORDER_START = 0x01;
+	this.ORDER_START_WITH_ARGS = 0x07;
+	this.FILE_SHAREDFOLDER = 0x01;
+	this.FILE_HTTP = 0x10;
+
 	/* Install instruction hook */
 	var self = this; /* closure */
 	for(var i=0 ; i<this.connections.length ; ++i) {
@@ -31,16 +42,17 @@ uovd.provider.applications.Html5.prototype = new uovd.provider.applications.Base
 
 uovd.provider.applications.Html5.prototype.applicationStart_implementation = function (application_id, token) { 
 	var server_id = this.getServerByAppId(application_id);
-	var opcode    = "01";                          /* uint8     */
-	var appToken  = this.write(token, 4);          /* uint32 Le */
-	var appId     = this.write(application_id, 4); /* uint32 Le */
+	var stream = new DataStream();
+	stream.write_Byte(this.ORDER_START);
+	stream.write_UInt32LE(parseInt(token));
+	stream.write_UInt32LE(parseInt(application_id));
 	this.applications[token] = new uovd.provider.applications.ApplicationInstance(this, application_id, token);
 
 	if(server_id != -1) {
 		var server = this.session_management.session.servers[server_id];
 
 		if(server.type != uovd.SERVER_TYPE_WEBAPPS) {
-			var ovdapp_message = opcode+""+appToken+""+appId;
+			var ovdapp_message = stream.toBase64();
 			this.connections[server_id].guac_tunnel.sendMessage("ovdapp", ovdapp_message);
 			this.session_management.fireEvent("ovd.applicationsProvider.statusChanged", this, {"application":this.applications[token], "from":"", "to":"unknown"});
 		}
@@ -55,26 +67,30 @@ uovd.provider.applications.Html5.prototype.applicationStartWithArgs_implementati
 	var file_path = args["path"];
 	var file_share = args["share"];
 
-	var opcode    = "07";                           /* uint8     */
-	var appToken  = this.write(token, 4);           /* uint32 Le */
-	var appId     = this.write(application_id, 4);  /* uint32 Le */
-	var dir_type  = "";                             /* uint8     */
-	var share_len = "";                             /* uint32 Le */
-	var share     = "";                             /* UTF-16 Le */
-	var path_len  = "";                             /* uint32 Le */
-	var path      = "";                             /* UTF-16 Le */
+	var stream = new DataStream();
+	stream.write_Byte(this.ORDER_START_WITH_ARGS);
+	stream.write_UInt32LE(parseInt(token));
+	stream.write_UInt32LE(parseInt(application_id));
 
 	switch(file_type) {
-		case "sharedfolder" : dir_type = "01"; break;
-		case "http"         : dir_type = "10"; break;
-		default             : dir_type = "01";
+		case "sharedfolder" : dir_type = this.FILE_SHAREDFOLDER; break;
+		case "http"         : dir_type = this.FILE_HTTP        ; break;
+		default             : dir_type = this.FILE_SHAREDFOLDER;
 	}
 
-	share = this.writeString(file_share);
-	share_len = this.write(share.length/2, 4);
+	stream.write_Byte(dir_type);
 
-	path = this.writeString(file_path);
-	path_len = this.write(path.length/2, 4);
+	var stream_share = new DataStream(); /* Only to compute encoded size of the UTF-16 string */
+	stream_share.write_UTF16LE(file_share);
+	var share_len = stream_share.get_size();
+	stream.write_UInt32LE(share_len);
+	stream.write_UTF16LE(file_share);
+
+	var stream_path = new DataStream(); /* Only to compute encoded size of the UTF-16 string */
+	stream_path.write_UTF16LE(file_path);
+	var path_len = stream_path.get_size();
+	stream.write_UInt32LE(path_len);
+	stream.write_UTF16LE(file_path);
 
 	this.applications[token] = new uovd.provider.applications.ApplicationInstance(this, application_id, token);
 
@@ -82,7 +98,7 @@ uovd.provider.applications.Html5.prototype.applicationStartWithArgs_implementati
 		var server = this.session_management.session.servers[server_id];
 
 		if(server.type != uovd.SERVER_TYPE_WEBAPPS) {
-			var ovdapp_message = opcode+""+appToken+""+appId+""+dir_type+""+share_len+""+share+""+path_len+""+path;
+			var ovdapp_message = stream.toBase64();
 			this.connections[server_id].guac_tunnel.sendMessage("ovdapp", ovdapp_message);
 			this.session_management.fireEvent("ovd.applicationsProvider.statusChanged", this, {"application":this.applications[token], "from":"", "to":"unknown"});
 		}
@@ -108,18 +124,20 @@ uovd.provider.applications.Html5.prototype.handleOrders = function(server_id, op
 		var guac_mouse    = connection.guac_mouse
 		var guac_keyboard = connection.guac_keyboard
 
-		var opcode = parameters[0].slice(0,2);
-		var bin =    parameters[0].slice(2);
-		if(opcode == "00") {         /* ORDER_INIT */
+		var stream = DataStream.fromBase64(parameters[0]);
+		var opcode = stream.read_Byte();
+
+		if(opcode == this.ORDER_INIT) {
 			/* channel connected */
 			this.rdp_provider.serverStatus(server_id, uovd.SERVER_STATUS_READY);
-		} else if (opcode == "02") { /* ORDER_STARTED */
+		} else if (opcode == this.ORDER_STARTED) {
 			/* Format :
 					uint32 = app id
 					uint32 = instance
 			*/
-			var app_id   = this.read(bin.slice(0,8),4); bin = bin.slice(8);
-			var instance = this.read(bin.slice(0,8),4); bin = bin.slice(8);
+
+			var app_id   = stream.read_UInt32LE();
+			var instance = stream.read_UInt32LE();
 
 			var application = null;
 			if(this.applications[instance]) {
@@ -135,97 +153,31 @@ uovd.provider.applications.Html5.prototype.handleOrders = function(server_id, op
 
 			this.applications[instance] = application;
 			this.session_management.fireEvent("ovd.applicationsProvider.statusChanged", this, {"application":application, "from":"unknown", "to":"started"});
-		} else if (opcode == "03") { /* ORDER_STOPPED */
+		} else if (opcode == this.ORDER_STOPPED) {
 			/* Format :
 					uint32 = instance
 			*/
-			var instance = this.read(bin.slice(0,8),4); bin = bin.slice(8);
+			var instance = stream.read_UInt32LE();
 			var application = this.applications[instance];
 
 			application.status = "stopped";
 			application.end = (new Date()).getTime();
 
 			this.session_management.fireEvent("ovd.applicationsProvider.statusChanged", this, {"application":application, "from":"started", "to":"stopped"});
-		} else if (opcode == "06") { /* ORDER_CANT_START */
+		} else if (opcode == this.ORDER_CANT_START) {
 			/* Format :
 					uint32 = instance
 			*/
-			var instance = this.read(bin.slice(0,8),4); bin = bin.slice(8);
+			var instance = stream.read_UInt32LE();
 			var application = this.applications[instance];
 
 			application.status = "aborted";
 			application.end = (new Date()).getTime();
 
 			this.session_management.fireEvent("ovd.applicationsProvider.statusChanged", this, {"application":application, "from":"unknown", "to":"aborted"});
-		} else if (opcode == "20") { /* ORDER_KNOWN_DRIVES */
+		} else if (opcode == this.ORDER_KNOWN_DRIVES) {
 			/* ??? */
 		} else {
 		}
 	}
-}
-
-uovd.provider.applications.Html5.prototype.write = function(num, bytes) {
-	var buffer = new Array();
-	var output = "";
-	var input = "";
-
-	/* convert to hex */
-	input = ""+parseInt(num).toString(16);
-
-	/* Prepend '0' if input.length is not even */
-	if((input.length % 2) != 0) {
-		input = "0" + input;
-	}
-
-	/* Push hex digits by pair */
-	for(var i=0 ; i<input.length; i+=2) {
-		buffer.push(input.charAt(i)+""+input.charAt(i+1));
-	}
-
-	/* Pad the aray with "00" up to 'bytes' arg */
-	var remains = parseInt(bytes) - buffer.length;
-	for(var i=0 ; i<remains ; ++i) {
-		buffer.push("00");
-	}
-
-	/* output as a string */
-	for(var i=0 ; i<buffer.length ; ++i) {
-		output = output+buffer[i];
-	}
-
-	return output;
-}
-
-uovd.provider.applications.Html5.prototype.read = function(str, bytes) {
-	var buffer = new Array();
-	var output = "0x";
-	var num = 0;
-
-	/* Push hex digits by pair */
-	for(var i=0 ; i<(parseInt(bytes)*2); i+=2) {
-		buffer.push(str.charAt(i)+""+str.charAt(i+1));
-	}
-
-	buffer.reverse();
-
-	for(var i=0 ; i<bytes ; ++i) {
-		output += buffer[i];
-	}
-
-	try{
-		num = parseInt(output);
-	} catch (e) {
-		return 0;
-	}
-
-	return num;
-}
-
-uovd.provider.applications.Html5.prototype.writeString = function(str) {
-	var buffer = "";
-	for(var i=0 ; i<str.length ; ++i) {
-		buffer+= this.write(str.charCodeAt(i), 2);
-	}
-
-	return buffer;
 }
--- client/web/web/media/script/uovd/provider/rdp/html5/guacamole/encodings.js
+++ client/web/web/media/script/uovd/provider/rdp/html5/guacamole/encodings.js
@@ -77,3 +77,143 @@ function base64_encode(text) {
 	if(window.btoa) { return window.btoa(text); }
 	else            { return base64.encode(text); }
 }
+
+
+DataStream = function(size) {
+	size = size || 80;
+
+	this.buffer = new Uint8Array(size);
+	this.read_pos = 0;
+	this.write_pos = 0;
+};
+
+DataStream.prototype.get_size = function() {
+	return this.write_pos;
+}
+
+DataStream.prototype.hex_dump = function() {
+	var buffer = "";
+
+	for (var i = 0; i < this.write_pos ; i++) {
+		var p = this.buffer[i].toString(16);
+		p      = (p.length == 1) ? "0"+p : p;
+		buffer = ((i+1)%8 == 0)  ? buffer+p+"\n" : buffer+p+" ";
+	}
+
+	console.log(buffer);
+}
+
+DataStream.prototype.read_Byte = function() {
+	if (this.read_pos == this.write_pos) {
+		throw "End of stream";
+	}
+
+	return this.buffer[this.read_pos++];
+}
+
+
+DataStream.prototype.read_UInt32LE = function() {
+	try {
+		var v1 = this.read_Byte();
+		var v2 = this.read_Byte();
+		var v3 = this.read_Byte();
+		var v4 = this.read_Byte();
+
+		return v1 + (v2 << 8) + (v3 << 16) + (v4 << 24);
+	} catch(e) {
+		throw "End of stream";
+	}
+};
+
+
+DataStream.prototype.write_Byte = function(byte_) {
+	if(this.write_pos == this.buffer.length) {
+		/* Double Array length */
+		var new_buffer = new Uint8Array(this.buffer.length*2);
+		new_buffer.set(this.buffer);
+		this.buffer = new_buffer;
+	}
+
+	this.buffer[this.write_pos++] = byte_;
+}
+
+DataStream.prototype.write_UInt32LE = function(value) {
+	this.write_Byte(value);
+	this.write_Byte(value >>> 8);
+	this.write_Byte(value >>> 16);
+	this.write_Byte(value >>> 24);
+};
+
+
+DataStream.prototype.write_UTF16LE = function(str) {
+	/* Limited UTF-16LE implementation */
+	/* Do not handle 4bytes notation since Javascript don't handle it */
+
+	for (var i=0; i<str.length; i++) {
+		var v = str[i].charCodeAt();
+		var w1 = v & 0xFF;
+		var w2 = (v & 0xFF00) >>> 8;
+
+		this.write_Byte(w1);
+		this.write_Byte(w2);
+	}
+};
+
+/*\
+|*|
+|*|  Base64 / binary data / UTF-8 strings utilities
+|*|
+|*|  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Base64_encoding_and_decoding
+|*|
+\*/
+
+/* Array of bytes to base64 string decoding */
+
+/* Base64 string to array encoding */
+
+DataStream.prototype.uint6ToB64 = function(nUint6) {
+	return nUint6 < 26 ?  nUint6 + 65 : nUint6 < 52 ?  nUint6 + 71 : nUint6 < 62 ?  nUint6 - 4 : nUint6 === 62 ?  43 : nUint6 === 63 ?  47 : 65;
+}
+
+DataStream.b64ToUint6 = function(nChr) {
+	return nChr > 64 && nChr < 91 ?  nChr - 65 : nChr > 96 && nChr < 123 ?  nChr - 71 : nChr > 47 && nChr < 58 ?  nChr + 4 : nChr === 43 ?  62 : nChr === 47 ?  63 : 0;
+}
+
+DataStream.prototype.toBase64 = function() {
+	aBytes = this.buffer.subarray(0, this.write_pos);
+
+	var nMod3 = 2, sB64Enc = "";
+
+	for (var nLen = aBytes.length, nUint24 = 0, nIdx = 0; nIdx < nLen; nIdx++) {
+		nMod3 = nIdx % 3;
+		if (nIdx > 0 && (nIdx * 4 / 3) % 76 === 0) { sB64Enc += "\r\n"; }
+		nUint24 |= aBytes[nIdx] << (16 >>> nMod3 & 24);
+		if (nMod3 === 2 || aBytes.length - nIdx === 1) {
+			sB64Enc += String.fromCharCode(this.uint6ToB64(nUint24 >>> 18 & 63), this.uint6ToB64(nUint24 >>> 12 & 63), this.uint6ToB64(nUint24 >>> 6 & 63), this.uint6ToB64(nUint24 & 63));
+			nUint24 = 0;
+		}
+	}
+
+	return sB64Enc.substr(0, sB64Enc.length - 2 + nMod3) + (nMod3 === 2 ? '' : nMod3 === 1 ? '=' : '==');
+};
+
+DataStream.fromBase64 = function(sBase64, nBlocksSize) {
+	var sB64Enc = sBase64.replace(/[^A-Za-z0-9\+\/]/g, "");
+	var nInLen = sB64Enc.length;
+	var nOutLen = nBlocksSize ? Math.ceil((nInLen * 3 + 1 >> 2) / nBlocksSize) * nBlocksSize : nInLen * 3 + 1 >> 2;
+	var taBytes = new DataStream(nOutLen);
+
+	for (var nMod3, nMod4, nUint24 = 0, nOutIdx = 0, nInIdx = 0; nInIdx < nInLen; nInIdx++) {
+		nMod4 = nInIdx & 3;
+		nUint24 |= DataStream.b64ToUint6(sB64Enc.charCodeAt(nInIdx)) << 18 - 6 * nMod4;
+		if (nMod4 === 3 || nInLen - nInIdx === 1) {
+			for (nMod3 = 0; nMod3 < 3 && nOutIdx < nOutLen; nMod3++, nOutIdx++) {
+				taBytes.write_Byte(nUint24 >>> (16 >>> nMod3 & 24) & 255);
+			}
+
+			nUint24 = 0;
+		}
+	}
+
+	return taBytes;
+}
