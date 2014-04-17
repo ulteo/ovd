ulteo/ovd

commit e057af63e1c86a11e346f0722328a23555ca4f1b

Packaging: Fix permissions on installation/update process
Bug introduced in
 - 72d55a0e608cd1b39f78885cf462da5f9c00eba1
 - 8cb365e400d70be7e95be3a0d6055096149861ca
 - a1afa0548daaee00bfe776d1202f97d492bd6bc7

packaging/rpm/ovd-session-manager-premium/rhel-5.5.spec
@@ -75,7 +75,7 @@ INSTALLDIR=/usr/share/ulteo/sessionmanager
 # Check if update is possible
 if [ -f $INSTALLDIR/tools/can_update.php ]
 then
-   php $INSTALLDIR/tools/can_update.php 2>/dev/null
+   su %{apache_user} "php $INSTALLDIR/tools/can_update.php" 2>/dev/null
    if [ $? -ne 0 ]
    then
       exit 1
@@ -93,7 +93,7 @@ INSTALLDIR=/usr/share/ulteo/sessionmanager
 if [ -f $INSTALLDIR/tools/update_database.php ]
 then
    echo "Updating database."
-   php $INSTALLDIR/tools/update_database.php 2>/dev/null
+   su %{apache_user} "php $INSTALLDIR/tools/update_database.php" 2>/dev/null
    if [ $? -ne 0 ]
    then
       exit 1
@@ -104,7 +104,7 @@ fi
 if [ -f $INSTALLDIR/tools/update_wsdl_cache.php ]
 then
    echo "Purging wsdl cache files."
-   php $INSTALLDIR/tools/update_wsdl_cache.php 2>/dev/null
+   su %{apache_user} "php $INSTALLDIR/tools/update_wsdl_cache.php" 2>/dev/null
    if [ $? -ne 0 ]
    then
       exit 1

packaging/rpm/ovd-session-manager/rhel-5.5.spec
@@ -91,7 +91,7 @@ INSTALLDIR=/usr/share/ulteo/sessionmanager
 # Check if update is possible
 if [ -f $INSTALLDIR/tools/can_update.php ]
 then
-   php $INSTALLDIR/tools/can_update.php 2>/dev/null
+   su %{apache_user} -c "php $INSTALLDIR/tools/can_update.php" 2>/dev/null
    if [ $? -ne 0 ]
    then
       exit 1
@@ -152,7 +152,7 @@ fi
 if [ -f $INSTALLDIR/tools/update_database.php ]
 then
    echo "Updating database."
-   php $INSTALLDIR/tools/update_database.php 2>/dev/null
+   su %{apache_user} -c "php $INSTALLDIR/tools/update_database.php" 2>/dev/null
    if [ $? -ne 0 ]
    then
       exit 1
@@ -163,7 +163,7 @@ fi
 if [ -f $INSTALLDIR/tools/update_wsdl_cache.php ]
 then
    echo "Purging wsdl cache files."
-   php $INSTALLDIR/tools/update_wsdl_cache.php 2>/dev/null
+   su %{apache_user} -c "php $INSTALLDIR/tools/update_wsdl_cache.php" 2>/dev/null
    if [ $? -ne 0 ]
    then
       exit 1

packaging/debian/ovd-session-manager-premium/lucid/ulteo-ovd-session-manager-premium.postinst
@@ -3,12 +3,13 @@
 . /usr/share/debconf/confmodule
 
 INSTALLDIR=/usr/share/ulteo/sessionmanager
+A2USER=www-data
 
 # Update database
 if [ -f $INSTALLDIR/tools/update_database.php ]
 then
    echo "Updating database"
-   php $INSTALLDIR/tools/update_database.php 2>/dev/null
+   su $A2USER -c "php $INSTALLDIR/tools/update_database.php" 2>/dev/null
    [ $? -ne 0 ] && exit 1
 fi
 
@@ -16,7 +17,7 @@ fi
 if [ -f $INSTALLDIR/tools/update_wsdl_cache.php ]
 then
    echo "Purging wsdl cache files."
-   php $INSTALLDIR/tools/update_wsdl_cache.php 2>/dev/null
+   su $A2USER -c "php $INSTALLDIR/tools/update_wsdl_cache.php" 2>/dev/null
    [ $? -ne 0 ] && exit 1
 fi
 

packaging/debian/ovd-session-manager/lucid/ulteo-ovd-session-manager.postinst
@@ -114,7 +114,7 @@ fi
 if [ -f $INSTALLDIR/tools/update_database.php ]
 then
    echo "Updating database."
-   php $INSTALLDIR/tools/update_database.php 2>/dev/null
+   su $A2USER -c "php $INSTALLDIR/tools/update_database.php" 2>/dev/null
    [ $? -ne 0 ] && exit 1
 fi
 
@@ -122,7 +122,7 @@ fi
 if [ -f $INSTALLDIR/tools/update_wsdl_cache.php ]
 then
    echo "Purging wsdl cache files."
-   php $INSTALLDIR/tools/update_wsdl_cache.php 2>/dev/null
+   su $A2USER -c "php $INSTALLDIR/tools/update_wsdl_cache.php" 2>/dev/null
    [ $? -ne 0 ] && exit 1
 fi
 

packaging/debian/ovd-session-manager/lucid/ulteo-ovd-session-manager.preinst
@@ -3,11 +3,12 @@
 . /usr/share/debconf/confmodule
 
 INSTALLDIR=/usr/share/ulteo/sessionmanager
+A2USER=www-data
 
 # Check if update is possible
 if [ -f $INSTALLDIR/tools/can_update.php ]
 then
-   php $INSTALLDIR/tools/can_update.php 2>/dev/null
+   su $A2USER -c "php $INSTALLDIR/tools/can_update.php" 2>/dev/null
    [ $? -ne 0 ] && exit 1
 fi
 

commit 1d7dc84b2baea7f1f7f2b61fe45ca5cae9dbcfdf

Bugfix: wrong su syntax in premium rhel package file. Bug introduced by the commit e057af63e1c86a11e346f0722328a23555ca4f1b

packaging/rpm/ovd-session-manager-premium/rhel-5.5.spec
@@ -75,7 +75,7 @@ INSTALLDIR=/usr/share/ulteo/sessionmanager
 # Check if update is possible
 if [ -f $INSTALLDIR/tools/can_update.php ]
 then
-   su %{apache_user} "php $INSTALLDIR/tools/can_update.php" 2>/dev/null
+   su %{apache_user} -s /usr/bin/php $INSTALLDIR/tools/can_update.php 2>/dev/null
    if [ $? -ne 0 ]
    then
       exit 1
@@ -93,7 +93,7 @@ INSTALLDIR=/usr/share/ulteo/sessionmanager
 if [ -f $INSTALLDIR/tools/update_database.php ]
 then
    echo "Updating database."
-   su %{apache_user} "php $INSTALLDIR/tools/update_database.php" 2>/dev/null
+   su %{apache_user} -s /usr/bin/php $INSTALLDIR/tools/update_database.php 2>/dev/null
    if [ $? -ne 0 ]
    then
       exit 1
@@ -104,7 +104,7 @@ fi
 if [ -f $INSTALLDIR/tools/update_wsdl_cache.php ]
 then
    echo "Purging wsdl cache files."
-   su %{apache_user} "php $INSTALLDIR/tools/update_wsdl_cache.php" 2>/dev/null
+   su %{apache_user} -s /usr/bin/php $INSTALLDIR/tools/update_wsdl_cache.php 2>/dev/null
    if [ $? -ne 0 ]
    then
       exit 1

packaging/rpm/ovd-session-manager/rhel-5.5.spec
@@ -91,7 +91,7 @@ INSTALLDIR=/usr/share/ulteo/sessionmanager
 # Check if update is possible
 if [ -f $INSTALLDIR/tools/can_update.php ]
 then
-   su %{apache_user} -c "php $INSTALLDIR/tools/can_update.php" 2>/dev/null
+   su %{apache_user} -s /usr/bin/php $INSTALLDIR/tools/can_update.php 2>/dev/null
    if [ $? -ne 0 ]
    then
       exit 1
@@ -152,7 +152,7 @@ fi
 if [ -f $INSTALLDIR/tools/update_database.php ]
 then
    echo "Updating database."
-   su %{apache_user} -c "php $INSTALLDIR/tools/update_database.php" 2>/dev/null
+   su %{apache_user} -s /usr/bin/php $INSTALLDIR/tools/update_database.php 2>/dev/null
    if [ $? -ne 0 ]
    then
       exit 1
@@ -163,7 +163,7 @@ fi
 if [ -f $INSTALLDIR/tools/update_wsdl_cache.php ]
 then
    echo "Purging wsdl cache files."
-   su %{apache_user} -c "php $INSTALLDIR/tools/update_wsdl_cache.php" 2>/dev/null
+   su %{apache_user} -s /usr/bin/php $INSTALLDIR/tools/update_wsdl_cache.php 2>/dev/null
    if [ $? -ne 0 ]
    then
       exit 1